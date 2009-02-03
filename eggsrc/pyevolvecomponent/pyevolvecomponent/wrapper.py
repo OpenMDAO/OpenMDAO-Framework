31bb5b5324074f56e0d211fee3f0ae8e4e4140b3
Justin Gray <justin.s.gray@nasa.gov>
2009-02-03 07:35:53 -0500

    commit of the initial pyevolve component creation

---
+++ eggsrc/pyevolvecomponent/pyevolvecomponent/wrapper.py
@@ -0,0 +1,26 @@
+from openmdao.main.component import Component
+
+from pyevolve import G1Dlist,GsimpleGA,Initializators,Mutators,Consts,DBadapters
+from NASAevolve import G1DListCrossOverRealHypersphere
+
+
+class wrapper(Component):
+    def __init__(self,name,parent=None,desc=None): 
+        Component.__init__(self,name,parent,desc)
+        
+        self.state = Component.STATE_IDLE
+        self._stop = False
+        self._input_changed = False
+        
+        self.resource_desc = {"python_version": 2.5, "operator": "home_sapien"}
+        
+        self.genome = G1Dlist(2)
+        self.ga = gaEngine.GsimpleGA(self.genome)
+        
+    def post_config(self):
+        pass
+        
+    def execute(self):
+        
+    
+    
---
+++ eggsrc/pyevolvecomponent/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='pyevolvecomponent',
+      version="0.1",
+      description="OpenMDAO component wrapper for pyevolve module",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='',
+      author='Justin Gray ',
+      author_email='Justin.S.Gray@nasa.gov',
+      url='',
+      license='',
+      zip_safe=True,
+      install_requires=[
+          'pyevolve'
+      ],
+      )
