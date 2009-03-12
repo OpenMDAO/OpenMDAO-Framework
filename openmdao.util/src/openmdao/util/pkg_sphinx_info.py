903c83ebd4cd0ebded22583bcfd6de36e7e25e99
Bret Naylor <bnaylor@grc.nasa.gov>
2009-03-12 14:05:17 -0400

    added metadata querying stuff to pkg_res_example.py so we can get metadata out of zipped eggs

--- openmdao.util/src/openmdao/util/pkg_sphinx_info.py
+++ openmdao.util/src/openmdao/util/pkg_sphinx_info.py
@@ -3,6 +3,8 @@ import os
 import sys
 from os.path import join, splitext, dirname, basename, abspath
 import fnmatch
+import rfc822
+import StringIO
 from pkg_resources import get_distribution, resource_listdir
 from pkg_resources import WorkingSet, Requirement
 
@@ -33,7 +35,7 @@ def get_resource_files(dist, exList=None, incList=None, dirname=''):
     """Retrieve resource file pathnames from within a distribution."""
     
     exlist = exList or []
-    inclist = incList or []
+    inclist = incList or ['*']
     
     for res in dist.resource_listdir(dirname):
         if dirname != '':
@@ -47,6 +49,34 @@ def get_resource_files(dist, exList=None, incList=None, dirname=''):
         else:
             if _match(respath, inclist) and not _match(respath, exlist):
                 yield respath
+
+                
+def get_metadata(dist, dirname=''):
+    """Retrieve metadata from within a distribution and return it as
+    a generator of tuples of the form (metadata_key, value).
+    """
+    
+    for name in dist.metadata_listdir(dirname):
+        if dirname != '':
+            path = '/'.join([dirname, name])
+        else:
+            path = name
+        if dist.metadata_isdir(path):
+            for md in get_metadata(dist, path):
+                yield md
+        elif name.endswith('.txt'):
+            yield (path[:-4], [x.strip() for x in dist.get_metadata(path).splitlines() if x.strip() != ''])
+        elif name == 'PKG-INFO':
+            instr = StringIO.StringIO(dist.get_metadata(name))
+            message = rfc822.Message(instr)
+            for k,v in message.items():
+                yield (k, v)
+        elif name == 'not-zip-safe':
+            yield ('zip-safe', False)
+        elif name == 'zip-safe':
+            yield ('zip-safe', True)
+        else:
+            yield (path, dist.get_metadata(path))
                 
             
 def pkg_sphinx_info(env,startdir, pkg, outfile, show_undoc=False, underline='-'):
@@ -71,6 +101,10 @@ def pkg_sphinx_info(env,startdir, pkg, outfile, show_undoc=False, underline='-')
     names.sort()
     for name in names:
         mod_sphinx_info(name, outfile, show_undoc=show_undoc)
+
+# TODO: add metadata info to doc page        
+#    for md,val in get_metadata(dist):
+#        print "%s:\n%s\n"%(md,val)
 
 
 if __name__ == '__main__':
