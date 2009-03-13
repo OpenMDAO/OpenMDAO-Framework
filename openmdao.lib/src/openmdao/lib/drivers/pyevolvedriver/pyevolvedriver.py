58dabbc937127625d8e4682a332398d85baaefdd
Justin Gray <Justin.S.Gray@nasa.gov>
2009-03-13 13:30:13 -0400

    improved pyevolve tests, adjusted pyvolve to work under new namespace package

--- openmdao.lib/src/openmdao/lib/drivers/pyevolvedriver/pyevolvedriver.py
+++ openmdao.lib/src/openmdao/lib/drivers/pyevolvedriver/pyevolvedriver.py
@@ -8,7 +8,7 @@ from pyevolve import G1DList,G1DBinaryString,G2DList,GAllele,GenomeBase
 from pyevolve import GSimpleGA,Selectors,Initializators,Mutators,Consts,DBAdapters
 from pyevolve import GenomeBase
 from openmdao.main.expreval import ExprEvaluator
-  
+
 class pyevolvedriver(Driver):
     """OpenMDAO wrapper for the pyevolve genetic algorithm framework. The 
     wrapper uses two attributes to configure the GA: The wrapper conforms to the
@@ -17,32 +17,32 @@ class pyevolvedriver(Driver):
     G1DList. By default the wrapper uses the GsimpleGA engine from pyevolve with 
     all its default settings. Currently, only the default configuration for the 
     genome is supported. 
-    
+
     The standard pyevolve library is provided:
         G1Dlist,G1DBinaryString,G2dList,GAllele
         GsimpleGA,Initializators,Mutators,Consts,DBadapters
-        
+
     ** Example **
     Setting up Genome
     >>> genome = G1DList
     >>> genome.initializator.set(Initializators.G1DListInitializatorReal)
     >>> genome.mutator.set(Mutators.G1DListMutatorIntegerGaussian)
     >>> genome.evaluator.set(somefunc)
-    
+
     :param freq_stats: the frequency which the driver records population information
     :param genome: the particular genome for optimization
     :param GA: the GA algorithm object, default configuration used GsimpleGA
-    
+
     """
-   
-    
-    
+
+
+
     def _get_objective(self):
         if self._objective is None:
             return ''
         else:
             return self._objective.text
-            
+
     def _set_objective(self,obj):
         self._objective = None
         try:
@@ -53,20 +53,20 @@ class pyevolvedriver(Driver):
             self.raise_exception("objective specified, '"+str(obj)+"', is not valid a valid OpenMDAO object. If it does exist in the model, a framework variable may need to be created",
                                  RuntimeError)            
     objective = property(_get_objective,_set_objective)
-    
+
     def get_objective_val(self):
         """evaluate the new objective"""
         if self.objective is None:
             return None
         else:
             return self._objective.evaluate()     
-            
+
     def __init__(self,name,parent=None,desc=None): 
         super(pyevolvedriver,self).__init__(name,parent,desc)
-       
+
         self.genome = GenomeBase.GenomeBase()
         self.GA = GSimpleGA.GSimpleGA(self.genome)
-        
+
         #inputs - value of None means use default
         self.decoder = None
         self.freq_stats = None
@@ -82,23 +82,23 @@ class pyevolvedriver(Driver):
         self.Generations = None
         self.Minimax = None
         self.Elitism = None
-        
+
         #outputs
         self.best_individual= None
         self.decoder = None
-        
+
         #internal stuff
         self._objective = None
-        
+
         #TODO Need to make a Genome socket
 
-        
-    
-        
+
+
+
     def set_GA_property(self,setFunc,val): 
         if val != None: 
             return setFunc(val)
-            
+
     def set_GA_FunctionSlot(self,slot,funcList,RandomApply=False,):
         if funcList == None: return
         slot.clear()
@@ -108,34 +108,38 @@ class pyevolvedriver(Driver):
                 slot.set(func)
             else: slot.add(func)
         slot.setRandomApply(RandomApply)
-            
-            
+
+
     def evaluate(self,genome):
         self.decoder(genome)
-        
+
         self.parent.workflow.run()
         return self.get_objective_val()
-    
+
     def verify(self):
         #genome verify
         if not isinstance(self.genome,GenomeBase.GenomeBase):
             self.raise_exception("genome provided is not valid. Does not inherit from pyevolve.GenomeBase.GenomeBase",TypeError)
-    
-	    #decoder verify
+
+            #decoder verify
         if self.decoder == None: # check if None first
             self.raise_exception("decoder specified as 'None'. A valid decoder must be present",TypeError)
         try: # won't work if decoder is None
             self.decoder(self.genome)
         except TypeError:
             self.raise_exception("decoder specified as does not have the right signature. Must take only 1 argument",TypeError)
-        
-    
+
+
     def execute(self):
         """ Perform the optimization"""
+        
+        if self.objective == '':
+            self.raise_exception("objective specified as None, please provide an objective expression.",RuntimeError)
+        
         self.verify()
         #configure the evaluator function of the genome
         self.genome.evaluator.set(self.evaluate)
-        
+
         self.GA = GSimpleGA.GSimpleGA(self.genome, self.seed)
         self.set_GA_property(self.GA.setDBAdapter,self.DBAdapter)
         self.set_GA_property(self.GA.setPopulationSize,self.PopulationSize)
@@ -145,12 +149,11 @@ class pyevolvedriver(Driver):
         self.set_GA_property(self.GA.setGenerations,self.Generations)
         self.set_GA_property(self.GA.setMinimax,self.Minimax)
         self.set_GA_property(self.GA.setElitism,self.Elitism)
-        
+
         self.set_GA_FunctionSlot(self.GA.selector,self.selector)
         self.set_GA_FunctionSlot(self.GA.stepCallback,self.stepCallback)
         self.set_GA_FunctionSlot(self.GA.terminationCriteria,self.terminationCriteria)
-		
+
         self.GA.evolve(freq_stats = self.freq_stats)
         self.best = self.GA.bestIndividual()
-        
-        
+
--- openmdao.lib/src/openmdao/lib/drivers/pyevolvedriver/test/test_opt_pyevolve.py
+++ openmdao.lib/src/openmdao/lib/drivers/pyevolvedriver/test/test_opt_pyevolve.py
@@ -54,7 +54,7 @@ class pyevolvedriverTestCase(unittest.TestCase):
 
     def tearDown(self):
         self.top = None
-        
+
     #basic test to make sure optmizer is working 
     def test_optimizeSphere(self):
         self.top.driver.objective = "comp.total" 
@@ -77,7 +77,7 @@ class pyevolvedriverTestCase(unittest.TestCase):
         self.top.driver.SortType = None
         self.top.driver.MutationRate = .02
         self.top.driver.CrossoverRate = None
-        self.top.driver.Generations = 200
+        self.top.driver.Generations = 1
         self.top.driver.Minimax = pyevolvedriver.Consts.minimaxType["minimize"]
         self.top.driver.Elitism = None
         
@@ -88,11 +88,46 @@ class pyevolvedriverTestCase(unittest.TestCase):
         
         self.top.run()
         
-        self.assertAlmostEqual(self.top.driver.best.score,0.000038,places = 4)
+        self.assertAlmostEqual(self.top.driver.best.score,0.1906,places = 4)
         x0,x1 = [x for x in self.top.driver.best] 
-        self.assertAlmostEqual(x0,0,places = 1)
-        self.assertAlmostEqual(x1,0,places = 1)
+        self.assertAlmostEqual(x0,.1966,places = 4)
+        self.assertAlmostEqual(x1,.3897,places = 4)
     
+    def test_noObjectiveSet(self):
+        #self.top.driver.objective = "comp.total" 
+        self.top.driver.genome = pyevolvedriver.G1DList.G1DList(2)
+        self.top.driver.genome.setParams(rangemin=-5.12, rangemax=5.13)
+        self.top.driver.genome.initializator.set(pyevolvedriver.Initializators.G1DListInitializatorReal)
+        self.top.driver.genome.mutator.set(pyevolvedriver.Mutators.G1DListMutatorRealGaussian)
+        
+        #configure the GAengine 
+          #None will configure the defualt
+        self.top.driver.decoder = self.decoder  
+        self.top.driver.freq_stats = 0
+        self.top.driver.seed = 123
+        
+        self.top.driver.terminationCriteria = None
+        self.top.driver.DBAdapter = None
+        self.top.driver.PopulationSize = None
+        self.top.driver.SortType = None
+        self.top.driver.MutationRate = .02
+        self.top.driver.CrossoverRate = None
+        self.top.driver.Generations = 1
+        self.top.driver.Minimax = pyevolvedriver.Consts.minimaxType["minimize"]
+        self.top.driver.Elitism = None
+        
+        self.top.driver.selector = None
+        self.top.driver.selector = [pyevolvedriver.Consts.CDefGASelector] #this is a default, just for testing
+        self.top.driver.stepCallback = None
+        self.top.driver.terminationCriteria = None
+        
+        try:
+            self.top.run()
+        except RuntimeError, err: 
+            self.assertEqual(str(err),"top.driver: objective specified as None, please provide an objective expression." )
+        else: 
+            self.fail("expecting RuntimeError")
+        
     def test_invalidObjective(self):
         try:
             self.top.driver.objective = "comp.badojbjective"        
@@ -111,6 +146,7 @@ class pyevolvedriverTestCase(unittest.TestCase):
     
     #should throw an error because no decode function is provided
     def test_noDecoder(self):
+        self.top.driver.objective = "comp.total" 
         self.top.driver.decoder = None
         try:
             self.top.driver.run()
@@ -121,6 +157,7 @@ class pyevolvedriverTestCase(unittest.TestCase):
 
     
     def test_wrongDecoderSignature(self):
+        self.top.driver.objective = "comp.total" 
         def decodeBad(genome,secondArgument):
             pass
         self.top.driver.decoder = decodeBad
@@ -134,6 +171,7 @@ class pyevolvedriverTestCase(unittest.TestCase):
     
     #should throw and error about the lack of a genome
     def test_noGenomeTest(self): 
+        self.top.driver.objective = "comp.total" 
         self.top.driver.genome = None
         try:
             self.top.driver.run()
@@ -145,6 +183,7 @@ class pyevolvedriverTestCase(unittest.TestCase):
     
     #should throw an error becuase genome does not inherit from GenomeBase
     def test_GenomeNotGenomeTest(self):
+        self.top.driver.objective = "comp.total" 
         self.top.driver.genome = [1,2]
         try:
             self.top.driver.run()
