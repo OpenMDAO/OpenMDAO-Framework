2e2a0bfb521555dadd8be7baf13790ad5faa90fc
Kenneth Moore <Kenneth.T.Moore-1@nasa.gov>
2009-03-19 11:14:09 -0400

    Fixed test for units in Float

--- openmdao.main/src/openmdao/main/test/test_float.py
+++ openmdao.main/src/openmdao/main/test/test_float.py
@@ -67,9 +67,8 @@ class FloatTestCase(unittest.TestCase):
     def test_units_None_to_None(self):
         self.float1.units = None
         self.float2.units = None
-        self.float1._convert(self.float2)
-        self.assertEqual(3.1415926, self.float1.value)
-        self.assertEqual(42., self.float2.value)
+        self.float1.setvar(None, self.float2)
+        self.assertEqual(42.0, self.float1.value)
         
     def test_units_Unit_to_None(self):
         self.float1.units = 'ft'
@@ -80,7 +79,7 @@ class FloatTestCase(unittest.TestCase):
             self.assertEqual(str(err), 
                 "h1.float2 units (None) are incompatible with units (ft) of h1.float1")
         else:
-            self.fail("ValueError expected")
+            self.fail("TypeError expected")
         
     def test_get(self):
         val = self.float1.get(None)
