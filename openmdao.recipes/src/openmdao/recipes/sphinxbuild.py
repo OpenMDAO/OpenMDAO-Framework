4f21bd55d4bf7c6b4e36499cd9f3fc1388f3f867
Bret Naylor <bnaylor@grc.nasa.gov>
2009-03-20 14:08:25 -0400

    small fixes to sphinxbuild

--- openmdao.recipes/src/openmdao/recipes/sphinxbuild.py
+++ openmdao.recipes/src/openmdao/recipes/sphinxbuild.py
@@ -2,11 +2,9 @@
 import os
 import os.path
 import sys
-import stat
-from subprocess import check_call, Popen
 import fnmatch
 import logging
-from pkg_resources import working_set, Environment
+from pkg_resources import Environment
 
 import openmdao.util.pkg_sphinx_info as pkg_sphinx_info
 from openmdao.util.procutil import run_command
@@ -39,17 +37,31 @@ class SphinxBuild(object):
         self.packages = options.get('packages') or ''  
         self.docdir = options.get('doc_dir') or 'docs'
         self.builddir = options.get('build_dir') or '_build' 
-        self.builder = options.get('build_script') or os.path.join(self.branchdir,
-                                                                   'docs',
-                                                                   'python-scripts',
-                                                                   'sphinx-build')
+        self.builder = options.get('build_script') or os.path.join(
+                                                           self.branchdir,
+                                                           'docs',
+                                                           'python-scripts',
+                                                           'sphinx-build')
         self.egg_dir = buildout['buildout']['eggs-directory']
         self.dev_egg_dir = buildout['buildout']['develop-eggs-directory']
+
+
+    def _write_src_docs(self):
+        for pack in self.packages.split():
+            self.logger.info('creating autodoc file for %s' % pack)
+            f = open(os.path.join(self.docdir,'srcdocs','packages',
+                                  pack+'.rst'),'w')
+            pkg_sphinx_info(self.env,self.branchdir, pack, f, 
+                            show_undoc=True, underline='-')
+            f.close()
+
                
     def install(self):
         dev_eggs = fnmatch.filter(os.listdir(self.dev_egg_dir),'*.egg-link')
         # grab the first line of each dev egg link file
-        self.dev_eggs = [open(os.path.join(self.dev_egg_dir,f),'r').readlines()[0].strip() for f in dev_eggs]
+        self.dev_eggs = [
+           open(os.path.join(self.dev_egg_dir,f),'r').readlines()[0].strip() 
+                                                               for f in dev_eggs]
         self.env = Environment(self.dev_eggs+[os.path.join(self.egg_dir,x) 
                           for x in fnmatch.filter(os.listdir(self.egg_dir),'*.egg')])
         
@@ -68,16 +80,10 @@ class SphinxBuild(object):
             os.makedirs(os.path.join(self.builddir,'html'))
         if not os.path.isdir(os.path.join(self.builddir,'doctrees')):
             os.makedirs(os.path.join(self.builddir,'doctrees'))
-        #if not os.path.isdir('generated_images'):
-        #    os.makedirs('generated_images')
             
         # build the docs using Sphinx
         try:
             sys.path[0:0] = [os.path.abspath('python-scripts')]
-#            execfile(os.path.join('python-scripts','rebuild.py'))
-#            check_call([self.interpreter, self.builder, '-P','-b', 'html', 
-#                        '-d', os.path.join(self.builddir,'doctrees'), 
-#                        '.', os.path.join(self.builddir,'html')])
             out,ret = run_command('%s %s -P -b html -d %s . %s' %
                                     (self.interpreter, self.builder, 
                                      os.path.join(self.builddir,'doctrees'), 
@@ -117,11 +123,3 @@ class SphinxBuild(object):
     def update(self):
         return []    
 
-    def _write_src_docs(self):
-        for pack in self.packages.split():
-            self.logger.info('creating autodoc file for %s' % pack)
-            f = open(os.path.join(self.docdir,'srcdocs','packages',
-                                  pack+'.rst'),'w')
-            pkg_sphinx_info(self.env,self.branchdir, pack, f, 
-                            show_undoc=True, underline='-')
-            f.close()
