de3f988381620e1c3945343060cf45c4be948ac8
Bret Naylor <bnaylor@grc.nasa.gov>
2009-03-25 12:47:40 -0400

    merged from working_main and turned openmdao.examples into a package

---
+++ openmdao.examples/setup.py
@@ -0,0 +1,31 @@
+import os, sys
+
+# pylint: disable-msg=F0401
+
+from setuptools import setup, find_packages
+
+version = '0.1.0'
+
+setup(name='openmdao.examples',
+      version=version,
+      description="OpenMDAO examples",
+      long_description="""\
+""",
+      classifiers=[],
+      keywords='optimization multidisciplinary multi-disciplinary analysis',
+      author='',
+      author_email='',
+      url='',
+      license='NASA Open Source Agreement',
+      namespace_packages=["openmdao"],
+      #package_dir={'': 'src'},
+      packages=find_packages(), #['openmdao','openmdao.examples'],
+      include_package_data=True,
+      test_suite='nose.collector',
+      zip_safe=False,
+      install_requires=[
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
