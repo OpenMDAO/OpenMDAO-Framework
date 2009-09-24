351fe4849a6cec577535232a5ce879adfdde8f27
Justin Gray <justin.s.gray@nasa.gov>
2009-09-24 12:47:55 -0400

    removed extra files in units package

--- contrib/units/unitsTest.py
+++
@@ -1,9 +0,0 @@
-from units import PhysicalQuantity as pq2
-from Scientific.Physics.PhysicalQuantities import PhysicalQuantity as pq1
-
-scientific_unit = pq1("1 1/min")
-justin_unit = pq2("1 1/min")
-
-print scientific_unit.unit.factor, scientific_unit.unit.names, scientific_unit.unit.powers
-
-print justin_unit.unit.factor,justin_unit.unit.names,justin_unit.unit.powers
