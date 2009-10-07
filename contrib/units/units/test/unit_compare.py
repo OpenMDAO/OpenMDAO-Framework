9f234b7a88631772e4bfb29ce8923ea20fc81ee8
Justin Gray <justin.s.gray@nasa.gov>
2009-10-07 08:20:59 -0400

    asdf

--- contrib/units/units/test/unit_compare.py
+++ contrib/units/units/test/unit_compare.py
@@ -4,25 +4,24 @@ import Scientific.Physics.PhysicalQuantities as case1
 import units as case2
 import time
 
+import cProfile
+
 n = 20000
 
 #x1 = case1.PhysicalQuantity('5cm')
 #x2 = case2.PhysicalQuantity('5cm')
 
-
-t1 = time.time()
-
+stmt = """
 for jj in xrange(n):
     pq = case1.PhysicalQuantity(5, 'mi/h')
     pq.convertToUnit('m/s')
-    
-print "Scientific -> Elapsed time: ", time.time()-t1
-print ""
+"""
+cProfile.run(stmt)
 
-t2 = time.time()
-
+stmt = """
 for jj in xrange(n):
     pq = case2.PhysicalQuantity(5, 'mi/h')
     pq.convertToUnit('m/s')
-    
-print "Justin -> Elapsed time: ", time.time()-t2
+"""
+
+cProfile.run(stmt)
