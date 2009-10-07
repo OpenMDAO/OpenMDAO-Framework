1e4c3ad4a8642cae0dafd74c68567ae69c54b482
Justin Gray <justin.s.gray@nasa.gov>
2009-10-07 08:23:17 -0400

    asdf

--- contrib/units/units/test/unit_compare.py
+++ contrib/units/units/test/unit_compare.py
@@ -11,6 +11,25 @@ n = 20000
 #x1 = case1.PhysicalQuantity('5cm')
 #x2 = case2.PhysicalQuantity('5cm')
 
+
+t1 = time.time()
+
+for jj in xrange(n):
+    pq = case1.PhysicalQuantity(5, 'mi/h')
+    pq.convertToUnit('m/s')
+    
+print "Scientific -> Elapsed time: ", time.time()-t1
+print ""
+
+t2 = time.time()
+
+for jj in xrange(n):
+    pq = case2.PhysicalQuantity(5, 'mi/h')
+    pq.convertToUnit('m/s')
+    
+print "Justin -> Elapsed time: ", time.time()-t2
+
+
 stmt = """
 for jj in xrange(n):
     pq = case1.PhysicalQuantity(5, 'mi/h')
