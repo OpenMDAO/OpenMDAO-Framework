fc5584fbb1fcdbc58d835a2768b3c769893372c0
Paulette Ziegfeld <paulette.j.ziegfeld@nasa.gov>
2009-11-24 15:48:30 -0500

    Minor corrections; alsod moved sidebar back to left as IE won't display right sidebar correctly.

--- docs/conf.py
+++ docs/conf.py
@@ -119,7 +119,7 @@ html_logo = 'OpenMDAO_Logo_sm1.bmp'
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-#html_favicon = None
+#html_favicon = 'none'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -166,7 +166,7 @@ html_last_updated_fmt = '%b %d, %Y'
 
 html_theme = "default"
 html_theme_options = {
-     "rightsidebar": "true",
+     "rightsidebar": "",
      "headtextcolor": "teal",
      "relbarbgcolor": "teal",
      "stickysidebar": "",
