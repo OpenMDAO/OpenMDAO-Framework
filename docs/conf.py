0814a3fa26cd33cd4ed6a927756d216521d9fcc1
Paulette Ziegfeld <paulette.j.ziegfeld@nasa.gov>
2009-12-24 15:42:12 -0500

    replaced favicon

--- docs/conf.py
+++ docs/conf.py
@@ -119,7 +119,7 @@ html_logo = 'OpenMDAO_Logo_sm1.bmp'
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_favicon = 'favicon.ico'
+html_favicon = 'newFavicon.ico'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
