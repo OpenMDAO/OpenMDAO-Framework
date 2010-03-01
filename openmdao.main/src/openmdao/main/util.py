0845c505ccf23b9897ee11992c409a28210cbb57
setowns1 <setowns1@torpedo.grc.nasa.gov>
2010-03-01 10:35:59 -0500

    close egg file to avoid problems on storm.

--- openmdao.main/src/openmdao/main/util.py
+++ openmdao.main/src/openmdao/main/util.py
@@ -20,22 +20,26 @@ def filexfer(src_server, src_path, dst_server, dst_path, mode=''):
     else:
         src_file = src_server.open(src_path, 'r'+mode)
 
-    if dst_server is None:
-        dst_file = open(dst_path, 'w'+mode)
-    else:
-        dst_file = dst_server.open(dst_path, 'w'+mode)
+    try:
+        if dst_server is None:
+            dst_file = open(dst_path, 'w'+mode)
+        else:
+            dst_file = dst_server.open(dst_path, 'w'+mode)
 
-    if src_server is None and dst_server is None:
-        chunk = 1 << 20  # 1MB locally.
-    else:
-        chunk = 1 << 17  # 128KB over network.
+        if src_server is None and dst_server is None:
+            chunk = 1 << 20  # 1MB locally.
+        else:
+            chunk = 1 << 17  # 128KB over network.
 
-    data = src_file.read(chunk)
-    while data:
-        dst_file.write(data)
-        data = src_file.read(chunk)
-    src_file.close()
-    dst_file.close()
+        try:
+            data = src_file.read(chunk)
+            while data:
+                dst_file.write(data)
+                data = src_file.read(chunk)
+        finally:
+            dst_file.close()
+    finally:
+        src_file.close()
 
     if src_server is None:
         mode = os.stat(src_path).st_mode
