2a61bb79621dbbfc4511b7be86a11277663c2c84
Bret Naylor <bret@ubuntu>
2010-03-26 16:36:37 -0400

    update to mkinstaller.py

--- scripts/mkinstaller.py
+++ scripts/mkinstaller.py
@@ -4,62 +4,6 @@ import virtualenv
 
 
 
-#def run(argvs=None):
-    #"""Set up the OpenMDAO environment in the specified directory. The 
-    #directory must NOT exist prior to calling this function.
-    #"""
-    #if argvs is None:
-        #argvs = sys.argv[1:]
-    #parser = OptionParser()
-    #parser.add_option("-f", "--find-links", action="append", type="string", dest='findlinks', 
-                      #help="URLs where distributions may be found") 
-    #parser.add_option("-d", "", action="store", type="string", dest='destination',
-                      #help="destination") 
-    
-    #(options, args) = parser.parse_args(argvs)
-    
-    #if options.destination:
-        #install_dir = options.destination
-    #else:
-        #raise RuntimeError("the install directory was not specified")
-
-    #startdir = os.getcwd()
-    #os.makedirs(install_dir)
-    #path = os.path.abspath(install_dir)
-    #if options.findlinks:
-        #flinks = ' '.join(options.findlinks)
-    #else:
-        #flinks = ''
-    
-    #opt_dict = {
-        #'find-links': flinks,
-        #}
-
-    #try:
-        #bstrapfname = os.path.join(os.path.dirname(__file__), 
-                                   #'bootstrap.py')
-        #bconfigfname = os.path.join(os.path.dirname(__file__), 
-                                    #'buildout.cfg')
-        #os.chdir(path)
-        
-        #infile = open(bconfigfname, 'r')
-        #boconf_template = infile.read()
-        #infile.close()
-        
-        #boconf = boconf_template % opt_dict
-        
-        #outf = open(os.path.join(path, 'buildout.cfg'), 'w')
-        #outf.write(boconf)
-        #outf.close()
-        
-        ##shutil.copy(bconfigfname, os.path.join(path, 'buildout.cfg'))
-        #check_call([sys.executable, bstrapfname])
-        #build_cmd = [os.path.join('bin','buildout')]
-        #check_call(build_cmd)
-    #finally:
-        #os.chdir(startdir)
-
-
 def main():
     """
     Takes a file containing a list of requirements, one per line, and
@@ -69,6 +13,12 @@ def main():
     
     script_str = """
 
+def adjust_options(options, args):
+    if sys.version_info[:2] < (2,6) or sys.version_info[:2] >= (3,0):
+        print 'ERROR: python version must be >= 2.6 and <= 3.0. yours is %s'\
+	% sys.version.split(' ')[0]
+	sys.exit(-1)
+    
 def after_install(options, home_dir):
     global logger
     reqs = %(reqs)s
@@ -99,7 +49,7 @@ def after_install(options, home_dir):
             reqs.append(line)
     
     optdict = {'reqs':reqs, 'cmds':cmds}
-    print virtualenv.create_bootstrap_script(script_str % optdict, '2.6')
+    print virtualenv.create_bootstrap_script(script_str % optdict)
 
 
 if __name__ == '__main__':
