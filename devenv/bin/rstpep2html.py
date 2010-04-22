2c6de1d2561326096b32f0fc593f4b56db7a3ab3
Bret Naylor <bret@ubuntu>
2010-04-22 11:30:14 -0400

    removed temp dir (devenv) from repository

--- devenv/bin/rstpep2html.py
+++
@@ -1,5 +0,0 @@
-#!/OpenMDAO/dev/pziegfel/T53-fix_docs/devenv/bin/python
-# EASY-INSTALL-SCRIPT: 'docutils==0.6','rstpep2html.py'
-__requires__ = 'docutils==0.6'
-import pkg_resources
-pkg_resources.run_script('docutils==0.6', 'rstpep2html.py')
