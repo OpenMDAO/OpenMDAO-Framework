e2befef75680f675e81fe255e8e6010fc8dc7902
Justin Gray <Justin.S.Gray@nasa.gov>
2010-04-23 11:21:04 -0400

    working on gentic some more

--- openmdao.lib/src/openmdao/lib/drivers/genetic.py
+++ openmdao.lib/src/openmdao/lib/drivers/genetic.py
@@ -1,8 +1,9 @@
 """A simple Pyevolve-based driver for OpenMDAO"""
 
 import random
+import re
 
-from enthought.traits.api import Python
+from enthought.traits.api import Python,BaseEnum
 
 from pyevolve import G1DList, G1DBinaryString, G2DList, GAllele, GenomeBase
 from pyevolve import GSimpleGA, Selectors, Initializators, Mutators, Consts
@@ -15,7 +16,9 @@ except ImportError:
     logging.warning('No pyevolve.DBAdaptors available.')
 
 from openmdao.main.api import Driver, StringRef, StringRefArray, set_as_top, Component, Assembly
-from openmdao.lib.api import Float, Int, Array
+from openmdao.lib.api import Float, Int, Enum, Array
+
+array_test = re.compile("\[[0-9]+\]$")
 
 
 class Genetic(Driver):
@@ -43,22 +46,40 @@ class Genetic(Driver):
         path = ".".join(ref.split(".")[0:-1]) #get the path to the object
         target = ref.split(".")[-1] #get the last part of the string after the last "."
         
+        #bunch of logic to check for array elements being passed as refs
+        
+        
         obj = getattr(self.parent,path)
        
-        #TODO: check for None, throw error if no trait found
         t = obj.trait(target) #get the trait
-        
-        minimum = low or t.low or 0
-        maximum = high or t.high or 100
-        
-        print "TESTING"
-        if t.is_trait_type(Float) or (t.is_trait_type(Python) and isinstance(val,float)):
-            allele = GAllele.GAlleleRange(begin=minimum,end=maximum,real=True)
-            print "TEST"
+        if t: 
+            minimum = low or t.low or 0
+            maximum = high or t.high or 100
             
-        elif t.is_trait_type(Int):
-            allele = GAllele.GAlleleRange(begin=minimum,end=maximum,real=False)
- 
+            if t.is_trait_type(Float) or (t.is_trait_type(Python) and isinstance(val,float)):
+                allele = GAllele.GAlleleRange(begin=minimum,end=maximum,real=True)
+                self._allels.add(allele)
+            
+            elif t.is_trait_type(Int) or (t.is_trait_type(Python) and isinstance(val,int)):
+                allele = GAllele.GAlleleRange(begin=minimum,end=maximum,real=False)
+                self._allels.add(allele)                
+                
+            elif t.is_trait_type(Enum): 
+                print "TESTING"
+                print t.values()
+            
+            else: 
+                self.raise_exception("Improper design variable type. Must be Float,Int, or an element of an Array.",ValueError)
+        
+        elif array_test.search(target): 
+            maximum = high or 100
+            minimum = low or 0
+            
+            
+            
+        else:
+            self.raise_exception("Improper design variable type. Must be Float,Int or an element of an Array.",ValueError)
+        
         
     def execute(self):
         """Perform the optimization"""
@@ -70,13 +91,14 @@ if __name__ == "__main__":
     class Rosenbrock(Component):
         x = Float(3,low=-10,high=10,iotype="in")
         y = Int(low=-100,high=100,iotype="in")
+        z = Array(dtype=float, value=[1.0,2.0,3.0,4.0], iotype="in")
+        a = Enum(1,2,3,"test",iotype="in")
         result = Float(iotype = 'out')
         
         def __init__(self, doc=None):
             super(Rosenbrock, self).__init__(doc)
             self.x = 10
             self.y = 10
-            self.z=10.0
             self.result = (1-self.x**2) + 100*(self.y - self.y**2)**2
         
 
@@ -88,5 +110,5 @@ if __name__ == "__main__":
     top.add_container("rosen",Rosenbrock())
     top.add_container("genetic",Genetic())
     
-    top.genetic.add_des_var("rosen.z")
+    top.genetic.add_des_var("rosen.a")
     
