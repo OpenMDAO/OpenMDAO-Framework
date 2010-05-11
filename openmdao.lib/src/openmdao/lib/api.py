18da50bf721f61514de810e7033c8ef2dc6c5e9f
Kenneth Moore <Kenneth.T.Moore-1@nasa.gov>
2010-05-11 15:51:28 -0400

    Added ExternCode and Dict to openmdao.lib.api

--- openmdao.lib/src/openmdao/lib/api.py
+++ openmdao.lib/src/openmdao/lib/api.py
@@ -7,6 +7,7 @@ Pseudo package containing plugins from the OpenMDAO Standard Library.
 |    Bool
 |    CBool
 |    Complex
+|    Dict
 |    Enum
 |    File
 |    Float
@@ -20,6 +21,10 @@ Pseudo package containing plugins from the OpenMDAO Standard Library.
 |    CaseIteratorDriver
 |    CONMINdriver
 |    pyevolvedriver
+
+| *Components*
+|
+|    ExternalCode
 """
 
 # Traits that we've modified
@@ -30,10 +35,13 @@ from openmdao.lib.traits.int import Int
 
 # Traits from Enthought
 from enthought.traits.api import Array, Bool, List, Str, Instance, \
-                                 Complex, CBool
+                                 Complex, CBool, Dict
 
 # Drivers
 from openmdao.lib.drivers.conmindriver import CONMINdriver
 from openmdao.lib.drivers.caseiterdriver import CaseIteratorDriver
 from openmdao.lib.drivers.pyevolvedriver import pyevolvedriver
+
+# Components
+from openmdao.lib.components.external_code import ExternalCode
 
