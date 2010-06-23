8353cd83279c4b988a9bc0b85fe14ed4cd7a1bba
Bret Naylor <bnaylor@grc.nasa.gov>
2010-06-23 10:11:48 -0400

    added license info to optlh.py

--- openmdao.lib/src/openmdao/lib/caseiterators/optlh.py
+++ openmdao.lib/src/openmdao/lib/caseiterators/optlh.py
@@ -1,3 +1,22 @@
+# This implementation is based on a matlab implementation that has the
+# following license:
+#
+# Copyright 2007 A Sobester
+#
+# This program is free software: you can redistribute it and/or modify  it
+# under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or any
+# later version.
+# 
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser
+# General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License and GNU
+# Lesser General Public License along with this program. If not, see
+# <http://www.gnu.org/licenses/>.
+
 from __future__ import division
 
 import random
