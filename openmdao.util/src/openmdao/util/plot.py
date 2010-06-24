8d38ec3ae74718f0c61b02499ed0ec34973f2584
bret <bret@bret-lynx>
2010-06-23 22:29:45 -0400

    updated plotter for db data

--- openmdao.util/src/openmdao/util/plot.py
+++ openmdao.util/src/openmdao/util/plot.py
@@ -1,2 +1,84 @@
+"""
+Utility functions related to plotting data
+"""
 
-# fill this in with a function to plot data previously collected (as in a DBCaseRecorder)
+import sqlite3
+
+def get_data(dbname, varnames, case_sql=None, var_sql=None):
+    """
+    Retrieve the values of specified variables from a sqlite DB containing
+    Case data.
+    
+    Returns a dict containing a list of values for each entry, keyed on 
+    variable name.
+    
+    dbname : str
+        The name of the sqlite DB file
+        
+    varnames : list[str]
+        The list of names of variables to be plotted
+        
+    case_sql : str, optional
+        Optional SQL syntax that will be placed in the WHERE clause for Case retrieval
+        
+    var_sql : str, optional
+        Option SQL syntax that will be placed in the WHERE clause for variable retrieval
+    
+    """
+    connection = sqlite3.connect(dbname)
+    
+    sql = ["SELECT * FROM cases"]
+    if case_sql:
+        sql.append("WHERE %s" % case_sql)
+        
+    casecur = connection.cursor()
+    casecur.execute(' '.join(sql))
+    
+    sql = ["SELECT name, value, entry from casevars WHERE case_id=%s"]
+    for i,name in enumerate(varnames):
+        if i==0:
+            sql.append("AND (")
+        else:
+            sql.append("OR")
+        sql.append("name='%s'" % name)
+    if i>0: sql.append(")")
+    
+    if var_sql:
+        sql.append("AND %s" % var_sql)
+    combined = ' '.join(sql)
+    
+    varcur = connection.cursor()
+    
+    vardict = {}
+    for case_id,name,msg,retries,model_id,timeEnter in casecur:
+        varcur.execute(combined % case_id)
+        for vname, value, entry in varcur:
+            if entry:
+                vname = "vname%s" % entry
+            dval = vardict.setdefault(vname, [])
+            dval.append(value)
+            print vname,'=',value
+            
+    return vardict
+
+
+def get_lines(dbname, xnames, ynames, case_sql=None, var_sql=None): 
+    
+    vardict = get_data(dbname, xnames+ynames, case_sql, var_sql)
+
+    lines = []
+    yvals = []
+    xvals = []
+    for i,name in enumerate(ynames):
+        yvals.append(vardict[name])
+        if len(xnames) == 0:
+            xvals.append(range(len(vardict[name])))
+        elif len(xnames) == 1:
+            xvals.append(vardict[xnames[0]])
+        else:
+            xvals.append(vardict[xnames[i]])
+    
+    for xdata,ydata in zip(xvals, yvals):
+        lines.append((xdata, ydata))
+        
+    return lines
--- scripts/dbplot.py
+++ scripts/dbplot.py
@@ -1,6 +1,5 @@
 import sys
 import os
-import sqlite3
 
 import numpy as np
 from optparse import OptionParser
@@ -8,6 +7,7 @@ from optparse import OptionParser
 from enthought.traits.api import on_trait_change
 from openmdao.main.api import Component, ExprEvaluator
 from openmdao.lib.api import Str
+from openmdao.util.plot import get_lines
 
 if 'matplotlib' not in sys.modules:
     import matplotlib
@@ -18,7 +18,7 @@ if 'matplotlib' not in sys.modules:
       
 import matplotlib.pyplot as plt
 
-plt.ion()  # interactive mode
+#plt.ion()  # interactive mode
 
 _colors = [
     'b',
@@ -60,49 +60,8 @@ _styles = [
     '_'
     ]
 
-lines = []
-typeidx = 0
 
-
-def get_data(dbname, varnames, case_selector=None, var_selector=None):
-    connection = sqlite3.connect(dbname)
-    
-    sql = ["SELECT * FROM cases"]
-    if case_selector:
-        sql.append("WHERE %s" % case_selector)
-        
-    casecur = connection.cursor()
-    casecur.execute(' '.join(sql))
-    
-    sql = ["SELECT name, value, entry from casevars WHERE case_id=%s"]
-    for i,name in enumerate(varnames):
-        if i==0:
-            sql.append("AND (")
-        else:
-            sql.append("OR")
-        sql.append("name='%s'" % name)
-    if i>0: sql.append(")")
-    
-    if var_selector:
-        sql.append("AND %s" % var_selector)
-    combined = ' '.join(sql)
-    
-    varcur = connection.cursor()
-    
-    vardict = {}
-    for case_id,name,msg,retries,model_id,timeEnter in casecur:
-        varcur.execute(combined % case_id)
-        for vname, value, entry in varcur:
-            if entry:
-                vname = "vname%s" % entry
-            dval = vardict.setdefault(vname, [])
-            dval.append(value)
-            print vname,'=',value
-            
-    return vardict
-
-
-def displayXY(title=''):
+def displayXY(lines, title='', grid=False, xlabel='', ylabel=''):
 
     fig = plt.figure()
     fig.add_subplot(111)
@@ -110,28 +69,21 @@ def displayXY(title=''):
     for line in lines:
         args = []
         kwargs = {}
-        if line[0]:
-            args.append(line[2])
-        args.append(line[3])
-        if line[4]:
-            args.append(line[4])
-        if line[5]:
-            kwargs['label'] = '%s' % line[5]
-        else:
-            kwargs['label'] = '%s' % line[1]
+        args.append(line[0])
+        args.append(line[1])
+        #if line[5]:
+            #kwargs['label'] = '%s' % line[5]
+        #else:
+            #kwargs['label'] = '%s' % line[0]
         plt.plot(*args, **kwargs)
+    if grid:
+        plt.grid(True)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
     plt.legend()
     plt.title(title)
     plt.show()
 
-
-def add_line(x, xvals, y, yvals, line_type=None, label=None):
-    global lines, typeidx
-    if line_type is None:
-        line_type = '%s%s' % (_colors[typeidx],_styles[typeidx])
-        typeidx += 1
-    lines.append((x, y, xvals, yvals, line_type, label))
-        
 if __name__ == '__main__':
     parser = OptionParser()
     parser.add_option("-x", "", action="store", type="string", dest="xnames",
@@ -141,10 +93,21 @@ if __name__ == '__main__':
     parser.add_option("-d", "--dbfile", action="store", type="string", dest="dbname",
                       help="database filename")
     parser.add_option("-t", "--title", action="store", type="string", dest="title",
-                      help="plot title")
+                      help="plot title",)
+    parser.add_option("", "--xlabel", action="store", type="string", dest="xlabel",
+                      help="x axis label")
+    parser.add_option("", "--ylabel", action="store", type="string", dest="ylabel",
+                      help="y axis label")
+    parser.add_option("-g", "--grid", action="store_true", dest="grid",
+                      help="makes grid visible")
+    parser.add_option("", "--cases", action="store", type="string", dest="case_sql",
+                      help="sql syntax to select certain cases")
+    parser.add_option("", "--vars", action="store", type="string", dest="var_sql",
+                      help="sql syntax to select certain vars")
+
     (options, args) = parser.parse_args(sys.argv[1:])
     
-    if len(args) > 0 or not options.xnames or not options.ynames or not options.dbname:
+    if len(args) > 0 or not options.ynames or not options.dbname:
         parser.print_help()
         sys.exit(-1)
     
@@ -155,16 +118,7 @@ if __name__ == '__main__':
         print "Number of x variables doesn't match number of y variables."
         sys.exit(-1)
     
-    vdict = get_data(options.dbname, xs+ys)
+    displayXY(get_lines(options.dbname, xs, ys, options.case_sql, options.var_sql), 
+              title=options.title, grid=options.grid, xlabel=options.xlabel,
+              ylabel=options.ylabel)
     
-    for i,yname in enumerate(ys):
-        if len(xs) == 1:
-            xname = xs[0]
-        else:
-            xname = xs[i]
-        add_line(xname, yname, vdict[xname], vdict[yname])
-        
-    displayXY(title=options.title)
-    
-    #import pprint
-    #pprint.pprint(vdict)
