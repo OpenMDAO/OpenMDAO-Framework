4888bc435baf99a06c574dcb9ba339dd97b04227
Suresh Khandelwal <Suresh.C.Khandelwal@nasa.gov>
2010-06-30 15:01:35 -0400

    new modified code

---
+++ contrib/axod/src/testaxod.py
@@ -0,0 +1,33 @@
+# testaxod.py
+
+import sys
+import numpy
+import subprocess
+import shutil
+import os
+
+if '.' not in sys.path:
+    sys.path.append('.')
+
+
+from axod_comp import axod 
+
+print axod.__doc__
+print axod.axod.__doc__
+loop = 1
+while  loop < 3 :
+    print 'loop =',loop
+    n = loop % 2
+    if n == 0: shutil.copy('one_stage.inp','axod.inp')
+    if n == 1: shutil.copy('eee_hpt.inp','axod.inp')
+    (hpower, tott, totp, mflow, effs, errr) = axod.axod() 
+    print 'loop =',loop,'  After AXOD  in testaxod.py****'
+    axod1 = 'axod.out'+ str(loop)
+    #shutil.copy('axod.out',axod1)
+    shutil.move('axod.out',axod1)
+    loop = loop + 1
+
+print ' COMPLETE ********'
+print dir('axod')
+
+
