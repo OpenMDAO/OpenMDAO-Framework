36290475a0fe22ea9f17a635845001736131fa38
Damon Rousis <damon.rousis@nasa.gov>
2010-07-16 15:22:31 -0400

    created multi criteria EI calculations

--- openmdao.main/src/openmdao/main/uncertain_distributions.py
+++ openmdao.main/src/openmdao/main/uncertain_distributions.py
@@ -3,7 +3,7 @@ from enthought.traits.api import implements, HasTraits
 from openmdao.lib.traits.float import Float
 from openmdao.main.interfaces import IUncertainVariable
 
-from random import normalvariate
+from random import normalvariate,uniform
 
 class NormalDistribution(HasTraits): 
     """IUncertainVariable which represents a quantity with a normal distribution of uncertainty"""
@@ -25,4 +25,44 @@ class NormalDistribution(HasTraits):
     def expected(self): 
         return self.mu
         
+class UniformDistribution(HasTraits):
+    """IUncertainVariable which represents a quantity with a uniform distribution of uncertainty"""
     
+    max = Float(0,desc="maximum value of random variable")
+    min = Float(1,desc="minimum value of random vatiable")
+    
+    implements(IUncertainVariable)
+
+    def __init__(self,max=0,min=1,*args,**kwargs):
+        super(UniformDistribution,self).__init__(*args,**kwargs)
+        
+        self.max = max
+        self.min = min
+        
+    def sample(self):
+        return uniform(self.min,self.max)
+        
+    def expected(self):
+        return (self.max+self.min)/2.
+            
+class TriangularDistribution(hasTraits):
+    """IUncertainVariable which represents a quantity with a triangular distribution of uncertainty"""
+
+    max = Float(0,desc="maximum value of random variable")
+    min = Float(1,desc="maximum value of random variable")
+    mode = Float(0.5,desc="location of the most frequent value of the distribution")
+
+    implements(IUncertainVariable)
+    
+    def __init__(self,max=0,min=1,mode=0.5,*args,**kwargs):
+        super(TriangularDistribution,self).__init__(*args,**kwargs)
+        
+        self.max = max
+        self.min = min
+        self.mode = mode
+        
+    def sample(self):
+        return triangular(self.min,self.max,self.mode)
+        
+    def expected(self):
+        return (self.max+self.mode+self.min)/3.
