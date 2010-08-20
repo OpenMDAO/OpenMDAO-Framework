db66150c718bd01c69bd5cff6901c97ccc0fc912
Bret Naylor <bnaylor@grc.nasa.gov>
2010-08-20 15:23:58 -0400

    removed unnecessary import in kriging_surrogate.py

--- openmdao.lib/src/openmdao/lib/components/test/test_kriging_surrogate.py
+++ openmdao.lib/src/openmdao/lib/components/test/test_kriging_surrogate.py
@@ -8,7 +8,6 @@ import numpy.random as numpy_random
 
 from openmdao.lib.components.kriging_surrogate import KrigingSurrogate
 from openmdao.lib.caseiterators.listcaseiter import ListCaseIterator
-from openmdao.main.case import Case
 from openmdao.main.uncertain_distributions import NormalDistribution
 
 class KrigingSurrogateTests(unittest.TestCase):
