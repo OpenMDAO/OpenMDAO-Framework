2d95b55cfc7c747f815e734c2234c7b774be0d93
Damon Rousis <damon.rousis@nasa.gov>
2010-09-28 12:33:45 -0400

    added multi architecture EI

---
+++ examples/openmdao.examples.multiArch_multiEI/openmdao/examples/multiArch_multiEI/alg_component1.py
@@ -0,0 +1,14 @@
+from numpy import sin
+
+from openmdao.main.api import Component
+from openmdao.lib.api import Float
+
+class Alg_Component1(Component): 
+    x = Float(iotype="in",low=0.,high=1.0)
+    
+    f1 = Float(0.,iotype="out")
+    f2 = Float(0.,iotype="out")
+    
+    def execute(self):
+        self.f1 = (6.*self.x-2)**2*sin(12.*self.x-4.)
+        self.f2 = 0.5*self.f1+10.*(self.x-0.5)-5.
--- go-openmdao-dev.py
+++ go-openmdao-dev.py
@@ -1139,7 +1139,8 @@ openmdao_packages = ['openmdao.util',
                      'examples/openmdao.examples.enginedesign',
                      'examples/openmdao.examples.mdao',
                      'examples/openmdao.examples.singleEI',
-                     'examples/openmdao.examples.multiEI'
+                     'examples/openmdao.examples.multiEI',
+                     'examples/openmdao.examples.multiArch_multiEI'
                     ]
     
 def adjust_options(options, args):
