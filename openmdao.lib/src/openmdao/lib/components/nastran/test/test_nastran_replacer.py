9d3d7971926c3b25de17eabd592eb5eb2d280f47
Kenneth Moore <Kenneth.T.Moore-1@nasa.gov>
2010-09-28 11:50:47 -0400

    Fixed a small error in the NastranReplacer test.

--- openmdao.lib/src/openmdao/lib/components/nastran/test/test_nastran_replacer.py
+++ openmdao.lib/src/openmdao/lib/components/nastran/test/test_nastran_replacer.py
@@ -12,38 +12,38 @@ class TestNastranReplacer(unittest.TestCase):
     def go_single(self, text, variables):
         return self.go([text], variables)[0]
 
-    def easy_test(self, text, outcome, variables):
+    def easy(self, text, outcome, variables):
         t = self.go_single(text, variables)
         self.assertTrue(t == outcome)
 
     def test_no_changes(self):
         s = "hello, i'm andrew"
-        self.easy_test(s, s, {})
+        self.easy(s, s, {})
 
     def test_normal_replace(self):
         s = "my height: %h"
         t = "my heighHEIGHT  "
-        self.easy_test(s, t, {"h" : "HEIGHT"})
+        self.easy(s, t, {"h" : "HEIGHT"})
 
     def test_normal_replace_beginning(self):
         s = "%h blahhREAL DATA"
         t = "andrew  REAL DATA"
-        self.easy_test(s, t, {"h" : "andrew"})
+        self.easy(s, t, {"h" : "andrew"})
 
     def test_overwrite_variable(self):
         s = "METHOD %*n"
         t = "METHOD 103"
-        self.easy_test(s, t, {"*n" : 103})
+        self.easy(s, t, {"*n" : 103})
 
     def test_overwrite_variable_2(self):
         s = "METHOD %*n DONT REMOVE"
         t = "METHOD 103 DONT REMOVE"
-        self.easy_test(s, t, {"*n" : 103})
+        self.easy(s, t, {"*n" : 103})
 
     def test_overwrite_long_var(self):
         s = "METHOD %*method"
         t = "METHOD 103     "
-        self.easy_test(s, t, {"*method": 103})
+        self.easy(s, t, {"*method": 103})
 
 
 if __name__ == "__main__":
