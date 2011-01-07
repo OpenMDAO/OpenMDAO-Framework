04b923f579ac4f8bea5e862c8cb4e1978321bf11
Bret Naylor <bnaylor@grc.nasa.gov>
2011-01-07 15:00:05 -0500

    push_docs added

--- openmdao.devtools/src/openmdao/devtools/build_docs.py
+++ openmdao.devtools/src/openmdao/devtools/build_docs.py
@@ -11,6 +11,9 @@ from subprocess import Popen, PIPE, STDOUT
 from pkg_resources import Environment, WorkingSet, Requirement, working_set
 from optparse import OptionParser
 
+from fabric.api import run, env, local, put, cd, get, settings
+from fabric.state import connections
+
 import sphinx
 
 from openmdao.util.dumpdistmeta import get_dist_metadata
@@ -249,6 +252,38 @@ def view_docs(browser=None):
     wb = webbrowser.get(browser)
     wb.open(idxpath)
 
+def push_docs():
+    """A script (push_docs) points to this. It pushes the current copy of the docs up
+    to the development doc area on openmdao.org.
+    """
+    startdir = os.getcwd()
+    branchdir, docdir, bindir =_get_dirnames()
+    idxpath = os.path.join(docdir, '_build', 'html', 'index.html')
+    if not os.path.isfile(idxpath):
+        build_docs()
+
+    try:
+        os.path.chdir(os.path.join(docdir, '_build'))
+        try:
+            if os.path.exists('docs.tar.gz'):
+                os.remove('docs.tar.gz')
+            archive = tarfile.open('docs.tar.gz', 'w:gz')
+            archive.add('html')
+            archive.close()
+        finally:
+            os.chdir(startdir)
+        
+        with settings(host_string='openmdao@web103.webfaction.com'):
+            # tar up the docs so we can upload them to the server
+            # put the docs on the server and untar them
+            put(os.path.join(docdir,'_build','docs.tar.gz'), 'docs.tar.gz') 
+            run('tar xzf docs.tar.gz')
+            run('mv html dev_docs')
+            run('rm -f docs.tar.gz')
+    finally:
+        for key in connections.keys():
+            connections[key].close()
+            del connections[key]
 
 def test_docs():
     """Tests the openmdao sphinx documentation.  
