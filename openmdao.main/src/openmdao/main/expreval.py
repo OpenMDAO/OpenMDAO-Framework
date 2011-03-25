cd2ab84ea4227b3338ffb1147f1be2883b407a83
Bret Naylor <bnaylor@grc.nasa.gov>
2011-03-25 16:41:36 -0400

    moved ExprPrinter over to test_expreval.py

--- openmdao.main/src/openmdao/main/expreval.py
+++ openmdao.main/src/openmdao/main/expreval.py
@@ -36,237 +36,6 @@ def _get_attr_node(names):
         node = ast.Attribute(value=node, attr=name, ctx=ast.Load())
     return node
 
-
-# dict with operator precedence.  We need this because otherwise we can't
-# tell where to put parens when we print out an expression with mixed operators.  
-# We could just put them around every operation, but that's a little ugly...
-_op_preds = {}
-_prec = 0
-
-_op_preds[ast.Lambda] = _prec
-_prec += 1
-_op_preds[ast.If] = _prec
-_prec += 1
-_op_preds[ast.Or] = _prec
-_prec += 1
-_op_preds[ast.And] = _prec
-_prec += 1
-_op_preds[ast.Not] = _prec
-_prec += 1
-_op_preds[ast.In] = _prec
-_op_preds[ast.NotIn] = _prec
-_op_preds[ast.Is] = _prec
-_op_preds[ast.IsNot] = _prec
-_op_preds[ast.Lt] = _prec
-_op_preds[ast.LtE] = _prec
-_op_preds[ast.Gt] = _prec
-_op_preds[ast.GtE] = _prec
-_op_preds[ast.NotEq] = _prec
-_op_preds[ast.Eq] = _prec
-_prec += 1
-_op_preds[ast.BitOr] = _prec
-_prec += 1
-_op_preds[ast.BitXor] = _prec
-_prec += 1
-_op_preds[ast.BitAnd] = _prec
-_prec += 1
-_op_preds[ast.LShift] = _prec
-_op_preds[ast.RShift] = _prec
-_prec += 1
-_op_preds[ast.Add] = _prec
-_op_preds[ast.Sub] = _prec
-_prec += 1
-_op_preds[ast.Mult] = _prec
-_op_preds[ast.Div] = _prec
-_op_preds[ast.FloorDiv] = _prec
-_op_preds[ast.Mod] = _prec
-_prec += 1
-_op_preds[ast.UAdd] = _prec
-_op_preds[ast.USub] = _prec
-_op_preds[ast.Invert] = _prec
-_prec += 1
-_op_preds[ast.Pow] = _prec
-
-def _pred_cmp(op1, op2):
-    """Used to determine operator precedence."""
-    return _op_preds[op1.__class__] - _op_preds[op2.__class__]
-
-class ExprPrinter(ast.NodeVisitor):
-    """A NodeVisitor that gets the python text of an expression or assignment
-    statement defined by an AST.
-    """
-    def __init__(self):
-        super(ExprPrinter, self).__init__()
-        self.txtlist = []
-        
-    def write(self, txt):
-        self.txtlist.append(txt)
-        
-    def get_text(self):
-        return ''.join(self.txtlist)
-
-    def visit_Attribute(self, node):
-        self.visit(node.value)
-        self.write(".%s" % node.attr)
-        
-    def visit_Assign(self, node):
-        for i,t in enumerate(node.targets):
-            if i>0: self.write(',')
-            self.visit(t)
-        self.write(' = ')
-        self.visit(node.value)
-        
-    def visit_Name(self, node):
-        self.write(node.id)
-        
-    def visit_BinOp(self, node):
-        # we have to add parens around any immediate BinOp child
-        # that has a lower precedence operation than we do
-        if isinstance(node.left, ast.BinOp) and _pred_cmp(node.left.op, node.op) < 0:
-            self.write('(')
-            self.visit(node.left)
-            self.write(')')
-        else:
-            self.visit(node.left)
-        self.visit(node.op)
-        if isinstance(node.right, ast.BinOp) and _pred_cmp(node.right.op, node.op) < 0:
-            self.write('(')
-            self.visit(node.right)
-            self.write(')')
-        else:
-            self.visit(node.right)
-
-    def visit_IfExp(self, node):
-        self.visit(node.body)
-        self.write(' if ')
-        self.visit(node.test)
-        self.write(' else ')
-        self.visit(node.orelse)
-        
-    def visit_Call(self, node):
-        self.visit(node.func)
-        self.write('(')
-        total_args = 0
-        for arg in node.args:
-            if total_args>0: self.write(',')
-            self.visit(arg)
-            total_args += 1
-            
-        if hasattr(node, 'keywords'):
-            for kw in node.keywords:
-                if total_args>0: self.write(',')
-                self.visit(kw)
-                total_args += 1
-            
-        if hasattr(node, 'starargs'):
-            if node.starargs:
-                if total_args>0: self.write(',')
-                self.write('*%s'%node.starargs)
-                total_args += 1
-            
-        if hasattr(node, 'kwargs'):
-            if node.kwargs:
-                if total_args>0: self.write(',')
-                self.write('**%s'%node.kwargs)
-    
-        self.write(')')
-        
-    def visit_Num(self, node):
-        self.write(str(node.n))
-        
-    def visit_Str(self, node):
-        self.write("'%s'" % node.s)
-        
-    def visit_Index(self, node):
-        self.write('[')
-        self.visit(node.value)
-        self.write(']')
-
-    def visit_Slice(self, node):
-        self.write('[')
-        if node.lower is not None:
-            self.visit(node.lower)
-        self.write(':')
-        if node.upper is not None:
-            self.visit(node.upper)
-        self.write(':')
-        if node.step is not None:
-            self.visit(node.step)
-        self.write(']')
-        
-    def visit_List(self, node):  
-        self.write('[')
-        for i,e in enumerate(node.elts):
-            if i>0: self.write(',')
-            self.visit(e)
-        self.write(']')
-        
-    def visit_Dict(self, node):  
-        self.write('{')
-        for i,tup in enumerate(zip(node.keys,node.values)):
-            if i>0: self.write(',')
-            self.write("'%s':" % tup[0].s)
-            self.visit(tup[1])
-        self.write('}')
-        
-    def visit_Tuple(self, node): 
-        self.write('(')
-        length = len(node.elts)
-        for i,e in enumerate(node.elts):
-            if i>0: self.write(',')
-            self.visit(e)
-        if length==1: self.write(',')
-        self.write(')')
-        
-    def visit_USub(self, node): self.write('-')
-    def visit_UAdd(self, node): self.write('+')
-    def visit_And(self, node):  self.write(' and ')
-    def visit_Or(self, node):   self.write(' or ')
-        
-    # operators
-    def visit_Add(self, node):      self.write('+')
-    def visit_Sub(self, node):      self.write('-')
-    def visit_Mult(self, node):     self.write('*')
-    def visit_Div(self, node):      self.write('/')
-    def visit_Mod(self, node):      self.write('%')
-    def visit_Pow(self, node):      self.write('**')
-    def visit_LShift(self, node):   self.write('<<')
-    def visit_Rshift(self, node):   self.write('>>')
-    def visit_BitOr(self, node):    self.write('|')
-    def visit_BitXor(self, node):   self.write('^')
-    def visit_BitAnd(self, node):   self.write('&')
-    def visit_FloorDiv(self, node): self.write('//')
-        
-    # cmp operators
-    def visit_Eq(self, node):    self.write('==')
-    def visit_NotEq(self, node): self.write('!=')
-    def visit_Lt(self, node):    self.write('<')
-    def visit_LtE(self, node):   self.write('<=')
-    def visit_Gt(self, node):    self.write('>')
-    def visit_GtE(self, node):   self.write('>=')
-    def visit_Is(self, node):    self.write(' is ')
-    def visit_IsNot(self, node): self.write(' is not ')
-    def visit_In(self, node):    self.write(' in ')
-    def visit_NotIn(self, node): self.write(' not in ')
-    
-    def _ignore(self, node):
-        super(ExprPrinter, self).generic_visit(node)
-
-    visit_Module     = _ignore
-    visit_Expr       = _ignore
-    visit_Expression = _ignore
-    visit_Compare    = _ignore
-    visit_UnaryOp    = _ignore
-    visit_Subscript  = _ignore
-    visit_Load       = _ignore
-    visit_Store      = _ignore
-    
-    def generic_visit(self, node):
-        # We want to fail if we see any nodes we don't know about rather than
-        # generating code that isn't correct.
-        raise RuntimeError("ExprPrinter can't handle a node of type %s" % node.__class__.__name__)
-
-        
 class ExprTransformer(ast.NodeTransformer):
     """Transforms dotted name references, e.g., abc.d.g in an expression AST into 
     scope.get('abc.d.g') or scope.parent.get('abc.d.g'). Also turns assignments
@@ -365,8 +134,12 @@ class ExprTransformer(ast.NodeTransformer):
         if long_name is None: # this Attribute contains more than just names/attrs
             if subs is None:
                 subs = []
-            subs[0:0] = [ast.Tuple(elts=[ast.Num(n=ATTR),ast.Str(s=node.attr)], ctx=ast.Load())]
-            return self.visit(node.value, subs)
+            subs[0:0] = [ast.Tuple(elts=[ast.Num(n=ATTR),ast.Str(s=node.attr)], 
+                                   ctx=ast.Load())]
+            newnode = self.visit(node.value, subs)
+            if newnode is node.value:
+                return node
+            return newnode
         return self._name_to_node(node, long_name, subs)
 
     def _get_slice_vals(self, node):
@@ -402,22 +175,8 @@ class ExprTransformer(ast.NodeTransformer):
     def visit_Call(self, node, subs=None):
         name = self._get_long_name(node.func)
         if name is not None:
-            if self.expreval._is_local(name):
+            if self.expreval._is_local(name) or '.' not in name:
                 return self.generic_visit(node)
-            elif isinstance(node.func, ast.Name):
-                self._stack.append(node)
-                kwargs = {
-                    'func': _get_attr_node(['scope',node.func.id]),
-                    'args': [self.visit(arg) for arg in node.args],
-                    'keywords': [(kw.arg,self.visit(kw.value)) for kw in node.keywords],
-                    'ctx': ast.Load(),
-                    }
-                self._stack.pop()
-                if hasattr(node, 'kwargs') and node.kwargs:
-                    kwargs['kwargs'] = node.kwargs
-                if hasattr(node, 'starargs') and node.starargs:
-                    kwargs['starargs'] = node.starargs
-                return ast.copy_location(ast.Call(**kwargs), node)
         
         if subs is None:
             subs = []
@@ -502,15 +261,6 @@ class ExprEvaluator(object):
     def text(self, value):
         self._parse_needed = True
         self._text = value
-
-    @property
-    def transformed_text(self):
-        """The transformed expression string"""
-        new_ast = ExprTransformer(self).visit(self._pre_parse())
-        
-        ep = ExprPrinter()
-        ep.visit(new_ast)
-        return ep.get_text()
 
     @property
     def scope(self):
@@ -604,13 +354,12 @@ class ExprEvaluator(object):
             assign_txt = "%s=_local_setter" % self.text
             root = ast.parse(assign_txt, mode='exec')
             ## transform into a 'set' call to set the specified variable
-            new_ast = ExprTransformer(self).visit(root)
-            #if isinstance(new_ast.body[0], ast.Call):
-            #    new_ast.body = [ast.Expr(value=new_ast.body[0])]
-            ast.fix_missing_locations(new_ast)
-            self._assignment_code = compile(new_ast,'<string>','exec')
+            assign_ast = ExprTransformer(self).visit(root)
+            ast.fix_missing_locations(assign_ast)
+            self._assignment_code = compile(assign_ast,'<string>','exec')
             
         self._parse_needed = False
+        return new_ast
                 
     def _get_updated_scope(self, scope):
         oldscope = self.scope
--- openmdao.main/src/openmdao/main/test/test_evalexpr.py
+++ openmdao.main/src/openmdao/main/test/test_evalexpr.py
@@ -1,5 +1,6 @@
 import unittest
 import math
+import ast
 
 import numpy
 
@@ -41,6 +42,236 @@ class Comp(Component):
     def get_attr(self, name):
         return getattr(self, name)
     
+
+# dict with operator precedence.  We need this because otherwise we can't
+# tell where to put parens when we print out an expression with mixed operators.  
+# We could just put them around every operation, but that's a little ugly...
+_op_preds = {}
+_prec = 0
+
+_op_preds[ast.Lambda] = _prec
+_prec += 1
+_op_preds[ast.If] = _prec
+_prec += 1
+_op_preds[ast.Or] = _prec
+_prec += 1
+_op_preds[ast.And] = _prec
+_prec += 1
+_op_preds[ast.Not] = _prec
+_prec += 1
+_op_preds[ast.In] = _prec
+_op_preds[ast.NotIn] = _prec
+_op_preds[ast.Is] = _prec
+_op_preds[ast.IsNot] = _prec
+_op_preds[ast.Lt] = _prec
+_op_preds[ast.LtE] = _prec
+_op_preds[ast.Gt] = _prec
+_op_preds[ast.GtE] = _prec
+_op_preds[ast.NotEq] = _prec
+_op_preds[ast.Eq] = _prec
+_prec += 1
+_op_preds[ast.BitOr] = _prec
+_prec += 1
+_op_preds[ast.BitXor] = _prec
+_prec += 1
+_op_preds[ast.BitAnd] = _prec
+_prec += 1
+_op_preds[ast.LShift] = _prec
+_op_preds[ast.RShift] = _prec
+_prec += 1
+_op_preds[ast.Add] = _prec
+_op_preds[ast.Sub] = _prec
+_prec += 1
+_op_preds[ast.Mult] = _prec
+_op_preds[ast.Div] = _prec
+_op_preds[ast.FloorDiv] = _prec
+_op_preds[ast.Mod] = _prec
+_prec += 1
+_op_preds[ast.UAdd] = _prec
+_op_preds[ast.USub] = _prec
+_op_preds[ast.Invert] = _prec
+_prec += 1
+_op_preds[ast.Pow] = _prec
+
+def _pred_cmp(op1, op2):
+    """Used to determine operator precedence."""
+    return _op_preds[op1.__class__] - _op_preds[op2.__class__]
+
+class ExprPrinter(ast.NodeVisitor):
+    """A NodeVisitor that gets the python text of an expression or assignment
+    statement defined by an AST.
+    """
+    def __init__(self):
+        super(ExprPrinter, self).__init__()
+        self.txtlist = []
+        
+    def write(self, txt):
+        self.txtlist.append(txt)
+        
+    def get_text(self):
+        return ''.join(self.txtlist)
+
+    def visit_Attribute(self, node):
+        self.visit(node.value)
+        self.write(".%s" % node.attr)
+        
+    def visit_Assign(self, node):
+        for i,t in enumerate(node.targets):
+            if i>0: self.write(',')
+            self.visit(t)
+        self.write(' = ')
+        self.visit(node.value)
+        
+    def visit_Name(self, node):
+        self.write(node.id)
+        
+    def visit_BinOp(self, node):
+        # we have to add parens around any immediate BinOp child
+        # that has a lower precedence operation than we do
+        if isinstance(node.left, ast.BinOp) and _pred_cmp(node.left.op, node.op) < 0:
+            self.write('(')
+            self.visit(node.left)
+            self.write(')')
+        else:
+            self.visit(node.left)
+        self.visit(node.op)
+        if isinstance(node.right, ast.BinOp) and _pred_cmp(node.right.op, node.op) < 0:
+            self.write('(')
+            self.visit(node.right)
+            self.write(')')
+        else:
+            self.visit(node.right)
+
+    def visit_IfExp(self, node):
+        self.visit(node.body)
+        self.write(' if ')
+        self.visit(node.test)
+        self.write(' else ')
+        self.visit(node.orelse)
+        
+    def visit_Call(self, node):
+        self.visit(node.func)
+        self.write('(')
+        total_args = 0
+        for arg in node.args:
+            if total_args>0: self.write(',')
+            self.visit(arg)
+            total_args += 1
+            
+        if hasattr(node, 'keywords'):
+            for kw in node.keywords:
+                if total_args>0: self.write(',')
+                self.visit(kw)
+                total_args += 1
+            
+        if hasattr(node, 'starargs'):
+            if node.starargs:
+                if total_args>0: self.write(',')
+                self.write('*%s'%node.starargs)
+                total_args += 1
+            
+        if hasattr(node, 'kwargs'):
+            if node.kwargs:
+                if total_args>0: self.write(',')
+                self.write('**%s'%node.kwargs)
+    
+        self.write(')')
+        
+    def visit_Num(self, node):
+        self.write(str(node.n))
+        
+    def visit_Str(self, node):
+        self.write("'%s'" % node.s)
+        
+    def visit_Index(self, node):
+        self.write('[')
+        self.visit(node.value)
+        self.write(']')
+
+    def visit_Slice(self, node):
+        self.write('[')
+        if node.lower is not None:
+            self.visit(node.lower)
+        self.write(':')
+        if node.upper is not None:
+            self.visit(node.upper)
+        self.write(':')
+        if node.step is not None:
+            self.visit(node.step)
+        self.write(']')
+        
+    def visit_List(self, node):  
+        self.write('[')
+        for i,e in enumerate(node.elts):
+            if i>0: self.write(',')
+            self.visit(e)
+        self.write(']')
+        
+    def visit_Dict(self, node):  
+        self.write('{')
+        for i,tup in enumerate(zip(node.keys,node.values)):
+            if i>0: self.write(',')
+            self.write("'%s':" % tup[0].s)
+            self.visit(tup[1])
+        self.write('}')
+        
+    def visit_Tuple(self, node): 
+        self.write('(')
+        length = len(node.elts)
+        for i,e in enumerate(node.elts):
+            if i>0: self.write(',')
+            self.visit(e)
+        if length==1: self.write(',')
+        self.write(')')
+        
+    def visit_USub(self, node): self.write('-')
+    def visit_UAdd(self, node): self.write('+')
+    def visit_And(self, node):  self.write(' and ')
+    def visit_Or(self, node):   self.write(' or ')
+        
+    # operators
+    def visit_Add(self, node):      self.write('+')
+    def visit_Sub(self, node):      self.write('-')
+    def visit_Mult(self, node):     self.write('*')
+    def visit_Div(self, node):      self.write('/')
+    def visit_Mod(self, node):      self.write('%')
+    def visit_Pow(self, node):      self.write('**')
+    def visit_LShift(self, node):   self.write('<<')
+    def visit_Rshift(self, node):   self.write('>>')
+    def visit_BitOr(self, node):    self.write('|')
+    def visit_BitXor(self, node):   self.write('^')
+    def visit_BitAnd(self, node):   self.write('&')
+    def visit_FloorDiv(self, node): self.write('//')
+        
+    # cmp operators
+    def visit_Eq(self, node):    self.write('==')
+    def visit_NotEq(self, node): self.write('!=')
+    def visit_Lt(self, node):    self.write('<')
+    def visit_LtE(self, node):   self.write('<=')
+    def visit_Gt(self, node):    self.write('>')
+    def visit_GtE(self, node):   self.write('>=')
+    def visit_Is(self, node):    self.write(' is ')
+    def visit_IsNot(self, node): self.write(' is not ')
+    def visit_In(self, node):    self.write(' in ')
+    def visit_NotIn(self, node): self.write(' not in ')
+    
+    def _ignore(self, node):
+        super(ExprPrinter, self).generic_visit(node)
+
+    visit_Module     = _ignore
+    visit_Expr       = _ignore
+    visit_Expression = _ignore
+    visit_Compare    = _ignore
+    visit_UnaryOp    = _ignore
+    visit_Subscript  = _ignore
+    visit_Load       = _ignore
+    visit_Store      = _ignore
+    
+    def generic_visit(self, node):
+        # We want to fail if we see any nodes we don't know about rather than
+        # generating code that isn't correct.
+        raise RuntimeError("ExprPrinter can't handle a node of type %s" % node.__class__.__name__)
+
 class ExprEvalTestCase(unittest.TestCase):
     def setUp(self):
         self.top = set_as_top(Assembly())
@@ -50,12 +281,16 @@ class ExprEvalTestCase(unittest.TestCase):
         self.top.comp.x = 3.14
         self.top.comp.y = 42.
 
+    def _ast_to_text(self, node):
+        ep = ExprPrinter()
+        ep.visit(node)
+        return ep.get_text()
+        
     def _do_tests(self, tests, top):
         # each test is a tuple of the form (input, expected output)
         for tst in tests:
             ex = ExprEvaluator(tst[0], top)
-            ex._parse()
-            self.assertEqual(ex.transformed_text, tst[1])
+            self.assertEqual(self._ast_to_text(ex._parse()), tst[1])
         
     def test_simple(self):
         tests = [
@@ -114,6 +349,9 @@ class ExprEvalTestCase(unittest.TestCase):
             ('a.b[2] = -comp.x',
              "scope.parent.set('a.b',-scope.parent.get('comp.x'),[(0,2)])"),
             ('a1d[foo]', "scope.a1d[scope.parent.get('foo')]"),
+            ('a1d[1].value', "scope.a1d[1].value"),
+            ('a1d(2).value', "scope.a1d(2).value"),
+            ('a.a1d(2).value', "scope.parent.get('a.a1d',[(2,[2]),(1,'value')])"),
         ]
 
         self._do_tests(tests, self.top.a)
@@ -166,26 +404,31 @@ class ExprEvalTestCase(unittest.TestCase):
         ex = ExprEvaluator("comp.get_attr('get_cont')(1).a1d", self.top)
         self.assertTrue(all(ex.evaluate() == numpy.array([4,4,4,123,4])))
         
+        # try an expression that's a simple assignment
+        ex = ExprEvaluator("f = 10.333", self.top.a)
+        self.assertEqual(ex.evaluate(), None)
+        self.assertEqual(self.top.a.f, 10.333)
+        
     def test_reparse_on_scope_change(self):
         self.top.comp.x = 99.5
         self.top.comp.y = -3.14
         
         ex = ExprEvaluator('comp.x', self.top)
         self.assertEqual(99.5, ex.evaluate())
-        self.assertEqual(ex.transformed_text, "scope.get('comp.x')")
+        self.assertEqual(self._ast_to_text(ex._parse()), "scope.get('comp.x')")
         
         ex.scope = self.top.a
         ex.set(0.5)
-        self.assertEqual(ex.transformed_text, "scope.parent.get('comp.x')")
+        self.assertEqual(self._ast_to_text(ex._parse()), "scope.parent.get('comp.x')")
         self.assertEqual(0.5, self.top.comp.x)
         self.assertEqual(0.5, ex.evaluate(self.top)) # set scope back to self.top
-        self.assertEqual(ex.transformed_text, "scope.get('comp.x')")
+        self.assertEqual(self._ast_to_text(ex._parse()), "scope.get('comp.x')")
         
         ex.text = 'comp.y'
         self.assertEqual(-3.14, ex.evaluate(self.top.a))
         ex.set(11.1)
         self.assertEqual(11.1, self.top.comp.y)
-        self.assertEqual(ex.transformed_text, "scope.parent.get('comp.y')")
+        self.assertEqual(self._ast_to_text(ex._parse()), "scope.parent.get('comp.y')")
         
     def test_no_scope(self):
         ex = ExprEvaluator('abs(-3)+int(2.3)+math.floor(5.4)')
