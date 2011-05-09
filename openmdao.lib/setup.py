9f5fc1447544296f159ce7f30d1805017882aa3c
Justin Gray <justin.s.gray@nasa.gov>
2011-05-09 11:30:27 -0400

    removing prob_intersect from api and setup.py

--- openmdao.lib/setup.py
+++ openmdao.lib/setup.py
@@ -68,7 +68,6 @@ Component, Driver, and Variable plugins for OpenMDAO
       openmdao.lib.components.mux.Mux = openmdao.lib.components.mux:Mux
       openmdao.lib.components.mux.DeMux = openmdao.lib.components.mux:DeMux
       openmdao.lib.components.pareto_filter.ParetoFilter = openmdao.lib.components.pareto_filter:ParetoFilter
-      openmdao.lib.components.prob_intersect.ProbIntersect = openmdao.lib.components.prob_intersect:ProbIntersect
       openmdao.lib.components.nastran.nastran.NastranComponent = openmdao.lib.components.nastran.nastran:NastranComponent
 
       [openmdao.differentiator]
--- openmdao.lib/src/openmdao/lib/components/api.py
+++ openmdao.lib/src/openmdao/lib/components/api.py
@@ -3,7 +3,6 @@ OpenMDAO components standard library."""
 
 from openmdao.lib.components.external_code import ExternalCode
 from openmdao.lib.components.metamodel import MetaModel
-from openmdao.lib.components.prob_intersect import ProbIntersect
 from openmdao.lib.components.pareto_filter import ParetoFilter
 from openmdao.lib.components.expected_improvement import ExpectedImprovement
 from openmdao.lib.components.expected_improvement_multiobj import MultiObjExpectedImprovement
