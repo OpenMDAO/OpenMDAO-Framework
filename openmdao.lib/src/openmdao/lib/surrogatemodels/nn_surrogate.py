16a9f5a381cecb87a8250736cc7431eda7437642
Caitlin Kavanaugh <cnkavanaugh@gmail.com>
2011-08-09 07:44:40 -0400

    nn_surr starts

--- openmdao.lib/src/openmdao/lib/surrogatemodels/nn_surrogate.py
+++ openmdao.lib/src/openmdao/lib/surrogatemodels/nn_surrogate.py
@@ -1,25 +1,11 @@
-from pybrain.structure import FeedForwardNetwork
-from pybrain.structure import LinearLayer, SigmoidLayer
-from pybrain.structure import FullConnection
+from pybrain.tools.shortcuts import buildNetwork
+from pybrain.supervised.trainers import BackpropTrainer
+from pybrain.datasets import ClassificationDataSet
 
 from pylab import ion, ioff, figure, draw, contourf, clf, show, hold, plot
 from scipy import diag, arange, meshgrid, where
 from numpy.random import multivariate_normal
 
-nn_surr = FeedForwardNetwork()
+nn_surr = buildNetwork(2,3,1)
 
-inLayer = LinearLayer(2)
-hiddenLayer = SigmoidLayer(3)
-outLayer = LinearLayer(1)
-
-nn_surr.addInputModule(inLayer)
-nn_surr.addModule(hiddenLayer)
-nn_surr.addOutputModule(outLayer)
-
-in_to_hidden = FullConnection(inLayer, hiddenLayer)
-hidden_to_out = FullConnection(hiddenLayer, outLayer)
-
-nn_surr.addConnection(in_to_hidden)
-nn_surr.addConnection(hidden_to_out)
-
-nn_surr.sortModules()
+trainer = BackpropTrainer(nn_surr, )
