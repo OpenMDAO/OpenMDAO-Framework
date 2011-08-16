31475fca4afe558b7c50dfa1c6ad193644bfd395
Caitlin Kavanaugh <cnkavanaugh@gmail.com>
2011-08-16 08:33:12 -0400

    switch to ffnet - no plotting

--- openmdao.lib/src/openmdao/lib/surrogatemodels/nn_surrogate.py
+++ openmdao.lib/src/openmdao/lib/surrogatemodels/nn_surrogate.py
@@ -4,6 +4,9 @@ from pybrain.tools.shortcuts import buildNetwork
 from pybrain.supervised.trainers import BackpropTrainer
 from pybrain.datasets import SupervisedDataSet
 
+from ffnet import ffnet,mlgraph
+
+
 from openmdao.main.interfaces import implements, ISurrogate
 
 class NeuralNet(object):
@@ -20,51 +23,24 @@ class NeuralNet(object):
         return value
     
     def train(self, X, Y):
-        X = array(X)
-        Y = array(Y)
-        n_inputs = len(X[0])
+        inp = array(X)
+        self.targ = array(Y)
+        n_inputs = len(inp[0])
+        
         # 1 Output node because Surrogate Model has only 1 output
-        self._nn_surr = buildNetwork(n_inputs, self.n_hidden_nodes, 1)
-                
-        #Scaling of inputs down to between .1 and .9, and outputs between -.9 and .9
-        S_in=X.copy()
-        self.M_in = []
-        self.B_in = []
-        for i,row in enumerate(X.T):
-            in_min = np.min(row)
-            in_max = np.max(row)
-            self.M_in.append(.8/(in_max-in_min))
-            self.B_in.append(.1-.8/(in_max-in_min)*in_min)
-            S_in[:,i] = self.M_in[-1]*row+self.B_in[-1]
-                
-        out_min = np.min(Y)
-        out_max = np.max(Y)
-        self.m_out = 1.8/(out_max-out_min)
-        self.b_out = -.9-(1.8/(out_max-out_min))*out_min
-        S_out = self.m_out*Y+self.b_out
+        self._nn_surr = ffnet(mlgraph((n_inputs, self.n_hidden_nodes, 1)))
                         
-        # Creating the Dataset
-        ds = SupervisedDataSet(n_inputs,1)
-        for inp,target in zip(S_in,S_out):
-            ds.addSample(inp,(target,))
-            
-        # Set the type of trainer
-        trainer = BackpropTrainer(self._nn_surr, ds, momentum = .1)
         # Start the training
-        trainer.trainUntilConvergence()
+        self._nn_surr.train_momentum(inp, self.targ, momentum = .1)
                 
     def predict(self, X):
         
-        S_in_p=[]
-        for m,b,row in zip(self.M_in,self.B_in,X.T):
-            S_in_p.append(m*row+b)
-        
-        out=self._nn_surr.activate(S_in_p)
-        return ((out-self.b_out)/self.m_out)[0]
+        output, regression = self._nn_surr.test(X, self.targ, iprint = 2)
+        return output,regression
   
 if __name__ =="__main__":     
     import numpy as np    
-    x = np.linspace(3, 5, 25)
+    x = np.linspace(0, 5, 25)
     y = np.sin(x) * 4
     
     size = len(x)
@@ -76,5 +52,4 @@ if __name__ =="__main__":
     
     nn.train(inp,y)
     
-    for x,tar in zip(inp,y):    
-        print nn.predict(x),tar
+    print nn.predict(x)
