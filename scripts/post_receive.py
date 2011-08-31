f81110498aab6afe7aceecd56fbacc3fb880729e
Bret Naylor <naylor.b@gmail.com>
2011-08-31 11:04:53 -0400

    updating post_receive app

--- scripts/post_receive.py
+++ scripts/post_receive.py
@@ -17,16 +17,39 @@ from Queue import Queue
 
 import web
 
+import model
+
 #REPO_URL = 'https://github.com/OpenMDAO/OpenMDAO-Framework'
 REPO_URL = 'https://github.com/naylor-b/OpenMDAO-Framework'
-REPO_DIR = '/home/openmdao/install/OpenMDAO-Framework'
+LOCAL_REPO_DIR = '/home/openmdao/install/OpenMDAO-Framework'
+APP_DIR = '/home/openmdao/webapps/custom_app'
+APP_URL = 'http://openmdao.org/p_r'
 REPO_BRANCHES = ['foo']
-REMOTE_NAME = 'bret'
-RESULTS_DIR = os.path.join(REPO_DIR,'devenv','host_results')
+REMOTE_NAME = 'bret'  # name of a remote in local repo pointing to remote repo
+RESULTS_DIR = os.path.join(APP_DIR, 'host_results')
+FROM_EMAIL = 'openmdao@web103.webfaction.com'
 RESULTS_EMAILS = ['naylor.b@gmail.com']
 PY = 'python2.6'
 HOSTS = ['meerkat32_instance']
 TEST_ARGS = ['--', '-v', 'openmdao.util.test.test_namelist']
+
+commit_queue = Queue()
+
+
+### Url mappings
+
+urls = (
+    '/', 'Index',
+    '/run', 'Run',
+    '/view/(\d+)', 'View',
+    '/delete/(\d+)', 'Delete',
+)
+
+### Templates
+t_globals = globals()
+
+render = web.template.render(os.path.join(APP_DIR,'templates'), 
+                             base='base', globals=t_globals)
 
 
 def _has_checkouts():
@@ -58,14 +81,15 @@ def activate_and_run(envdir, cmd):
         command = ['source ./bin/activate', '&&'] + cmd
     
     # activate the environment and run command
-    os.chdir(envdir)
-    print("running %s from %s" % (' '.join(command), envdir))
+    
+    print("running %s in %s" % (' '.join(command), envdir))
     env = os.environ.copy()
     for name in ['VIRTUAL_ENV','_OLD_VIRTUAL_PATH','_OLD_VIRTUAL_PROMPT']:
         if name in env: 
             del env[name]
 
-    return _run_sub(' '.join(command), env=env, shell=True)
+    return _run_sub(' '.join(command), env=env, shell=True, cwd=envdir)
+
 
 def _run_sub(cmd, **kwargs):
     p = subprocess.Popen(cmd, stdout=subprocess.PIPE,
@@ -73,21 +97,44 @@ def _run_sub(cmd, **kwargs):
     output = p.communicate()[0]
     return (output, p.returncode)
 
+
 def do_tests(q):
     while True:
         payload = q.get(block=True)
-        print 'got a commit!!!!'
         try:
             test_commit(payload)
         except Exception as err:
             print str(err)
         
+
 def send_mail(commit_id, retval, msg):
     status = 'succeeded' if retval == 0 else 'failed'
-    web.sendmail('openmdao@web103.webfaction.com',
-                 RESULTS_EMAILS,
+    web.sendmail(FROM_EMAIL, RESULTS_EMAILS,
                  'test %s for commit %s' % (status, commit_id),
                  msg)
+
+def set_branch(branch, commit_id):
+    """Set the local repo to the specified branch as long as the local
+    repo is clean, and then pull the latest changes from the remote
+    branch.
+    """
+    if _has_checkouts():
+        send_mail(commit_id, -1, 'branch %s is not clean!' % branch)
+        return
+    
+    out, ret = _run_sub('git checkout %s' % branch, shell=True)
+    print out
+    if ret != 0:
+        send_mail(commit_id, ret, out)
+        return
+    
+    out, ret = _run_sub('git pull %s %s' % (REMOTE_NAME,
+                                            branch), shell=True)
+    print out
+    if ret != 0:
+        send_mail(commit_id, ret, out)
+        return
+
 
 def test_commit(payload):
         pprint.pprint(payload)
@@ -107,22 +154,7 @@ def test_commit(payload):
                                                                    REPO_BRANCHES)
             return
         
-        if _has_checkouts():
-            send_mail(commit_id, -1, 'branch %s is not clean!' % branch)
-            return
-        
-        out, ret = _run_sub('git checkout %s' % branch, shell=True)
-        print out
-        if ret != 0:
-            send_mail(commit_id, ret, out)
-            return
-        
-        out, ret = _run_sub('git pull %s %s' % (REMOTE_NAME,
-                                                branch), shell=True)
-        print out
-        if ret != 0:
-            send_mail(commit_id, ret, out)
-            return
+        set_branch(branch, commit_id)
     
         tmp_results_dir = os.path.join(RESULTS_DIR, commit_id)
         
@@ -136,57 +168,69 @@ def test_commit(payload):
         
         os.makedirs(tmp_results_dir)
         try:
-            out, ret = activate_and_run(os.path.join(REPO_DIR,'devenv'), cmd)
+            out, ret = activate_and_run(os.path.join(LOCAL_REPO_DIR,'devenv'), cmd)
             print out
-            if ret == 0:
-                send_mail(commit_id, ret, out)
-                shutil.rmtree(tmp_results_dir)
-            else:
-                send_mail(commit_id, ret, collect_results(tmp_results_dir))
+            process_results(commit_id, ret, tmp_results_dir, out)
         except Exception as err:
-            send_mail(commit_id, -1, str(err))
+            process_results(commit_id, -1, tmp_results_dir, str(err))
+        finally:
+            shutil.rmtree(tmp_results_dir)
 
+def process_results(commit_id, returncode, results_dir, output):
+    msg = "\n\nFull test results can be found here: %s" % os.path.join(APP_URL,
+                                                                       'view',
+                                                                       commit_id)
+    results = collect_results(results_dir)
+    model.new_test(commit_id, results)
+    send_mail(commit_id, returncode, output+msg)
 
-def collect_results(tmp_results_dir):
-    print 'collecting results from %s' % tmp_results_dir
+def collect_results(results_dir):
+    print 'collecting results from %s' % results_dir
     results = StringIO.StringIO()
-    for d in os.listdir(tmp_results_dir):
-        with open(os.path.join(tmp_results_dir, d, 'run.out'), 'r') as f:
+    for d in os.listdir(results_dir):
+        with open(os.path.join(results_dir, d, 'run.out'), 'r') as f:
             s = f.read()
             results.write(s)
             results.write('\n---------------------------------\n')
     return results.getvalue()
 
 
-class TestRunner:
-    def __init__(self, q):
-        self.q = q
+class Index:
 
-    #def GET(self):
-        #i = web.input(name = 'web')
-        #return 'Hello, ' + web.websafe(i.name) + '!'
+    def GET(self):
+        """ Show test index """
+        tests = model.get_tests()
+        return render.index(tests)
+
+class View:
+
+    def GET(self, commit_id):
+        """ View results for a single commit """
+        test = model.get_test(commit_id)
+        return render.view(test)
+
+class Delete:
+
+    def POST(self, commit_id):
+        """ Delete results for a commit """
+        model.delete_test(commit_id)
+        raise web.seeother('/')
+
+class Run:
 
     def POST(self):
         data = web.input('payload')
         payload = json.loads(data.payload)
-        self.q.put(payload)
+        commit_queue.put(payload)
         
-    # this is just a kludge to let me use an instance instead of a class
-    # with web.py
-    def __call__(self):
-        return self
 
 if __name__ == "__main__":
-    os.chdir(os.path.join(REPO_DIR,'devenv'))
-    
-    urls = ('/', 'runtests')
-    q = Queue()
-    runtests = TestRunner(q)
-    tester = Thread(target=do_tests, name='tester', args=(q,))
+    tester = Thread(target=do_tests, name='tester', args=(commit_queue,))
     tester.daemon = True
     print 'starting tester thread'
     tester.start()
-    app = web.application(urls, {'runtests': runtests})
+    
+    app = web.application(urls, globals())
     print 'running app'
     app.run()
 
