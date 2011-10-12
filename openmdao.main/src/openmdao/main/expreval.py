4880437cc04ca346f098134f942b7296ff457ee0
naylor-b <naylor.b@gmail.com>
2011-10-12 11:16:53 -0400

    added numpy and scipy functions to ExprEvaluator

--- openmdao.main/src/openmdao/main/expreval.py
+++ openmdao.main/src/openmdao/main/expreval.py
@@ -20,6 +20,21 @@ _expr_dict = {
 for name in dir(math):
     if not name.startswith('_'):
         _expr_dict[name] = getattr(math, name)
+        
+# make numpy and scipy functions available if possible
+try:
+    import numpy
+except ImportError:
+    pass
+else:
+    _expr_dict['numpy'] = numpy
+    
+try:
+    import scipy
+except ImportError:
+    pass
+else:
+    _expr_dict['scipy'] = scipy
 
 _Missing = object()
 
