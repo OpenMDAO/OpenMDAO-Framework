a6b46fdcbde92d9f9458700d2b2296d586b0de54
Tristan Hearn <tristan.a.hearn@nasa.gov>
2011-10-17 17:26:31 -0400

    added neighborhood doe driver

--- openmdao.lib/src/openmdao/lib/drivers/doedriver.py
+++ openmdao.lib/src/openmdao/lib/drivers/doedriver.py
@@ -4,7 +4,7 @@
 """
 
 # pylint: disable-msg=E0611,F0401
-from openmdao.lib.datatypes.api import ListStr, Slot
+from openmdao.lib.datatypes.api import ListStr, Slot,Float
 
 from openmdao.main.case import Case
 from openmdao.main.interfaces import IDOEgenerator
@@ -12,7 +12,46 @@ from openmdao.lib.drivers.caseiterdriver import CaseIterDriverBase
 from openmdao.util.decorators import add_delegate
 from openmdao.main.hasparameters import HasParameters
 
-
+@add_delegate(HasParameters)
+class DOENeighbordriver(CaseIterDriverBase):
+    """ Driver for Design of Experiments within a specified neighborhood around a point"""
+    
+    # pylint: disable-msg=E1101
+    DOEgenerator = Slot(IDOEgenerator, iotype='in', required=True,
+                          desc='Iterator supplying normalized DOE values.')
+    
+    case_outputs = ListStr([], iotype='in',desc='A list of outputs to be saved with each case.')
+    
+    alpha=Float(1., iotype='in',desc='Multiplicative factor for neighborhood DOE Driver')
+    def get_case_iterator(self):
+        """Returns a new iterator over the Case set."""
+        return self._get_cases()
+        
+    def _get_cases(self):
+        params = self.get_parameters().values()
+        self.DOEgenerator.num_parameters = len(params)
+        
+        for row in self.DOEgenerator:
+            #vals = [p.low+(p.high-p.low)*val for p,val in zip(params,row)]        
+            
+            vals=[]
+            for p,val in zip(params,row):
+                #newval=(p.low+(p.high-p.low)*val-p.evaluate())*alpha+p.evaluate()
+                newval=(p.low+(p.high-p.low)*val)*self.alpha-p.evaluate()*(self.alpha-1.)
+                if newval<p.low:
+                    newval=p.low
+                elif newval>p.high:
+                    newval=p.high
+                vals.append(newval)
+            
+            case = self.set_parameters(vals,Case())
+            # now add events
+            for varname in self.get_events(): 
+                case.add_input(varname,True)
+            case.add_outputs(self.case_outputs)    
+            
+            yield case
+           
 @add_delegate(HasParameters)
 class DOEdriver(CaseIterDriverBase):
     """ Driver for Design of Experiments """
@@ -31,7 +70,6 @@ class DOEdriver(CaseIterDriverBase):
     def _get_cases(self):
         params = self.get_parameters().values()
         self.DOEgenerator.num_parameters = len(params)
-        
         for row in self.DOEgenerator:
             vals = [p.low+(p.high-p.low)*val for p,val in zip(params,row)]
             case = self.set_parameters(vals,Case())
