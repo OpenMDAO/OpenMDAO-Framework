7d002f12a35dc985988d912e281a1eb892ed1ac0
Tristan Hearn <tristan.a.hearn@nasa.gov>
2011-11-04 11:07:00 -0400

    bliss2000 development

--- openmdao.lib/src/openmdao/lib/drivers/doedriver.py
+++ openmdao.lib/src/openmdao/lib/drivers/doedriver.py
@@ -13,7 +13,8 @@ from openmdao.util.decorators import add_delegate
 from openmdao.main.hasparameters import HasParameters
 
 @add_delegate(HasParameters)
-class DOENeighbordriver(CaseIterDriverBase):
+
+class DOEdriver(CaseIterDriverBase):
     """ Driver for Design of Experiments within a specified neighborhood around a point"""
     
     # pylint: disable-msg=E1101
@@ -31,9 +32,11 @@ class DOENeighbordriver(CaseIterDriverBase):
         params = self.get_parameters().values()
         self.DOEgenerator.num_parameters = len(params)
         
-        for row in self.DOEgenerator:
-            #vals = [p.low+(p.high-p.low)*val for p,val in zip(params,row)]        
-            
+        M=[]
+        for p in params:
+            M.append((p.evaluate()-p.low)/(p.high-p.low))
+        
+        for row in list(self.DOEgenerator)+[M]:
             vals=[]
             for p,val in zip(params,row):
                 #newval=(p.low+(p.high-p.low)*val-p.evaluate())*alpha+p.evaluate()
@@ -48,34 +51,7 @@ class DOENeighbordriver(CaseIterDriverBase):
             # now add events
             for varname in self.get_events(): 
                 case.add_input(varname,True)
-            case.add_outputs(self.case_outputs)    
-            
-            yield case
-           
-@add_delegate(HasParameters)
-class DOEdriver(CaseIterDriverBase):
-    """ Driver for Design of Experiments """
-    
-    # pylint: disable-msg=E1101
-    DOEgenerator = Slot(IDOEgenerator, iotype='in', required=True,
-                          desc='Iterator supplying normalized DOE values.')
-    
-    case_outputs = ListStr([], iotype='in', 
-                           desc='A list of outputs to be saved with each case.')
-    
-    def get_case_iterator(self):
-        """Returns a new iterator over the Case set."""
-        return self._get_cases()
-        
-    def _get_cases(self):
-        params = self.get_parameters().values()
-        self.DOEgenerator.num_parameters = len(params)
-        for row in self.DOEgenerator:
-            vals = [p.low+(p.high-p.low)*val for p,val in zip(params,row)]
-            case = self.set_parameters(vals,Case())
-            # now add events
-            for varname in self.get_events(): 
-                case.add_input(varname,True)
-            case.add_outputs(self.case_outputs)    
+            case.add_outputs(self.case_outputs)  
+            print "TEST"            
             
             yield case
