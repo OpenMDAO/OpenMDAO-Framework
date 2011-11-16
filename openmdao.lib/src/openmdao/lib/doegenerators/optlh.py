54df4368ceedfa82eea6eab1e19c6cd93ee6d261
Bret Naylor <naylor.b@gmail.com>
2011-11-16 11:35:57 -0500

    put contents of openmdao.util.mdo into openmdo.lib.doegenerators.optlh and removed openmdao.util.mdo.py'

--- openmdao.lib/src/openmdao/lib/doegenerators/optlh.py
+++ openmdao.lib/src/openmdao/lib/doegenerators/optlh.py
@@ -17,18 +17,59 @@
 # Lesser General Public License along with this program. If not, see
 # <http://www.gnu.org/licenses/>.
 
-from random import randint
+from random import randint, shuffle
 
 # pylint: disable-msg=E0611,F0401
-from numpy import array, size, sum, floor
+from numpy import array, size, sum, floor, zeros
 from numpy.linalg import norm
 
 from enthought.traits.api import HasTraits
 
 from openmdao.lib.datatypes.api import Int, Enum, Float
-
-from openmdao.util.mdo import rand_latin_hypercube
 from openmdao.main.interfaces import implements, IDOEgenerator
+
+
+def rand_latin_hypercube(n, k, edges=False):
+    """
+    Calculates a random latin hypercube set of n points in k 
+    dimensions within [0,1]^k hypercube.
+    
+    n: int
+       Desired number of points.
+    k: int
+       Number of design variables (dimensions).
+    edges: bool (optional)
+       if Edges=True, the extreme bins will have their centres on the
+       edges of the domain; otherwise the bins will be entirely 
+       contained within the domain (default setting).
+
+    Returns an n by k numpy array.
+    """
+    #generate nxk array of random numbers from the list of range(n) choices
+    X = zeros((n, k))
+    row = range(1, n+1)
+    for i in range(k):
+        shuffle(row)
+        X[:,i] = row
+        
+    if edges:
+        return (X-1.0)/float((n-1))
+ 
+    return (X-.5)/float(n)
+
+
+def is_latin_hypercube(lh):
+    """Returns True if the given array is a latin hypercube.
+    The given array is assumed to be a numpy array.
+    """
+    n,k = lh.shape
+    for j in range(k):
+        col = lh[:,j]
+        colset = set(col)
+        if len(colset) < len(col):
+            return False  # something was duplicated
+    return True
+
 
 class LatinHypercube(object):
     
@@ -192,6 +233,11 @@ def _mmlhs(x_start, population, generations):
 
 if __name__== "__main__":  # pragma no cover
     import sys
+    
+    lh1 = array([[1,2,3],[3,1,2],[2,3,1]])
+    assert(is_latin_hypercube(lh1))
+    badlh = array([[1,2,3],[1,3,2],[3,2,1]])
+    assert(is_latin_hypercube(badlh) is False)
     
     try:
         from matplotlib import pyplot
