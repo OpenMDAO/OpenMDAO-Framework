0f20706cecbf7f3f778b04aa3fb9e31f1819402a
Bret Naylor <naylor.b@gmail.com>
2011-12-13 10:17:44 -0500

    tweaking test_docs

--- openmdao.devtools/src/openmdao/devtools/test/test_docs.py
+++ openmdao.devtools/src/openmdao/devtools/test/test_docs.py
@@ -10,19 +10,11 @@ from argparse import Namespace
 
 class SphinxDocsTestCase(unittest.TestCase):
     def test_docs(self):
-        tmpdir = tempfile.mkdtemp()
-        try:
-            fpath = os.path.join(tmpdir, 'outfile')
-            openmdao_cmd = os.path.join(os.path.dirname(sys.executable), 'openmdao')
-            with open(fpath, 'wb') as f:
-                p = Popen('%s test_docs' % openmdao_cmd, stdout=f, stderr=STDOUT,
-                          env=os.environ, shell=True)
-                p.wait()
-            with open(fpath, 'rb') as f:
-                output = f.read()
-            retval = p.returncode
-            if not output.strip().endswith('build succeeded.'):
-                self.fail('problem in building sphinx documentation:\\n'+output)
-        finally:
-            shutil.rmtree(tmpdir, ignore_errors=True)
+        openmdao_cmd = os.path.join(os.path.dirname(sys.executable), 'openmdao')
+        p = Popen([openmdao_cmd, 'test_docs'], stdout=PIPE, stderr=STDOUT,
+                  env=os.environ, cwd=os.getcwd())
+        output = p.communicate()[0]
+        retval = p.returncode
+        if not output.strip().endswith('build succeeded.'):
+            self.fail('problem in building sphinx documentation:\\n'+output)
 
