b8a1d21173b2050197e87f13299c1542f54f24cf
Justin Gray <Justin.S.Gray@gmail.com>
2012-01-06 08:46:41 -0500

    split DOEdriver into standard DOEdriver and NeighborhoodDOEdriver

--- openmdao.lib/src/openmdao/lib/drivers/doedriver.py
+++ openmdao.lib/src/openmdao/lib/drivers/doedriver.py
@@ -15,6 +15,36 @@ from openmdao.main.hasparameters import HasParameters
 @add_delegate(HasParameters)
 
 class DOEdriver(CaseIterDriverBase):
+    """ Driver for Design of Experiments """
+    
+    # pylint: disable-msg=E1101
+    DOEgenerator = Slot(IDOEgenerator, iotype='in', required=True,
+                          desc='Iterator supplying normalized DOE values.')
+    
+    case_outputs = ListStr([], iotype='in', 
+                           desc='A list of outputs to be saved with each case.')
+    
+    def get_case_iterator(self):
+        """Returns a new iterator over the Case set."""
+        return self._get_cases()
+        
+    def _get_cases(self):
+        params = self.get_parameters().values()
+        self.DOEgenerator.num_parameters = len(params)
+        
+        for row in self.DOEgenerator:
+            vals = [p.low+(p.high-p.low)*val for p,val in zip(params,row)]
+            case = self.set_parameters(vals, Case(parent_uuid=self._case_id))
+            # now add events
+            for varname in self.get_events(): 
+                case.add_input(varname,True)
+            case.add_outputs(self.case_outputs)    
+            
+            yield case
+            
+            
+@add_delegate(HasParameters)            
+class NeiborhoodDOEdriver(CaseIterDriverBase):
     """ Driver for Design of Experiments within a specified neighborhood around a point"""
     
     # pylint: disable-msg=E1101
@@ -55,4 +85,4 @@ class DOEdriver(CaseIterDriverBase):
             for varname in self.get_events(): 
                 case.add_input(varname,True)
             case.add_outputs(self.case_outputs)              
-            yield case
+            yield case            
--- openmdao.lib/src/openmdao/lib/drivers/test/test_doedriver.py
+++ openmdao.lib/src/openmdao/lib/drivers/test/test_doedriver.py
@@ -17,7 +17,7 @@ from openmdao.main.exceptions import RunStopped
 from openmdao.main.resource import ResourceAllocationManager, ClusterAllocator
 from openmdao.lib.datatypes.api import Float, Bool, Array
 from openmdao.lib.casehandlers.listcaseiter import ListCaseIterator
-from openmdao.lib.drivers.doedriver import DOEdriver
+from openmdao.lib.drivers.doedriver import DOEdriver, NeiborhoodDOEdriver
 from openmdao.lib.casehandlers.api import ListCaseRecorder, DumpCaseRecorder
 from openmdao.lib.doegenerators.optlh import OptLatinHypercube
 from openmdao.lib.doegenerators.full_factorial import FullFactorial
@@ -82,7 +82,7 @@ class MyModel(Assembly):
                                     
 
 
-class TestCase(unittest.TestCase):
+class TestCaseDOE(unittest.TestCase):
     """ Test DOEdriver. """
 
     # Need to be in this directory or there are issues with egg loading.
@@ -162,8 +162,7 @@ class TestCase(unittest.TestCase):
         
         self.model.run()
 
-        #runs one extra case to put model back to starting point
-        self.assertEqual(len(results), 1+self.model.driver.DOEgenerator.num_sample_points)
+        self.assertEqual(len(results), self.model.driver.DOEgenerator.num_sample_points)
         msg = "driver: Exception getting case outputs: " \
             "driven: 'DrivenComponent' object has no attribute 'sum_z'"
         for case in results.cases:
@@ -236,7 +235,7 @@ class TestCase(unittest.TestCase):
 
         if retry:
             self.model.run()
-            self.assertEqual(len(results), 11)
+            self.assertEqual(len(results), 10)
             self.verify_results(forced_errors)
         else:
             assert_raises(self, 'self.model.run()', globals(), locals(),
@@ -260,6 +259,199 @@ class TestCase(unittest.TestCase):
                 self.assertEqual(case['driven.rosen_suzuki'],
                                  rosen_suzuki(*[case['driven.x%s'%i] for i in range(4)]))
 
+class MyModel2(Assembly):
+    """ Use DOEdriver with DrivenComponent. """
+
+    def __init__(self, *args, **kwargs):
+        super(MyModel2, self).__init__(*args, **kwargs)
+        self.add('driver', NeiborhoodDOEdriver())
+        self.add('driven', DrivenComponent())
+        self.driver.workflow.add('driven')
+        self.driver.DOEgenerator = OptLatinHypercube(num_samples=10)
+        self.driver.case_outputs = ['driven.rosen_suzuki']
+        self.driver.add_parameter(('driven.x0','driven.y0'),low=-10.,high=10.,
+                                      scaler=20., adder=10.)
+        for name in ['x1','x2', 'x3']:
+            self.driver.add_parameter("driven.%s"%name,low=-10.,high=10.,
+                                      scaler=20., adder=10.)
+                
+                
+class TestCaseNeighborhoodDOE(unittest.TestCase):
+    """ Test NeighborhoodDOEdriver. """
+
+    # Need to be in this directory or there are issues with egg loading.
+    directory = pkg_resources.resource_filename('openmdao.lib.drivers', 'test')
+
+    def setUp(self):
+        os.chdir(self.directory)
+        self.model = set_as_top(MyModel2())
+
+    def tearDown(self):
+        self.model.pre_delete()
+        self.model = None
+
+        # Verify we didn't mess-up working directory.
+        end_dir = os.getcwd()
+        os.chdir(ORIG_DIR)
+        if os.path.realpath(end_dir).lower() != os.path.realpath(self.directory).lower():
+            self.fail('Ended in %s, expected %s' % (end_dir, self.directory))
+
+    def test_sequential(self):
+        logging.debug('')
+        logging.debug('test_sequential')
+        self.run_cases(sequential=True)
+
+    def test_sequential_errors(self):
+        logging.debug('')
+        logging.debug('test_sequential_errors')
+        self.model.driver._call_execute = True
+        self.run_cases(sequential=True, forced_errors=True, retry=True)
+        
+    def test_sequential_errors_abort(self):
+        self.run_cases(sequential=True, forced_errors=True)
+        
+    def test_no_parameter(self):
+        logging.debug('')
+        logging.debug('test_no_parameter')
+        try:
+            self.model.driver.add_parameter('foobar.blah')
+        except AttributeError as err:
+            self.assertEqual(str(err), 
+                             "driver: Can't add parameter 'foobar.blah' because it doesn't exist.")
+            
+    def test_event_removal(self):
+        self.model.driver.add_event('driven.err_event')
+        lst = self.model.driver.get_events()
+        self.assertEqual(lst, ['driven.err_event'])
+        self.model.driver.remove_event('driven.err_event')
+        lst = self.model.driver.get_events()
+        self.assertEqual(lst, [])
+        
+    def test_param_removal(self):
+        lst = self.model.driver.list_param_targets()
+        self.assertEqual(lst, ['driven.x0', 'driven.y0', 'driven.x1', 'driven.x2', 'driven.x3'])
+        self.model.driver.remove_parameter('driven.x1')
+        lst = self.model.driver.list_param_targets()
+        self.assertEqual(lst, ['driven.x0', 'driven.y0', 'driven.x2', 'driven.x3'])
+
+    def test_no_event(self):
+        logging.debug('')
+        logging.debug('test_no_event')
+        try:
+            self.model.driver.add_event('foobar.blah')
+        except AttributeError as err:
+            self.assertEqual(str(err), 
+                             "driver: Can't add event 'foobar.blah' because it doesn't exist")
+        else:
+            self.fail("expected AttributeError")
+
+    def test_nooutput(self):
+        logging.debug('')
+        logging.debug('test_nooutput')
+
+        results = ListCaseRecorder()
+        self.model.driver.recorders = [results]
+        self.model.driver.error_policy = 'RETRY'
+        self.model.driver.case_outputs.append('driven.sum_z')
+        
+        self.model.run()
+
+        self.assertEqual(len(results), 1+self.model.driver.DOEgenerator.num_sample_points)
+        msg = "driver: Exception getting case outputs: " \
+            "driven: 'DrivenComponent' object has no attribute 'sum_z'"
+        for case in results.cases:
+            self.assertEqual(case.msg, msg)
+
+    def test_noiterator(self):
+        logging.debug('')
+        logging.debug('test_noiterator')
+
+        # Check resoponse to no iterator set.
+        self.model.driver.recorders = [ListCaseRecorder()]
+        self.model.driver.DOEgenerator = None
+        try:
+            self.model.run()
+        except Exception as exc:
+            msg = "driver: required plugin 'DOEgenerator' is not present"
+            self.assertEqual(str(exc), msg)
+        else:
+            self.fail('Exception expected')
+
+    def test_norecorder(self):
+        logging.debug('')
+        logging.debug('test_norecorder')
+
+        self.model.driver.recorders = []
+        self.model.run()
+        
+    def test_output_error(self):
+        class Dummy(Component): 
+            x = Float(0,iotype="in")
+            y = Float(0,iotype="out")
+            z = Float(0,iotype="out")
+            
+            def execute(self): 
+                self.y = 10+self.x
+                
+        class Analysis(Assembly): 
+            
+            def __init__(self): 
+                super(Analysis,self).__init__(self)
+                
+                self.add('d',Dummy())
+                
+                self.add('driver',NeiborhoodDOEdriver())
+                self.driver.DOEgenerator = FullFactorial(2) 
+                self.driver.recorders = [DumpCaseRecorder()]
+                self.driver.add_parameter('d.x',low=0,high=10)     
+                self.driver.case_outputs = ['d.y','d.bad','d.z']
+                
+        a = Analysis()
+        
+        try:
+            a.run()
+        except Exception as err:
+            self.assertTrue(str(err).startswith('driver: Run aborted: Traceback '))
+            self.assertTrue(str(err).endswith("d: 'Dummy' object has no attribute 'bad'"))
+        else:
+            self.fail("Exception expected")
+
+
+    def run_cases(self, sequential, forced_errors=False, retry=True):
+        # Evaluate cases, either sequentially or across  multiple servers.
+        
+        self.model.driver.sequential = sequential
+        results = ListCaseRecorder()
+        self.model.driver.recorders = [results]
+        self.model.driver.error_policy = 'RETRY' if retry else 'ABORT'
+        if forced_errors:
+            self.model.driver.add_event('driven.err_event')
+
+        if retry:
+            self.model.run()
+            self.assertEqual(len(results), 11)
+            self.verify_results(forced_errors)
+        else:
+            assert_raises(self, 'self.model.run()', globals(), locals(),
+                          RuntimeError,
+                          "driver: Run aborted: RuntimeError('driven: Forced error',)")
+
+    def test_scaling(self):
+        self.model.driver.DOEgenerator = ff = FullFactorial(num_levels=3)
+        ff.num_parameters = 4
+        for case in self.model.driver._get_cases():
+            print case
+        
+    def verify_results(self, forced_errors=False):
+        # Verify recorded results match expectations.
+        
+        for case in self.model.driver.recorders[0].cases:
+            if forced_errors:
+                self.assertEqual(case.msg, 'driven: Forced error')
+            else:
+                self.assertEqual(case.msg, None)
+                self.assertEqual(case['driven.rosen_suzuki'],
+                                 rosen_suzuki(*[case['driven.x%s'%i] for i in range(4)]))                
         
 
 if __name__ == "__main__":
