e7567f483f1494731325d545d7676fe0e4df3a01
Bret Naylor <naylor.b@gmail.com>
2012-01-26 10:08:45 -0500

    trying zmq stuff

--- openmdao.main/src/openmdao/main/actorcomp.py
+++ openmdao.main/src/openmdao/main/actorcomp.py
@@ -1,17 +1,20 @@
 
+import pickle
+
 import zmq
 from zmq.eventloop import ioloop, zmqstream
 
 import time
 import threading
 
-class Actor(object):
-    def __init__(self, context, name, url=None, sleep=1):
+from openmdao.test.execcomp import ExecComp
+
+class ActorCompWrapper(object):
+    def __init__(self, context, comp, url=None):
         if url is None:
             url = 'inproc://%s' % name
         self._context = context
-        self._name = name
-        self._sleep = sleep
+        self._comp = comp
         
         self._repsock = ctx.socket(zmq.REP)
         self._repsock.bind(url)
@@ -19,25 +22,27 @@ class Actor(object):
         self._repstream.on_recv(self.handle_req)
         
         self._pubsock = ctx.socket(zmq.PUB)
-        self._pubsock.bind('inproc://%s_pub_' % name)
+        self._pubsock.bind('inproc://%s_pub_' % comp.get_pathname())
         self._pubstream = zmqstream.ZMQStream(self._pubsock)
         
     def handle_req(self, msg):
-        print 'received %s' % msg
+        parts = pickle.loads(msg[0])
+        print 'received %s' % parts
+        funct = getattr(self._comp, parts[0])
+        ret = funct(*parts[1], **parts[2])
+        self._repstream.send_pyobj(ret)
     
     def execute(self):
         self._pubsock.send_multipart("[('a',1.0), ('b',2.0)]")
     
 if __name__ == '__main__':
-    ioloop.install()
-    
     loop = ioloop.IOLoop.instance()
     
     ctx = zmq.Context()
     
-    Actor(ctx, 'foo', 3)
-    Actor(ctx, 'bar', 0.5)
-    Actor(ctx, 'baz')
+    comp =ExecComp(exprs=['z=x+y'])
+    comp.name = 'foo'
+    ActorCompWrapper(ctx, comp, url='tcp://127.0.0.1:5555')
     
     loop.start()
     
