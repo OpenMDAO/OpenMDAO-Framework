f3ca909fc291f24dc6051c80654c42d74f7cba17
Justin Gray <Justin.S.Gray@gmail.com>
2012-02-29 20:52:07 -0500

    updatex CO and IDF tutorials to use slsqp

--- examples/openmdao.examples.mdao/openmdao/examples/mdao/sellar_IDF.py
+++ examples/openmdao.examples.mdao/openmdao/examples/mdao/sellar_IDF.py
@@ -2,7 +2,7 @@
     Solution of the sellar analytical problem using IDF.
 """
 from openmdao.main.api import Assembly
-from openmdao.lib.drivers.api import CONMINdriver
+from openmdao.lib.drivers.api import SLSQPdriver
 from openmdao.lib.optproblems import sellar
 
 
@@ -17,7 +17,7 @@ class SellarIDF(Assembly):
         Optimal Objective = 3.18339"""
                 
         # create Optimizer instance
-        self.add('driver', CONMINdriver())
+        self.add('driver', SLSQPdriver())
 
         # Disciplines
         self.add('dis1', sellar.Discipline1())
@@ -39,19 +39,10 @@ class SellarIDF(Assembly):
         self.driver.add_parameter('dis2.y1',      low = 3.16,  high=10.0)
         self.driver.add_parameter('dis1.y2',      low = -10.0, high=24.0)
             
-        self.driver.add_constraint('(dis2.y1-dis1.y1)**3 <= 0')
-        self.driver.add_constraint('(dis1.y1-dis2.y1)**3 <= 0')
-        self.driver.add_constraint('(dis2.y2-dis1.y2)**3 <= 0')
-        self.driver.add_constraint('(dis1.y2-dis2.y2)**3 <= 0')
+        self.driver.add_constraint('(dis2.y1-dis1.y1)**2 <= 0')
+        self.driver.add_constraint('(dis2.y2-dis1.y2)**2 <= 0')
   
         self.driver.iprint = 0
-        self.driver.itmax = 100
-        self.driver.fdch = .003
-        self.driver.fdchm = .003
-        self.driver.delfun = .0001
-        self.driver.dabfun = .00001
-        self.driver.ct = -.01
-        self.driver.ctlmin = 0.001
 
 
 if __name__ == "__main__": # pragma: no cover         
@@ -71,7 +62,6 @@ if __name__ == "__main__": # pragma: no cover
     prob.run()
 
     print "\n"
-    print "CONMIN Iterations: ", prob.driver.iter_count
     print "Minimum found at (%f, %f, %f)" % (prob.dis1.z1, \
                                              prob.dis2.z2, \
                                              prob.dis1.x1)
