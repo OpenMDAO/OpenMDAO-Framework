19fe633d8bb2a55a47c09290ed0b27ab21c5196e
Tristan Hearn <tristanhearn@gmail.com>
2012-04-06 11:15:29 -0400

    added sympy dependencies

--- go-openmdao-dev.py
+++ go-openmdao-dev.py
@@ -1621,7 +1621,7 @@ def _single_install(cmds, req, bin_dir, failures, dodeps=False):
 def after_install(options, home_dir):
     global logger, openmdao_prereqs
     
-    reqs = ['SetupDocs==1.0.5', 'Pyevolve==0.6', 'networkx==1.3', 'slsqp==1.0.1', 'pyparsing==1.5.2', 'Pygments==1.3.1', 'docutils==0.8.1', 'argparse==1.2.1', 'nose==0.11.3', 'zope.interface==3.6.1', 'Sphinx==1.1.3', 'Jinja2==2.4', 'decorator==3.2.0', 'ordereddict==1.1', 'newsumt==1.1.0', 'Traits==3.3.0', 'boto==2.0rc1', 'cobyla==1.0.1', 'paramiko==1.7.7.1', 'Fabric==0.9.3', 'virtualenv==1.6.4', 'conmin==1.0.1', 'pycrypto==2.3']
+    reqs = ['SetupDocs==1.0.5', 'Pyevolve==0.6', 'networkx==1.3', 'slsqp==1.0.1', 'pyparsing==1.5.2', 'Pygments==1.3.1', 'docutils==0.8.1', 'argparse==1.2.1', 'nose==0.11.3', 'zope.interface==3.6.1', 'Sphinx==1.1.3', 'Jinja2==2.4', 'decorator==3.2.0', 'ordereddict==1.1', 'newsumt==1.1.0', 'Traits==3.3.0', 'boto==2.0rc1', 'cobyla==1.0.1', 'paramiko==1.7.7.1', 'Fabric==0.9.3', 'virtualenv==1.6.4', 'conmin==1.0.1', 'pycrypto==2.3','sympy==0.7.1']
     guireqs = ['pyzmq-static==2.1.11.1', 'jsonpickle==0.4.0', 'tornado==2.2', 'Django==1.3.1']
     guitestreqs = ['entrypoint2==0.0.5', 'mocker==1.1', 'zope.testing==4.1.1', 'EasyProcess==0.1.3', 'zope.exceptions==3.6.1', 'path.py==2.2.2', 'tornado==2.2', 'pyzmq-static==2.1.11.1', 'PyVirtualDisplay==0.0.9', 'Django==1.3.1', 'zope.testrunner==4.0.4', 'lazr.testing==0.1.2a', 'jsonpickle==0.4.0']
     
