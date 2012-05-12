051df9ea21ae717922019e59aff72e5f5854d627
Justin Gray <Justin.S.Gray@gmail.com>
2012-05-11 20:19:21 -0400

    VAF now works with state var convergence asmb

--- openmdao.lib/src/openmdao/lib/optproblems/VAF.py
+++ openmdao.lib/src/openmdao/lib/optproblems/VAF.py
@@ -28,7 +28,7 @@ class Structural(Component):
     constants = Slot(Constants(), iotype="in")
     
     l = Float(0.02,units="m", iotype="in", desc="length of the stator flap")
-    F = Float(0.02,units="N", iotype="in", desc="Aerodynamic force on the stator flap")
+    F = Float(2.0,units="N", iotype="in", desc="Aerodynamic force on the stator flap")
     
     theta = Float(0.1,units="rad", iotype="in", desc="Radial deflection of the stator flap") #state variable
     
@@ -41,14 +41,18 @@ class Structural(Component):
 class StructuralAsmb(Assembly): 
     
     def configure(self): 
-        self.add('strut_comp',Structural())
-        self.add('theta_passthrough',ExecComp('theta_out=theta_in')) #a comp to make theta an output of the asmb
+        self.add('struct_comp',Structural())
+        self.add('theta_passthrough',ExecComp(['theta_out=theta_in'])) #a comp to make theta an output of the asmb
         self.create_passthrough('theta_passthrough.theta_out',alias="theta")
+        self.create_passthrough('struct_comp.l')
+        self.create_passthrough('struct_comp.F')
+        self.create_passthrough('struct_comp.theta_residual')
+        self.create_passthrough('struct_comp.constants')
         
         self.add('driver',BroydenSolver())
         self.driver.add_parameter(['struct_comp.theta','theta_passthrough.theta_in'],low=0,high=1)
-        self.driver.add_constraint('struct.theta_residual=0')
-        
+        self.driver.add_constraint('struct_comp.theta_residual=0')
+        self.driver.workflow.add(['struct_comp','theta_passthrough'])
         
         
 class Aerodynamics(Component): 
@@ -58,11 +62,11 @@ class Aerodynamics(Component):
     w = Float(0.02,units="m", iotype="in", desc="width of the stator flap")
     theta = Float(0.1,units="rad", iotype="in", desc="Radial deflection of the stator flap") #state variable
     
-    F = Float(0.02,units="N", iotype="out", desc="Aerodynamic force on the stator flap")
+    F = Float(0.0,units="N", iotype="out", desc="Aerodynamic force on the stator flap")
     
     def execute(self): 
         self.F = self.constants.C*self.l*self.w*cos(self.theta)*self.constants.v**2
-    
+            
     
 class VaneAirFlow(OptProblem): 
     constants = Slot(Constants(), iotype="in")
@@ -70,7 +74,7 @@ class VaneAirFlow(OptProblem):
     def configure(self): 
         self.constants = Constants()
         
-        self.add('struct',Structural())
+        self.add('struct',StructuralAsmb())
         self.add('aero',Aerodynamics())
         
         self.connect('constants',['struct.constants','aero.constants'])
@@ -91,16 +95,20 @@ if __name__ == "__main__":
     p.driver.add_constraint('(aero.l*aero.w - 0.01000)**2 <= 0')
     
     p.add('mda',BroydenSolver())
-    p.mda.add_parameter(['struct.theta','aero.theta'],low=-1e99,high=1e99)
+    #p.mda.add_parameter(['struct.theta','aero.theta'],low=-1e99,high=1e99)
     p.mda.add_parameter('struct.F',low=-1e99,high=1e99)
-    p.mda.add_constraint('struct.theta_residual=0')
+    #p.mda.add_constraint('struct.theta_residual=0')
     p.mda.add_constraint('struct.F=aero.F')
+    p.connect('struct.theta','aero.theta')
     
     p.driver.workflow.add('mda')
     p.mda.workflow.add(['struct','aero'])
     
+    p.struct.l = p.aero.l = .03
+    p.aero.w = .2
+    
     p.run()
     
     print p.aero.F, p.struct.F
-    print p.struct.theta_residual
+    print p.struct.theta_residual, p.struct.theta
     print p.aero.l, p.aero.w
