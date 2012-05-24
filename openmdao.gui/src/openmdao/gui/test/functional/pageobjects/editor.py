98e7983294ea4304fa14816880211e68732e73a4
Scott Townsend <setowns1@viper.grc.nasa.gov>
2012-05-24 10:47:34 -0400

    Start on GUI VariableTree test.

--- openmdao.gui/src/openmdao/gui/test/functional/pageobjects/editor.py
+++ openmdao.gui/src/openmdao/gui/test/functional/pageobjects/editor.py
@@ -142,7 +142,16 @@ class EditorPage(BasePageObject):
         element = WebDriverWait(self.browser, TMO).until(
             lambda browser: browser.find_element_by_xpath(xpath))
         chain = ActionChains(self.browser)
-        chain.context_click(element).perform()
+        for i in range(10):
+            try:
+                chain.context_click(element).perform()
+            except StaleElementReferenceException:
+                logging.critical('import_file: StaleElementReferenceException')
+                element = WebDriverWait(self.browser, 1).until(
+                    lambda browser: browser.find_element_by_xpath(xpath))
+                chain = ActionChains(self.browser)
+            else:
+                break
         self('file_import').click()
         NotifierPage.wait(self.browser, self.port)
 
