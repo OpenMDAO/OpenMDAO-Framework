c901d26dcf0d5150806c28c45715c8e9ccb2dcef
Calvin Robinson <crobinsonut+openmdao@gmail.com>
2012-06-01 08:18:59 -0400

    Modified UploadHandler of handlers_workspace.py to add multiple files to server.

--- openmdao.gui/src/openmdao/gui/handlers_workspace.py
+++ openmdao.gui/src/openmdao/gui/handlers_workspace.py
@@ -399,14 +399,15 @@ class UploadHandler(ReqHandler):
     def post(self):
         path = self.get_argument('path', default=None)
         cserver = self.get_server()
-        file = self.request.files['myfile'][0]
-        if file:
-            filename = file['filename']
-            if len(filename) > 0:
-                if path:
-                    filename = os.path.sep.join([path, filename])
-                cserver.add_file(filename, file['body'])
-                self.render('closewindow.html')
+        files = self.request.files['myfile']
+        if files:
+            for file_ in files:
+                filename = file_['filename']
+                if len(filename) > 0:
+                    if path:
+                        filename = os.path.sep.join([path, filename])
+                    cserver.add_file(filename, file_['body'])
+                    self.render('closewindow.html')
 
     @web.authenticated
     def get(self):
