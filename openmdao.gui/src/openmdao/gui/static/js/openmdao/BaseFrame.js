47263ab05d4b393ee835a2cf744b1eddbda19cee
swryan <swryan@vbuntu12.(none)>
2012-07-16 10:50:37 -0400

    fix issue with size of popout (for diagrams)

--- openmdao.gui/src/openmdao/gui/static/js/openmdao/BaseFrame.js
+++ openmdao.gui/src/openmdao/gui/static/js/openmdao/BaseFrame.js
@@ -80,9 +80,26 @@ openmdao.BaseFrame.prototype.popup = function (title) {
         'close': function(ev, ui) {
                     this.close();
                  }.bind(this),
-        width:   'auto',
-        height:  'auto'
+        'width': 'auto',
+        'height': 'auto'
     });
+    
+    // make sure the popup is in the window
+    if (this.elm.height() > window.innerHeight*.8) {
+        this.elm.height(window.innerHeight*.8);
+    }
+    if (this.elm.width() > window.innerWidth*.8) {
+        this.elm.width(window.innerWidth*.8);
+    }
+    var off  = this.elm.offset(),
+        top  = off.top,
+        left = off.left;
+    if (top < 0 || top > window.innerHeight) {
+        this.elm.dialog({ position: "top" });
+    }
+    if (left < 0 || left > window.innerWidth) {
+        this.elm.dialog({ position: "left" });
+    }
 };
 
 openmdao.BaseFrame.prototype.setTitle = function (title) {
