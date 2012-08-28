cc18b437d833c292346c6638fe32b67459336efa
Scott Townsend <scott.e.townsend@nasa.gov>
2012-08-28 14:12:52 -0400

    Doc test fix

--- openmdao.gui/src/openmdao/gui/test/functional/test_dataflow.py
+++ openmdao.gui/src/openmdao/gui/test/functional/test_dataflow.py
@@ -254,7 +254,7 @@ def _test_connections(browser):
 
     # reconnect transmission RPM to engine RPM
     conn_page.connect_vars('transmission.RPM', 'engine.RPM')
-    time.sleep(0.5)
+    time.sleep(1)
     eq(len(conn_page.get_variable_figures()), 2)
     eq(sorted(conn_page.get_variable_names()),
        ['RPM', 'RPM'])
