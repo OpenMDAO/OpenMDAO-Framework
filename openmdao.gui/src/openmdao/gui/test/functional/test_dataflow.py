7b2694d296208e7d7743a3be96d3dc530d508e0b
Scott Townsend <scott.e.townsend@nasa.gov>
2012-08-29 15:59:13 -0400

    GUI test tweak

--- openmdao.gui/src/openmdao/gui/test/functional/test_dataflow.py
+++ openmdao.gui/src/openmdao/gui/test/functional/test_dataflow.py
@@ -245,6 +245,7 @@ def _test_connections(browser):
     # disconnect transmission
     tranny = workspace_page.get_dataflow_figure('transmission', 'sim.vehicle')
     tranny.disconnect()
+    time.sleep(0.5)
 
     # now there are no connections between transmission and engine
     conn_page.set_source_component('transmission')
@@ -254,7 +255,7 @@ def _test_connections(browser):
 
     # reconnect transmission RPM to engine RPM
     conn_page.connect_vars('transmission.RPM', 'engine.RPM')
-    time.sleep(1)
+    time.sleep(0.5)
     eq(len(conn_page.get_variable_figures()), 2)
     eq(sorted(conn_page.get_variable_names()),
        ['RPM', 'RPM'])
