77683455aa8eb74d3cfd59b62fbcaab25b66b53c
Kenneth Moore <kenneth.t.moore-1@nasa.gov>
2012-08-30 17:02:13 -0400

    Fix for creating passthrough of entire variable tree

--- openmdao.main/src/openmdao/main/test/test_vartree_passthrough.py
+++ openmdao.main/src/openmdao/main/test/test_vartree_passthrough.py
@@ -27,9 +27,19 @@ class TstAssembly(Assembly):
         self.create_passthrough('comp.dummy_data_out.dummy1', 'dummy1_out')
         self.driver.workflow.add('comp')
 
+class TstAssembly2(Assembly):
+
+    def configure(self):
+        self.add('comp',TstComponent())
+        self.create_passthrough('comp.dummy_data')
+        self.create_passthrough('comp.dummy_data_out', 'dummy1_out')
+        self.driver.workflow.add('comp')
+
 class VarTreePassthroughTestCase(unittest.TestCase):
 
     def test_vartree_passthrough(self):
+        
+        # Tests that we can create a passthrough of one variable in a tree
         blah = set_as_top(TstAssembly())
         blah.dummy1 = 5.0
         self.assertEqual(blah.dummy1_out, 1.0)
@@ -37,6 +47,17 @@ class VarTreePassthroughTestCase(unittest.TestCase):
         self.assertEqual(blah.comp.dummy_data.dummy1, 5.0)
         self.assertEqual(blah.dummy1, 5.0)
         self.assertEqual(blah.dummy1_out, 5.0)
+    
+    def test_vartree_passthrough(self):
+        
+        # Tests that we can create a passthrough of an entire variable tree
+        blah = set_as_top(TstAssembly2())
+        blah.dummy_data.dummy1 = 5.0
+        self.assertEqual(blah.dummy1_out.dummy1, 1.0)
+        blah.run()
+        self.assertEqual(blah.comp.dummy_data.dummy1, 5.0)
+        self.assertEqual(blah.dummy_data.dummy1, 5.0)
+        self.assertEqual(blah.dummy1_out.dummy1, 5.0)
     
         
 if __name__ == "__main__":
