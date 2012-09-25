83e100126f62c3542d2d6de7cebef3217213c0de
Calvin Robinson <crobinsonut+openmdao@gmail.com>
2012-09-25 08:42:28 -0400

    Modified devtools.releasetools.py to copy docs to openmdao.main

--- openmdao.devtools/src/openmdao/devtools/releasetools.py
+++ openmdao.devtools/src/openmdao/devtools/releasetools.py
@@ -399,8 +399,12 @@ def build_release(parser, options):
         
         if not os.path.isfile(idxpath) or not options.nodocbuild:
             build_docs(parser, options)
+
         shutil.copytree(os.path.join(topdir,'docs','_build', 'html'), 
                     os.path.join(destdir,'docs'))
+
+        shutil.copytree(os.path.join(topdir,'docs','_build', 'html'), 
+                    os.path.join(topdir,'openmdao.main', 'docs'))
 
         if not options.test:
             # commit the changes to the release branch
