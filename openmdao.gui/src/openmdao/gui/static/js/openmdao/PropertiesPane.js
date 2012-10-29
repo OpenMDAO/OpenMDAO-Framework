2a39aa5a769693b42057f3d5d1469d4a6e6ba087
Kenneth Moore <kenneth.t.moore-1@nasa.gov>
2012-10-29 19:36:45 -0400

    losing power soon

--- openmdao.gui/src/openmdao/gui/static/js/openmdao/PropertiesPane.js
+++ openmdao.gui/src/openmdao/gui/static/js/openmdao/PropertiesPane.js
@@ -190,11 +190,14 @@ openmdao.PropertiesPane = function(elm,model,pathname,name,editable,meta) {
                 }
             });
 
-            //props.setData(properties);
             dataView.beginUpdate();
             dataView.setItems(properties);
             dataView.setFilter(this.filter);
             dataView.endUpdate();
+            // This seems to be needed if we reuse this pane for another
+            // component (which is what the properties panel does.)
+            // The data view manages the data otherwise.
+            props.setData(properties);
 
         }
         else {
