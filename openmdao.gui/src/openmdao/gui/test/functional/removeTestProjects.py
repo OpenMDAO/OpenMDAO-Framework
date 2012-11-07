4db3e01fdddd17b2c6e227bbd33fa5fefaf41cd0
S Ryan <stephen.w.ryan@nasa.gov>
2012-11-07 14:18:16 -0500

    enable removeTestprojects.py under windows add loop to delete_all_test_projects() so it will get all of them

--- openmdao.gui/src/openmdao/gui/test/functional/removeTestProjects.py
+++ openmdao.gui/src/openmdao/gui/test/functional/removeTestProjects.py
@@ -3,13 +3,12 @@ import sys
 from nose.tools import with_setup
 
 
-if sys.platform != 'win32':  # No testing on Windows yet.
-    from util import main, setup_server, teardown_server, generate, begin
+from util import main, setup_server, teardown_server, generate, begin
 
-    @with_setup(setup_server, teardown_server)
-    def test_generator():
-        for _test, browser in generate(__name__):
-            yield _test, browser
+@with_setup(setup_server, teardown_server)
+def test_generator():
+    for _test, browser in generate(__name__):
+        yield _test, browser
 
 
 def _test_new_project(browser):
