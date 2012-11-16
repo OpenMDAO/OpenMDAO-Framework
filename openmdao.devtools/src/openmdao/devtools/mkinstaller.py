a312a6c2fbc948f4ead8b18816296f459d71cd69
Calvin Robinson <crobinsonut+openmdao@gmail.com>
2012-11-16 16:32:00 -0500

    Fixed errors in building docs. Environment is activated for building the docs and deactivate once done.

--- openmdao.devtools/src/openmdao/devtools/mkinstaller.py
+++ openmdao.devtools/src/openmdao/devtools/mkinstaller.py
@@ -271,11 +271,11 @@ def after_install(options, home_dir):
         for req in options.reqs:
             _single_install(cmds, req, bin_dir, failures, dodeps=True)
 
-        activate = os.path.join(home_dir, 'activate')
-        deactivate = os.path.join(home_dir, 'deactivate')
+        activate = os.path.join(bin_dir, 'activate')
+        deactivate = os.path.join(bin_dir, 'deactivate')
         source_command = "." if not sys.platform.startswith("win") else ""
         
-        if(os.system('%%s %%s && openmdao build_docs && %%s' %% (source_commad, activate, deactivate)) != 0):
+        if(os.system('%%s %%s && openmdao build_docs && deactivate' %% (source_command, activate)) != 0):
             print "Failed to build the docs."
 
         if sys.platform.startswith('win'): # retrieve MinGW DLLs from server
