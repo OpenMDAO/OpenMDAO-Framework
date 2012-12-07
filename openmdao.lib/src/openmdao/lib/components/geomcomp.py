dbfb459773832c77d8854fff4bede39a75c1f7c1
Bret Naylor <naylor.b@gmail.com>
2012-12-07 16:44:52 -0500

    interim commit

--- openmdao.lib/src/openmdao/lib/components/geomcomp.py
+++ openmdao.lib/src/openmdao/lib/components/geomcomp.py
@@ -1,7 +1,8 @@
 
-
 from openmdao.main.component import Component
 from openmdao.main.interfaces import IParametricGeometry
+from openmdao.main.datatypes.api import Slot
+
 
 class GeomComponent(Component):
 
@@ -11,7 +12,6 @@ class GeomComponent(Component):
 
     def __init__(self):
         super(GeomComponent, self).__init__()
-        
 
     def execute(self):
         """Rebuild the geometry using the current set of parameters.
