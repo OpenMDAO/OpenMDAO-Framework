f0fdf8e81149cf5f3fa33f7abb6436fa6595cef9
Scott Townsend <scott.e.townsend@nasa.gov>
2012-12-11 15:01:06 -0500

    EC2 websocket debug

--- openmdao.gui/src/openmdao/gui/zmqstreamserver.py
+++ openmdao.gui/src/openmdao/gui/zmqstreamserver.py
@@ -37,10 +37,16 @@ class ZMQStreamHandler(websocket.WebSocketHandler):
     ''' a handler that forwards output from a ZMQStream to a WebSocket
     '''
 
+    def __init__(self, application, request, **kwargs):
+        DEBUG('__init__ %s %s' % (request, kwargs))
+        super(ZMQStreamHandler, self).__init__(application, request, **kwargs)
+
     def initialize(self, addr):
+        DEBUG('initialize %s' % addr)
         self.addr = addr
 
     def open(self):
+        DEBUG('open %s' % self.addr)
         stream = None
         try:
             context = zmq.Context()
@@ -58,6 +64,7 @@ class ZMQStreamHandler(websocket.WebSocketHandler):
             stream.on_recv(self._write_message)
 
     def _write_message(self, message):
+        DEBUG('_write_message %s %s' % (self.addr, message))
         if len(message) == 1:
             message = message[0]
             message = make_unicode(message)  # tornado websocket wants unicode
@@ -90,10 +97,11 @@ class ZMQStreamHandler(websocket.WebSocketHandler):
             self.write_message(message)
 
     def on_message(self, message):
+        DEBUG('on_message %s %s' % (self.addr, message))
         pass
 
     def on_close(self):
-        DEBUG('zmqstream connection closed')
+        DEBUG('zmqstream %s connection closed' % self.addr)
 
 
 class ZMQStreamApp(web.Application):
