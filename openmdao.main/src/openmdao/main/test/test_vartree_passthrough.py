67ea731cda12ad027cfae84df887fac89eaf7789
Kenneth Moore <kenneth.t.moore-1@nasa.gov>
2013-01-10 10:14:37 -0500

    Tests added

--- openmdao.main/src/openmdao/main/test/test_vartree_passthrough.py
+++ openmdao.main/src/openmdao/main/test/test_vartree_passthrough.py
@@ -63,7 +63,7 @@ class VarTreePassthroughTestCase(unittest.TestCase):
         
         # Tests the attributres dictionary for passthrough trees
         
-        blah = TstAssembly2()
+        blah = set_as_top(TstAssembly2())
         attrs = blah.get_attributes(True)
         self.assertTrue({'indent': 0, 
                          'name': 'dummy_data', 
@@ -84,6 +84,50 @@ class VarTreePassthroughTestCase(unittest.TestCase):
                          'low': None, 
                          'type': 'float', 
                          'id': 'dummy_data_dummy1'} in attrs['Inputs'])
+        self.assertTrue({'indent': 0, 
+                         'name': 'dummy1_out', 
+                         'vt': 'vt', 
+                         'implicit': '', 
+                         'valid': False, 
+                         'connected': '', 
+                         'ttype': 'slot', 
+                         'type': 'TstContainer', 
+                         'id': 'dummy1_out'} in attrs['Outputs'])
+        self.assertTrue({'indent': 1, 
+                         'name': 'dummy1', 
+                         'parent': 'dummy1_out', 
+                         'value': 1.0, 
+                         'high': None, 
+                         'valid': False, 
+                         'connected': '', 
+                         'low': None, 
+                         'type': 'float', 
+                         'id': 'dummy1_out_dummy1'} in attrs['Outputs'])
+
+        blah = set_as_top(TstAssembly())
+        attrs = blah.get_attributes(True)
+        self.assertTrue({'indent': 0, 
+                         'name': 'dummy1', 
+                         'value': 1.0, 
+                         'high': None, 
+                         'valid': True, 
+                         'implicit': '', 
+                         'connected': '', 
+                         'target': 'comp.dummy_data.dummy1', 
+                         'low': None, 
+                         'type': 'float', 
+                         'id': 'dummy1'} in attrs['Inputs'])
+        self.assertTrue({'indent': 0, 
+                         'name': 'dummy1_out', 
+                         'value': 1.0, 
+                         'high': None, 
+                         'valid': False, 
+                         'implicit': '', 
+                         'connected': '', 
+                         'target': 'comp.dummy_data_out.dummy1', 
+                         'low': None, 
+                         'type': 'float', 
+                         'id': 'dummy1_out'} in attrs['Outputs'])
         
 if __name__ == "__main__":
     unittest.main()
