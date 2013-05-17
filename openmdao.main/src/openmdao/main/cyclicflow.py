dcaee29d9a5e047b0e8febc9123a9d97fa0cdf57
Kenneth Moore <kenneth.t.moore-1@nasa.gov>
2013-05-17 14:38:46 -0400

    working

--- openmdao.main/src/openmdao/main/cyclicflow.py
+++ openmdao.main/src/openmdao/main/cyclicflow.py
@@ -1,6 +1,8 @@
 """ A workflow that contains cyclic graphs. Note that a special solver is
 required to converge this workflow in order to execute it. """
 
+from openmdao.main.interfaces import IDriver
+from openmdao.main.mp_support import has_interface
 from openmdao.main.seqentialflow import SequentialWorkflow
 
 __all__ = ['CyclicFlow']
@@ -14,4 +16,14 @@ class CyclicFlow(SequentialWorkflow):
     def __init__(self, parent=None, scope=None, members=None):
         """ Create an empty flow. """
         
-        super(CyclicFlow, self).__init__(parent, scope, members)    
+        super(CyclicFlow, self).__init__(parent, scope, members)   
+        
+    def check_config(self):
+        """Any checks we need. For now, drivers are not allowed. You can get
+        around this by placing them in an assembly."""         
+        
+        for comp in self.get_components():
+            if has_interface(comp, IDriver):
+                self.scope.raise_exception('circular dependency found between'
+                                           ' the following: %s'
+                                           % str(strcon[0]), RuntimeError)                
