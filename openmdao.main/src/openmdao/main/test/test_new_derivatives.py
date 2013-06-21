e18766f1e2e76efa983874612a2d0bbfbeb8ac05
Kenneth Moore <kenneth.t.moore-1@nasa.gov>
2013-06-21 10:30:35 -0400

    large dataflow test passes

--- openmdao.main/src/openmdao/main/test/test_new_derivatives.py
+++ openmdao.main/src/openmdao/main/test/test_new_derivatives.py
@@ -10,11 +10,12 @@ try:
 except ImportError as err:
     from openmdao.main.numpy_fallback import zeros, array
 
-from openmdao.main.api import Component, VariableTree, Driver, Assembly
+from openmdao.main.api import Component, VariableTree, Driver, Assembly, set_as_top
 from openmdao.main.datatypes.api import Array, Float, VarTree
 from openmdao.main.hasparameters import HasParameters
 from openmdao.main.hasobjective import HasObjective
 from openmdao.main.interfaces import IHasParameters, implements
+from openmdao.test.execcomp import ExecCompWithDerivatives
 from openmdao.util.decorators import add_delegate
 from openmdao.util.testutil import assert_rel_error
 
@@ -188,6 +189,71 @@ class Testcase_derivatives(unittest.TestCase):
         J = top.driver.workflow.calc_gradient(outputs=['comp.f_xy'])
         assert_rel_error(self, J[0,0], 5.0, 0.0001)
         assert_rel_error(self, J[0,1], 21.0, 0.0001)
+
+    def test_large_dataflow(self):
+        
+        self.top = set_as_top(Assembly())
+    
+        exp1 = ['y1 = 2.0*x1**2',
+                'y2 = 3.0*x1']
+        deriv1 = ['dy1_dx1 = 4.0*x1',
+                  'dy2_dx1 = 3.0']
+    
+        exp2 = ['y1 = 0.5*x1']
+        deriv2 = ['dy1_dx1 = 0.5']
+        
+        exp3 = ['y1 = 3.5*x1']
+        deriv3 = ['dy1_dx1 = 3.5']
+    
+        exp4 = ['y1 = x1 + 2.0*x2',
+                'y2 = 3.0*x1',
+                'y3 = x1*x2']
+        deriv4 = ['dy1_dx1 = 1.0',
+                  'dy1_dx2 = 2.0',
+                  'dy2_dx1 = 3.0',
+                  'dy2_dx2 = 0.0',
+                  'dy3_dx1 = x2',
+                  'dy3_dx2 = x1']
+        
+        exp5 = ['y1 = x1 + 3.0*x2 + 2.0*x3']
+        deriv5 = ['dy1_dx1 = 1.0',
+                  'dy1_dx2 = 3.0',
+                  'dy1_dx3 = 2.0']
+        
+        self.top.add('comp1', ExecCompWithDerivatives(exp1, deriv1))
+        self.top.add('comp2', ExecCompWithDerivatives(exp2, deriv2))
+        self.top.add('comp3', ExecCompWithDerivatives(exp3, deriv3))
+        self.top.add('comp4', ExecCompWithDerivatives(exp4, deriv4))
+        self.top.add('comp5', ExecCompWithDerivatives(exp5, deriv5))
+    
+        self.top.driver.workflow.add(['comp1', 'comp2', 'comp3', 'comp4', 'comp5'])
+        
+        self.top.connect('comp1.y1', 'comp2.x1')
+        self.top.connect('comp1.y2', 'comp3.x1')
+        self.top.connect('comp2.y1', 'comp4.x1')
+        self.top.connect('comp3.y1', 'comp4.x2')
+        self.top.connect('comp4.y1', 'comp5.x1')
+        self.top.connect('comp4.y2', 'comp5.x2')
+        self.top.connect('comp4.y3', 'comp5.x3')
+        
+        #obj = 'comp5.y1'
+        #con = 'comp5.y1-comp3.y1 > 0'
+        #self.top.driver.add_parameter('comp1.x1', low=-50., high=50., fd_step=.0001)
+        #self.top.driver.add_objective(obj)
+        #self.top.driver.add_constraint(con)
+    
+        self.top.comp1.x1 = 2.0
+        self.top.run()
+        J = self.top.driver.workflow.calc_gradient(inputs=['comp1.x1'],
+                                                   outputs=['comp5.y1'])
+        
+        assert_rel_error(self, J[0, 0], 313.0, .001)
+        
+        #grad = self.top.driver.differentiator.get_gradient(obj)
+        #assert_rel_error(self, grad[0], 626.0, .001)
+        
+        #grad = self.top.driver.differentiator.get_gradient('comp5.y1-comp3.y1>0')
+        #assert_rel_error(self, grad[0], -626.0+10.5, .001)
         
 if __name__ == '__main__':
     import nose
