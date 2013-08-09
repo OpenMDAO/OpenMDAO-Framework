612729016a8cafa2e403596f03e33911393576b1
S Ryan <stephen.w.ryan@nasa.gov>
2013-08-08 22:59:29 -0400

    implement a gui functional test of the geometry viewer tree frame

--- openmdao.gui/src/openmdao/gui/test/functional/pageobjects/geometry.py
+++ openmdao.gui/src/openmdao/gui/test/functional/pageobjects/geometry.py
@@ -7,7 +7,31 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import StaleElementReferenceException
 
 from basepageobject import BasePageObject, TMO
-from elements import GenericElement
+from elements import GenericElement, TextElement, CheckboxElement
+
+
+class EdgeNode(BasePageObject):
+    """ An edge node in the Edges tree. """
+
+    name = TextElement((By.XPATH, './a'))
+    viz  = CheckboxElement((By.XPATH, './div/span/input[@value=1]'))
+    grd  = CheckboxElement((By.XPATH, './div/span/input[@value=16]'))
+    ori  = CheckboxElement((By.XPATH, './div/span/input[@value=8]'))
+
+    def __init__(self, browser, port, root):
+        super(EdgeNode, self).__init__(browser, port, root)
+
+
+class FaceNode(BasePageObject):
+    """ A face node in the Faces tree. """
+
+    name = TextElement((By.XPATH, './a'))
+    viz  = CheckboxElement((By.XPATH, './div/span/input[@value=1]'))
+    grd  = CheckboxElement((By.XPATH, './div/span/input[@value=32]'))
+    trn  = CheckboxElement((By.XPATH, './div/span/input[@value=2]'))
+
+    def __init__(self, browser, port, root):
+        super(FaceNode, self).__init__(browser, port, root)
 
 
 class GeometryPage(BasePageObject):
@@ -26,43 +50,83 @@ class GeometryPage(BasePageObject):
     def __init__(self, browser, port):
         super(GeometryPage, self).__init__(browser, port)
 
-        self.locators = {}
-        self.locators["edges"] = (By.XPATH, "//div[@id='tree_frame_edges_tree']//a")
-        self.locators["faces"] = (By.XPATH, "//div[@id='tree_frame_faces_tree']//a")
+    def expand_edges(self):
+        expander = self.edges_tree.find_element_by_css_selector('ins')
+        expander.click()
 
-    def get_edges(self):
+    def expand_faces(self):
+        expander = self.faces_tree.find_element_by_css_selector('ins')
+        expander.click()
+
+    def get_edge_names(self):
         """ Return names in the edges tree. """
         WebDriverWait(self.browser, TMO).until(
             lambda browser: browser.find_element(By.ID, 'tree_frame_edges_tree'))
         # FIXME: absolute delay for tree population.
         time.sleep(2)
-        edges = self.browser.find_elements(*self.locators["edges"])
+
+        edges = self.edges_tree.find_elements_by_css_selector('ul > li > ul > li')
         edge_names = []
         for i in range(len(edges)):
-            for retry in range(10):  # This has had issues...
+            for retry in range(10):
                 try:
-                    print edges[i]
-                    edge_names.append(edges[i].text.strip())
+                    edge_names.append(edges[i].get_attribute('nom').strip())
                 except StaleElementReferenceException:
-                    logging.warning('get_edges: StaleElementReferenceException')
+                    logging.warning('get_edge_names: StaleElementReferenceException')
                 else:
                     break
         return edge_names
 
-    def get_faces(self):
+    def get_face_names(self):
         """ Return names in the edges tree. """
         WebDriverWait(self.browser, TMO).until(
             lambda browser: browser.find_element(By.ID, 'tree_frame_faces_tree'))
         # FIXME: absolute delay for tree population.
         time.sleep(2)
-        faces = self.browser.find_elements(*self.locators["faces"])
+        faces = self.faces_tree.find_elements_by_css_selector('ul > li > ul > li')
         face_names = []
         for i in range(len(faces)):
-            for retry in range(10):  # This has had issues...
+            for retry in range(10):
                 try:
-                    face_names.append(faces[i].text.strip())
+                    face_names.append(faces[i].get_attribute('nom').strip())
                 except StaleElementReferenceException:
-                    logging.warning('get_edges: StaleElementReferenceException')
+                    logging.warning('get_face_names: StaleElementReferenceException')
                 else:
                     break
         return face_names
+
+    def get_edge(self, name):
+        """ Return the edge element with the given name in the edges tree. """
+        WebDriverWait(self.browser, TMO).until(
+            lambda browser: browser.find_element(By.ID, 'tree_frame_edges_tree'))
+
+        edges = self.edges_tree.find_elements_by_css_selector('li')
+        for i in range(len(edges)):
+            for retry in range(10):
+                try:
+                    edge_name = edges[i].get_attribute('nom').strip()
+                    if edge_name == name:
+                        edge = EdgeNode(self.browser, self.port, edges[i])
+                        return edge
+                except StaleElementReferenceException:
+                    logging.warning('get_edge: StaleElementReferenceException')
+                else:
+                    break
+
+    def get_face(self, name):
+        """ Return the face element with the given name in the faces tree. """
+        WebDriverWait(self.browser, TMO).until(
+            lambda browser: browser.find_element(By.ID, 'tree_frame_faces_tree'))
+
+        faces = self.faces_tree.find_elements_by_css_selector('li')
+        for i in range(len(faces)):
+            for retry in range(10):
+                try:
+                    face_name = faces[i].get_attribute('nom').strip()
+                    if face_name == name:
+                        face = FaceNode(self.browser, self.port, faces[i])
+                        return face
+                except StaleElementReferenceException:
+                    logging.warning('get_face: StaleElementReferenceException')
+                else:
+                    break
--- openmdao.gui/src/openmdao/gui/test/functional/test_geometry.py
+++ openmdao.gui/src/openmdao/gui/test/functional/test_geometry.py
@@ -2,11 +2,12 @@
 Tests of overall workspace functions.
 """
 
-import os
 import time
-import filecmp
 
-import pkg_resources
+# these imports are for comparing screen capture to existing file
+# import os
+# import filecmp
+# import pkg_resources
 
 from nose.tools import eq_ as eq
 from nose.tools import with_setup
@@ -61,18 +62,6 @@ def _test_view_geometry(browser):
     browser.switch_to_window(geom_window)
     geom_page = GeometryPage.verify(browser, workspace_page.port)  # FIXME
 
-    edges = geom_page.get_edges()
-    faces = geom_page.get_faces()
-
-    print 'edges:', edges
-    print 'faces:', faces
-
-    assert len(edges) == 7
-    assert len(faces) == 7
-
-    assert edges[0] == 'Edges'
-    assert faces[0] == 'Faces'
-
     # FIXME: there are still problems with diffing the PNG files.  Not sure
     # if there are differences due to platform or what.  Also on windows
     # document.getElementById("statusline") returns null (could be just a timing thing)
@@ -92,6 +81,64 @@ def _test_view_geometry(browser):
     #    os.remove("geom.png")
     #except IOError:
     #    pass
+
+    # check initial state of edges tree
+    geom_page.expand_edges()
+    edges = geom_page.get_edge_names()
+    eq(edges, ['Edge 1', 'Edge 2', 'Edge 3', 'Edge 4', 'Edge 5', 'Edge 6'])
+
+    edges = geom_page.get_edge('Edges')
+    edge1 = geom_page.get_edge('Edge 1')
+    edge2 = geom_page.get_edge('Edge 2')
+
+    eq([edges.viz, edges.grd, edges.ori], [True, False, False])
+    eq([edge1.viz, edge1.grd, edge1.ori], [True, False, False])
+    eq([edge2.viz, edge2.grd, edge2.ori], [True, False, False])
+
+    # toggle visibility for an edge
+    edge1.viz = False
+
+    # check for expected new edges state
+    eq([edges.viz, edges.grd, edges.ori], [False, False, False])
+    eq([edge1.viz, edge1.grd, edge1.ori], [False, False, False])
+    eq([edge2.viz, edge2.grd, edge2.ori], [True, False, False])
+
+    # toggle visibility for all edges
+    edges.viz = True
+
+    # check for expected new edges state
+    eq([edges.viz, edges.grd, edges.ori], [True, False, False])
+    eq([edge1.viz, edge1.grd, edge1.ori], [True, False, False])
+    eq([edge2.viz, edge2.grd, edge2.ori], [True, False, False])
+
+    # check initial state of faces tree
+    geom_page.expand_faces()
+    faces = geom_page.get_face_names()
+    eq(faces, ['Face 1', 'Face 2', 'Face 3', 'Face 4', 'Face 5', 'Face 6'])
+
+    faces = geom_page.get_face('Faces')
+    face3 = geom_page.get_face('Face 3')
+    face5 = geom_page.get_face('Face 5')
+
+    eq([faces.viz, faces.grd, faces.trn], [True, False, False])
+    eq([face3.viz, face3.grd, face3.trn], [True, False, False])
+    eq([face5.viz, face5.grd, face5.trn], [True, False, False])
+
+    # toggle transparency for a face
+    face3.trn = True
+
+    # check for expected new faces state
+    eq([faces.viz, faces.grd, faces.trn], [True, False, False])
+    eq([face3.viz, face3.grd, face3.trn], [True, False, True])
+    eq([face5.viz, face5.grd, face5.trn], [True, False, False])
+
+    # toggle grid for all faces
+    faces.grd = True
+
+    # check for expected new faces state
+    eq([faces.viz, faces.grd, faces.trn], [True, True, False])
+    eq([face3.viz, face3.grd, face3.trn], [True, True, True])
+    eq([face5.viz, face5.grd, face5.trn], [True, True, False])
 
     # Back to workspace.
     browser.close()
