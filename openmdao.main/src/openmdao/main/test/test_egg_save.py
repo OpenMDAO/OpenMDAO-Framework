7711f50a35b727877a18f66768399786a1334611
Keith Marsteller <kmarsteller@gmail.com>
2013-11-12 10:32:11 -0500

    editing test for release problems

--- openmdao.main/src/openmdao/main/test/test_egg_save.py
+++ openmdao.main/src/openmdao/main/test/test_egg_save.py
@@ -476,8 +476,8 @@ class TestCase(unittest.TestCase):
             ]
 
             # Add our file if we're not considered part of an egg.
-            if sys.modules[self.__module__].__file__.find('.egg') < 0:
-                expected.append(('extract', 'Egg_TestModel/test_egg_save.py'))
+            #if sys.modules[self.__module__].__file__.find('.egg') < 0:
+	    expected.append(('extract', 'Egg_TestModel/test_egg_save.py'))
             expected.append(('complete', None))
 
             self.assertEqual(len(OBSERVATIONS), len(expected))
