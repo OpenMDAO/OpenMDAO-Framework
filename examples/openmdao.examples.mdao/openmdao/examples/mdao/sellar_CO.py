8c715b98c5203ab96866feb8d5de4b2243615f13
Kenneth Moore <kenneth.t.moore-1@nasa.gov>
2013-11-21 15:31:17 -0500

    fix for 2d slices in applyJ&T

--- examples/openmdao.examples.mdao/openmdao/examples/mdao/sellar_CO.py
+++ examples/openmdao.examples.mdao/openmdao/examples/mdao/sellar_CO.py
@@ -114,7 +114,7 @@ if __name__ == "__main__":
     tt = time.time()
     prob.run()
 
-    prob.driver.workflow.check_gradient()
+    #prob.driver.workflow.check_gradient()
     
     print "\n"
     print "Minimum found at (%f, %f, %f)" % (prob.dis1.z1, \
