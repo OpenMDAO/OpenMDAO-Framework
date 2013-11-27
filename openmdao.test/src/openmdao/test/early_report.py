4a1172f5eaa0cdc9394c005b447abfcd8abeb65d
Bret Naylor <naylor.b@gmail.com>
2013-11-27 17:35:09 -0500

    added early reporting nose plugin

--- openmdao.test/src/openmdao/test/early_report.py
+++ openmdao.test/src/openmdao/test/early_report.py
@@ -3,21 +3,29 @@ import time
 import traceback
 
 from nose.plugins import Plugin
+from nose import SkipTest
 
 class TestInfo(object):
-    def __init__(self, name):
+    def __init__(self, test):
         self.start = time.time()
-        self.end = None
-        self.name = name
+        try:
+            ctxt = test.context.__name__
+        except:
+            ctxt = None
+        self.name = test.shortDescription()
+        if self.name is None:
+            self.name = test.id()
         self.status = None
+        self.elapsed = 0.
 
     def end(self):
-        self.elapsed = time-time() - self.start
+        self.elapsed = time.time() - self.start
 
-class BroadcastStream:
+
+class BroadcastStream(object):
     """Sends stream output to multiple streams."""
     def __init__(self, *streams):
-        self._streams = streams
+        self._streams = streams[:]
 
     def write(self, *arg):
         for s in self._streams:
@@ -25,8 +33,24 @@ class BroadcastStream:
 
     def writeln(self, *arg):
         for s in self._streams:
-            s.writeln(*arg)
+            for a in arg:
+                s.write(a)
+            s.write('\n')
 
+    def close(self):
+        for s in self._streams:
+            try:
+                s.close()
+            except:
+                pass
+            
+    def flush(self):
+        for s in self._streams:
+            try:
+                s.flush()
+            except:
+                pass
+            
 
 class EarlyTestInfo(Plugin):
     """This plugin writes test info (failures, run time) to
@@ -36,7 +60,8 @@ class EarlyTestInfo(Plugin):
     """
 
     name = 'early-report'
-    score = 100
+    score = 1001 # need high score to get called before ErroClassPlugin,
+                 # otherwise we lose Skips
 
     def options(self, parser, env):
         """Sets additional command line options."""
@@ -50,7 +75,7 @@ class EarlyTestInfo(Plugin):
         """Configures the plugin."""
         super(EarlyTestInfo, self).configure(options, config)
         self.config = config
-        self._all_tests = []
+        self._tests = {}
         self._failed_tests = []
         self._error_tests = []
         self._report_path = os.path.abspath(options.report)
@@ -59,72 +84,113 @@ class EarlyTestInfo(Plugin):
         """Report the test failures."""
         if not self.enabled:
             return
-        if self._failed_tests:
-            stream.writeln("The following tests had failures:")
-            for test in self._failed_tests:
+
+        failed = [k for k,v in self._tests.items() if v.status=='F']
+        errors = [k for k,v in self._tests.items() if v.status=='E']
+        skips  = [k for k,v in self._tests.items() if v.status=='S']
+
+        self.stream.writeln("\n\n")
+        
+        if skips:
+            stream.writeln("The following tests were skipped")
+            for test in skips:
                 stream.writeln(test)
-        if self._error_tests:
+        if failed:
+            stream.writeln("\nThe following tests had failures:")
+            for test in failed:
+                stream.writeln(test)
+        if errors:
             stream.writeln("\nThe following tests had errors:")
-            for test in self._error_tests:
+            for test in errors:
                 stream.writeln(test)
+                
+        self.stream.writeln("\n")
 
     def formatErr(self, err):
         exctype, value, tb = err
         return ''.join(traceback.format_exception(exctype, value, tb))
     
+    def _show_test(self, tinfo):
+        tinfo.end()
+        self.stream.write(tinfo.name)
+        self.stream.write(' (%.3g sec) ... ' % tinfo.elapsed)
+        if tinfo.status is None:
+            self.stream.writeln('OK')
+        elif tinfo.status == 'F':
+            self.stream.writeln('FAIL')
+        elif tinfo.status == 'E':
+            self.stream.writeln('ERROR')
+        elif tinfo.status == 'S':
+            self.stream.writeln('SKIP')
+
     def addError(self, test, err, capt=None):
-        self._error_tests.append(test.id())
+        if err[0] == SkipTest:
+            self._tests[test.id()].status = 'S'
+        else:
+            self._tests[test.id()].status = 'E'
+        self._show_test(self._tests[test.id()])
+        self.stream.writeln(self.formatErr(err))
 
     def addFailure(self, test, err, capt=None, tb_info=None):
-        self._failed_tests.append(test.id())
+        if err[0] == SkipTest:
+            self._tests[test.id()].status = 'S'
+        else:
+            self._tests[test.id()].status = 'F'
+        self._show_test(self._tests[test.id()])
+        self.stream.writeln(self.formatErr(err))
 
     def addSuccess(self, test, capt=None):
-        pass
+        self._show_test(self._tests[test.id()])
+        
+    def addSkip(self, test, *args, **kwargs):
+        self._show_test(self._tests[test.id()])
 
     def finalize(self, result):
-        self.html.append('<div>')
-        self.html.append("Ran %d test%s" %
+        self.stream.writeln("\n\nRan %d test%s\n" %
                          (result.testsRun, result.testsRun != 1 and "s" or ""))
-        self.html.append('</div>')
-        self.html.append('<div>')
-        if not result.wasSuccessful():
-            self.html.extend(['<span>FAILED ( ',
-                              'failures=%d ' % len(result.failures),
-                              'errors=%d' % len(result.errors),
-                              ')</span>'])                             
+
+        skips  = [k for k,v in self._tests.items() if v.status=='S']
+
+        if result.wasSuccessful():
+            self.stream.write("OK")
         else:
-            self.html.append('OK')
-        self.html.append('</div></body></html>')
-        # print >> sys.stderr, self.html
-        for l in self.html:
-            self.stream.writeln(l)
+            self.stream.write('FAILED ( failures=%d ' % len(result.failures))
+            self.stream.write('errors=%d )' % len(result.errors))
+
+        if skips:
+            self.stream.write('  skipped=%d ' % len(skips))
+                        
+        self.report(self.stream)
 
     def setOutputStream(self, stream):
+        outfile = open(self._report_path, 'w')
+
         # grab for own use
-        self.stream = stream        
-        # return dummy stream
+        self.stream = BroadcastStream(stream, outfile)
+        
+        # return dummy stream so normal output isn't displayed
         class dummy:
             def write(self, *arg):
                 pass
             def writeln(self, *arg):
+                pass
+            def flush(self):
                 pass
         d = dummy()
         return d
 
     def startContext(self, ctx):
         try:
-            n = ctx.__name__
+            self._context = ctx.__name__
         except AttributeError:
             return
         # TODO: possibly add indenting here per module and TestCase
 
     def stopContext(self, ctx):
-        pass
+        self._context = None
     
     def startTest(self, test):
-        self.html.extend([ '<div><span>',
-                           test.shortDescription() or str(test),
-                           '</span>' ])
+        self._tests[test.id()] = TestInfo(test)
         
     def stopTest(self, test):
-        self.html.append('</div>')
+        pass
