cd5a2a4a7a04c2641227025484839bb33b54f265
Kenneth Moore <kenneth.t.moore-1@nasa.gov>
2013-12-03 10:58:12 -0500

    a better fix

--- openmdao.main/src/openmdao/main/array_helpers.py
+++ openmdao.main/src/openmdao/main/array_helpers.py
@@ -50,8 +50,8 @@ def flattened_size(name, val, scope=None):
             size += flattened_size('.'.join((name, key)), value)
         return size
     
-    elif '.' not in name.partition('.')[2]:
-        meta = scope.get_metadata(name)
+    else:
+        meta = scope.get_metadata(name.split('[')[0])
         
         # Custom data objects with data_shape in the metadata
         if 'data_shape' in meta:
