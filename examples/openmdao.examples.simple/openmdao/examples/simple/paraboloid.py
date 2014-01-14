efe164e8f433f9f94a774dbc9041de3c59f6a109
Kenneth Moore <kenneth.t.moore-1@nasa.gov>
2014-01-14 10:55:27 -0500

    stuff

--- examples/openmdao.examples.simple/openmdao/examples/simple/paraboloid.py
+++ examples/openmdao.examples.simple/openmdao/examples/simple/paraboloid.py
@@ -9,23 +9,24 @@ from openmdao.main.datatypes.api import Float
 
 class Paraboloid(Component):
     """ Evaluates the equation f(x,y) = (x-3)^2 + xy + (y+4)^2 - 3 """
-    
-    # set up interface to the framework  
+
+    # set up interface to the framework
     # pylint: disable-msg=E1101
     x = Float(0.0, iotype='in', desc='The variable x')
     y = Float(0.0, iotype='in', desc='The variable y')
 
-    f_xy = Float(iotype='out', desc='F(x,y)')        
+    f_xy = Float(iotype='out', desc='F(x,y)')
 
-        
+
     def execute(self):
         """f(x,y) = (x-3)^2 + xy + (y+4)^2 - 3
         Optimal solution (minimum): x = 6.6667; y = -7.3333
         """
-        
+
         x = self.x
         y = self.y
-        
+
         self.f_xy = (x-3.0)**2 + x*y + (y+4.0)**2 - 3.0
-        
+        print x, y, self.f_xy
+
 # End paraboloid.py
