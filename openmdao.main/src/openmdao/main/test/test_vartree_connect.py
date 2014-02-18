adad8678a6e60859fb305e714b3d9b40494162a8
Bret Naylor <naylor.b@gmail.com>
2014-02-18 09:34:05 -0500

    fixed issue with setting a vtree subvar where another subvar is connected

--- openmdao.main/src/openmdao/main/test/test_vartree_connect.py
+++ openmdao.main/src/openmdao/main/test/test_vartree_connect.py
@@ -1,15 +1,24 @@
 
 import unittest
 
-from openmdao.main.api import VariableTree, Assembly, set_as_top
+from openmdao.main.api import VariableTree, Assembly, Component, set_as_top
 from openmdao.main.datatypes.api import Float, VarTree
 
 class TstVtree(VariableTree):
-    floatval = Float(1.0) 
+    floatval = Float(1.0)
+    floatval2 = Float(2.0)
 
 class VTAssembly(Assembly):
     vtree_in = VarTree(TstVtree(), iotype='in')
     vtree_out = VarTree(TstVtree(), iotype='out')
+
+class VTComp(Component):
+    vtree_in = VarTree(TstVtree(), iotype='in')
+    vtree_out = VarTree(TstVtree(), iotype='out')
+
+    def execute(self):
+        self.vtree_out.floatval = self.vtree_in.floatval
+        self.vtree_out.floatval2 = self.vtree_in.floatval2
 
 class AsmWithSub(Assembly):
     
@@ -28,6 +37,18 @@ class VarTreeConnectTestCase(unittest.TestCase):
         self.assertEqual(top.subasm.vtree_in.floatval, 1.0)
         top.run()
         self.assertEqual(top.subasm.vtree_in.floatval, 5.0)
+
+    def test_vartree_set_subvar_after_connecting_other_subvar(self):
+        top = set_as_top(Assembly())
+        top.add("comp1", VTComp())
+        top.add("comp2", VTComp())
+        top.driver.workflow.add(['comp1', 'comp2'])
+        top.connect('comp1.vtree_out.floatval', 'comp2.vtree_in.floatval')
+        top.comp1.vtree_in.floatval = 7.7
+        top.comp2.vtree_in.floatval2 = -2.2
+        top.run()
+        self.assertEqual(7.7, top.comp2.vtree_out.floatval)
+        self.assertEqual(-2.2, top.comp2.vtree_out.floatval2)
     
         
 if __name__ == "__main__":
