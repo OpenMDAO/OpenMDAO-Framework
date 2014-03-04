cc32a25794a3bf24368cb4e315a188d84a707616
Herb Schilling <hschilling@nasa.gov>
2014-03-04 11:00:22 -0500

    Fixed uniform doegenerator so it handles nested loops

--- openmdao.lib/src/openmdao/lib/doegenerators/test/test_uniform.py
+++ openmdao.lib/src/openmdao/lib/doegenerators/test/test_uniform.py
@@ -25,16 +25,33 @@ class TestCase(unittest.TestCase):
         cases = [case for case in uni]
         self.assertEqual(len(expected),len(cases))
         self.assertEqual(len(expected[0]),len(cases[0]))
-       
+
+    def test_nested_loop(self):
+        # test to make sure the generator can handle nested loops
+        uni = Uniform(5)
+        uni.num_parameters = 2
+
+        inner_count = 0
+        outer_count = 0
+        for case_outer in uni:
+            outer_count += 1
+            for case_inner in uni:
+                inner_count += 1
+
+        self.assertEqual(5,outer_count)
+        self.assertEqual(25,inner_count)
+        
     def test_low_sample_count(self): 
         uni = Uniform()
-        uni.num_paramters = 1
+        uni.num_samples = 1
         
         try: 
             for case in uni: 
                 pass
         except ValueError as err: 
-            self.assertEqual(str(err),"Uniform distributions must have at least 2 samples. num_samples is set to less than 2.")
+            self.assertEqual(str(err),"Uniform distributions "
+                             "must have at least 2 samples. "
+                             "num_samples is set to less than 2.")
 
 if __name__ == "__main__":
     unittest.main()
