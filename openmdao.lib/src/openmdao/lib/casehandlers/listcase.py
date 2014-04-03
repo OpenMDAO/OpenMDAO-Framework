dd65d90325e7cf9998b768aa30daea439450dd28
cwmine <greatcwmine@gmail.com>
2014-04-03 21:32:24 +0800

    fix the error that inherited list copy the argument "cases" not refered, which will get the new cases by the iterator at the runtime

--- openmdao.lib/src/openmdao/lib/casehandlers/listcase.py
+++ openmdao.lib/src/openmdao/lib/casehandlers/listcase.py
@@ -5,43 +5,46 @@
 from openmdao.main.interfaces import implements, ICaseRecorder, ICaseIterator
 
 
-class ListCaseIterator(list):
+class ListCaseIterator(object):
     """An iterator that returns :class:`Case` objects from a passed-in iterator
     of cases. This can be useful for runtime-generated cases from an
     optimizer, etc.
     """
-    
+
     implements(ICaseIterator)
-    
+
     def __init__(self, cases):
-        super(ListCaseIterator, self).__init__(cases)
+        self._cases=cases
+        super(ListCaseIterator, self).__init__()
+    def __getitem__(self,num):
+        return self._cases[num]
 
     def get_attributes(self, io_only=True):
         """ We need a custom get_attributes because we aren't using Traits to
         manage our changeable settings. This is unfortunate and should be
         changed to something that automates this somehow."""
-        
+
         attrs = {}
         attrs['type'] = type(self).__name__
-        
+
         return attrs
 
 
 class ListCaseRecorder(object):
     """Stores cases in a list."""
-    
+
     implements(ICaseRecorder)
-    
+
     def __init__(self):
         self.cases = []
-        
+
     def __len__(self):
         return len(self.cases)
 
     def startup(self):
         """ Nothing needed for a list case."""
         pass
-        
+
     def record(self, case):
         """Store the case in our internal list."""
         self.cases.append(case)
@@ -53,13 +56,13 @@ class ListCaseRecorder(object):
     def get_iterator(self):
         '''Return ListCaseIterator that uses our current list.'''
         return ListCaseIterator(self.cases)
-    
+
     def get_attributes(self, io_only=True):
         """ We need a custom get_attributes because we aren't using Traits to
         manage our changeable settings. This is unfortunate and should be
         changed to something that automates this somehow."""
-        
+
         attrs = {}
         attrs['type'] = type(self).__name__
-        
+
         return attrs
