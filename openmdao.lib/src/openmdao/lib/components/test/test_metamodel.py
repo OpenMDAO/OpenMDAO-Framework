4779eb099143a480f153b89780cc30da0d8d7e16
Kenneth-T-Moore <kenneth.t.moore-1@nasa.gov>
2014-04-07 17:02:48 -0400

    wrking

--- openmdao.lib/src/openmdao/lib/components/test/test_metamodel.py
+++ openmdao.lib/src/openmdao/lib/components/test/test_metamodel.py
@@ -49,6 +49,40 @@ class MetaModelTestCase(unittest.TestCase):
         model.meta.run()
         assert_rel_error(self, model.meta.y1, 1.4609, .001)
         
+    def test_array_inputs(self):
+        
+        model = set_as_top(Assembly())
+        model.add('meta', MetaModel(params=('x',), 
+                                    responses=('y1', 'y2')))
+        model.driver.workflow.add('meta')
+        
+        model.meta.params.x = [[1.0, 1.0, 1.0, 1.0], 
+                               [2.0, 1.0, 1.0, 1.0],
+                               [1.0, 2.0, 1.0, 1.0],
+                               [1.0, 1.0, 2.0, 1.0],
+                               [1.0, 1.0, 1.0, 2.0]]
+        model.meta.responses.y1 = [3.0, 2.0, 1.0, 6.0, -2.0]
+        model.meta.responses.y2 = [1.0, 4.0, 7.0, -3.0, 3.0]
+    
+        model.meta.default_surrogate = ResponseSurface()
+    
+        model.meta.x1 = [[1.0, 2.0], [1.0, 1.0]]
+        model.meta.run()
+        assert_rel_error(self, model.meta.y1, 1.0, .00001)
+        assert_rel_error(self, model.meta.y2, 7.0, .00001)
+        
+        model.meta.x1 = 2.5
+        model.meta.x2 = 3.5
+        model.meta.run()
+        assert_rel_error(self, model.meta.y1, 1.5934, .001)
+        
+        # Slot a new surrogate.
+        model.meta.default_surrogate = FloatKrigingSurrogate()
+        
+        self.assertTrue(model.meta._train == True)
+        model.meta.run()
+        assert_rel_error(self, model.meta.y1, 1.4609, .001)
+        
     def test_warm_start(self):
         
         model = set_as_top(Assembly())
