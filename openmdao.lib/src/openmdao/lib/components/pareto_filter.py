83e3e453b6b52b0bc08b744097b533b257f7f37a
Kenneth Moore <kenneth.t.moore-1@nasa.gov>
2014-04-08 15:44:54 -0400

    working

--- openmdao.lib/src/openmdao/lib/components/pareto_filter.py
+++ openmdao.lib/src/openmdao/lib/components/pareto_filter.py
@@ -1,18 +1,50 @@
 """ Pareto Filter -- finds non-dominated cases. """
 
 # pylint: disable-msg=E0611,F0401
-from openmdao.main.datatypes.api import Instance, Slot, List, Str
-from openmdao.lib.casehandlers.api import CaseSet, caseiter_to_caseset
+from openmdao.main.datatypes.api import List, VarTree
+from openmdao.main.api import Component
 
-from openmdao.main.component import Component
-from openmdao.main.interfaces import ICaseIterator
-
-
-class ParetoFilterBase(Component):
+class ParetoFilter(Component):
+    """Takes a set of cases and filters out the subset of cases which are
+    pareto optimal. Assumes that smaller values for model responses are
+    better, so all problems must be posed as minimization problems.
     """
-    Base functionality for a pareto filter.
-    Not to be instantiated directly. Should be subclassed.
-    """
+
+    # pylint: disable-msg=E1101
+    criteria = List(Str, iotype="in",
+                    desc="List of outputs from the case to consider for "
+                         "filtering. Note that only case outputs are allowed as "
+                         "criteria.")
+
+    params = VarTree(VariableTree(), iotype='in')
+
+    responses = VarTree(VariableTree(), iotype='in')
+
+    def __init__(self, params=None, responses=None):
+        super(ParetoFilter, self).__init__()
+
+        if params is None or not isinstance(params, tuple):
+            msg = "ParetoFilter params argument needs to be a tuple of " + \
+                  "variable names."
+            self.raise_exception(msg, ValueError)
+
+        if responses is None or not isinstance(responses, tuple):
+            msg = "ParetoFilter responses argument needs to be a tuple of " + \
+                  "variable names."
+            self.raise_exception(msg, ValueError)
+
+        # Inputs and Outputs created immediately.
+
+        input_tree = self.get('params')
+        self._param_data = []
+        for name in params:
+            input_tree.add(name, Array([0.0], desc='ParetoFilter param'))
+
+        output_tree = self.get('responses')
+        self._response_data = {}
+        for name in responses:
+            output_tree.add(name, Array([0.0], desc='ParetoFilter response'))
+
 
     def _is_dominated(self, y1, y2):
         """Tests to see if the point y1 is dominated by the point y2.
@@ -69,112 +101,174 @@ class ParetoFilterBase(Component):
             if not dominated:
                 self.pareto_set.record(case)
 
-class ConnectableParetoFilter(ParetoFilterBase):
-    """
-    Same functionality as ParetoFilter but without slots.
-    Allows for issuing connections to pareto_set and dominated_set
-    """
 
-    criteria = List(Str, iotype="in",
-                    desc="List of outputs from the case to consider for "
-                         "filtering. Note that only case outputs are allowed as "
-                         "criteria.")
+#class ParetoFilterBase(Component):
+    #"""
+    #Base functionality for a pareto filter.
+    #Not to be instantiated directly. Should be subclassed.
+    #"""
 
-    #case_set = Slot(ICaseIterator,
-    #                    desc="CaseIterator with the cases to be filtered to "
-    #                         "Find the pareto optimal subset.")
+    #def _is_dominated(self, y1, y2):
+        #"""Tests to see if the point y1 is dominated by the point y2.
+        #True if y1 is dominated by y2, False otherwise.
+        #"""
+        #if y1 == y2:
+            #return False
+        #for a, b in zip(y1, y2):
+            #if a < b:
+                #return False
 
-    case_sets = List(Instance(ICaseIterator), value=[], iotype="in",
-                     desc="CaseSet with the cases to be filtered to "
-                     "find the pareto optimal subset.")
+        #return True
 
-    pareto_set = Instance(CaseSet, iotype="out",
-                        desc="Resulting collection of pareto optimal cases.", copy="shallow")
+    #def execute(self):
+        #"""Finds and removes pareto optimal points in the given case set.
+        #Returns a list of pareto optimal points. Smaller is better for all
+        #criteria.
+        #"""
+        ##convert stuff to caseSets if they are not
+        #case_sets = []
+        #for ci in self.case_sets:
+            #if not isinstance(ci, CaseSet):
+                #case_sets.append(caseiter_to_caseset(ci))
+            #else:
+                #case_sets.append(ci)
 
-    dominated_set = Instance(CaseSet, iotype="out",
-                           desc="Resulting collection of dominated cases.", copy="shallow")
+        #y_list = []
+        #if len(case_sets) > 1:
+            #case_set = case_sets[0].union(*case_sets[1:])
+        #else:
+            #case_set = case_sets[0]
+        #criteria_count = len(self.criteria)
 
-class ParetoFilter(ParetoFilterBase):
-    """Takes a set of cases and filters out the subset of cases which are
-    pareto optimal. Assumes that smaller values for model responses are
-    better, so all problems must be posed as minimization problems.
-    """
+        #try:
+            ## need to transpose the list of outputs
+            #y_list = zip(*[case_set[crit] for crit in self.criteria])
+        #except KeyError:
+            #self.raise_exception('no cases provided had all of the outputs '
+                 #'matching the provided criteria, %s' % self.criteria, ValueError)
 
-    # pylint: disable-msg=E1101
-    criteria = List(Str, iotype="in",
-                    desc="List of outputs from the case to consider for "
-                         "filtering. Note that only case outputs are allowed as "
-                         "criteria.")
+        #y_temp = list(y_list)
 
-    #case_set = Slot(ICaseIterator,
-    #                    desc="CaseIterator with the cases to be filtered to "
-    #                         "Find the pareto optimal subset.")
+        #self.dominated_set = CaseSet()
+        #self.pareto_set = CaseSet()  # TODO: need a way to copy casesets
 
-    case_sets = List(Slot(ICaseIterator), value=[], iotype="in",
-                     desc="CaseSet with the cases to be filtered to "
-                     "find the pareto optimal subset.")
+        #for point1, case in zip(y_list, iter(case_set)):
+            #dominated = False
+            #for point2 in y_temp:
+                #if self._is_dominated(point1, point2):
+                    #self.dominated_set.record(case)
+                    #y_temp.remove(point1)
+                    #dominated = True
+                    #break
+            #if not dominated:
+                #self.pareto_set.record(case)
 
-    pareto_set = Slot(CaseSet,
-                        desc="Resulting collection of pareto optimal cases.", copy="shallow")
+#class ConnectableParetoFilter(ParetoFilterBase):
+    #"""
+    #Same functionality as ParetoFilter but without slots.
+    #Allows for issuing connections to pareto_set and dominated_set
+    #"""
 
-    dominated_set = Slot(CaseSet,
-                           desc="Resulting collection of dominated cases.", copy="shallow")
+    #criteria = List(Str, iotype="in",
+                    #desc="List of outputs from the case to consider for "
+                         #"filtering. Note that only case outputs are allowed as "
+                         #"criteria.")
 
-if __name__ == "__main__":  # pragma: no cover
+    ##case_set = Slot(ICaseIterator,
+    ##                    desc="CaseIterator with the cases to be filtered to "
+    ##                         "Find the pareto optimal subset.")
 
-    # pylint: disable-msg=C0103, E1101
+    #case_sets = List(Instance(ICaseIterator), value=[], iotype="in",
+                     #desc="CaseSet with the cases to be filtered to "
+                     #"find the pareto optimal subset.")
 
-    from matplotlib import pyplot as py
-    from mpl_toolkits.mplot3d import Axes3D
-    from numpy import random
-    random.seed(10)
+    #pareto_set = Instance(CaseSet, iotype="out",
+                        #desc="Resulting collection of pareto optimal cases.", copy="shallow")
 
-    from openmdao.main.case import Case
-    pf = ParetoFilter()
+    #dominated_set = Instance(CaseSet, iotype="out",
+                           #desc="Resulting collection of dominated cases.", copy="shallow")
 
-    # 2D PARETO FILTERING EXAMPLE
-    n = 1000
-    x = random.uniform(-1, 0, n)
-    y = -(1 - x**2)**0.5 * random.random(n)
-    cases = CaseSet()
-    for x_0, y_0 in zip(x, y):
-        cases.record(Case(inputs=[("x", x_0), ("y", y_0)]))
+#class ParetoFilter(ParetoFilterBase):
+    #"""Takes a set of cases and filters out the subset of cases which are
+    #pareto optimal. Assumes that smaller values for model responses are
+    #better, so all problems must be posed as minimization problems.
+    #"""
 
-    pf.case_sets = [cases]
-    pf.criteria = ['x', 'y']
-    pf.execute()
+    ## pylint: disable-msg=E1101
+    #criteria = List(Str, iotype="in",
+                    #desc="List of outputs from the case to consider for "
+                         #"filtering. Note that only case outputs are allowed as "
+                         #"criteria.")
 
-    x_p, y_p = pf.pareto_set['x'], pf.pareto_set['y']
-    x_dom, y_dom = pf.dominated_set['x'], pf.dominated_set['y']
+    ##case_set = Slot(ICaseIterator,
+    ##                    desc="CaseIterator with the cases to be filtered to "
+    ##                         "Find the pareto optimal subset.")
 
-    py.figure()
-    py.scatter(x, y, s=5)
-    py.scatter(x_dom, y_dom, c='', edgecolor='b', s=80)
-    py.scatter(x_p, y_p, c='', edgecolors='r', s=80)
+    #case_sets = List(Slot(ICaseIterator), value=[], iotype="in",
+                     #desc="CaseSet with the cases to be filtered to "
+                     #"find the pareto optimal subset.")
 
-    #3D PARETO FILTERING EXAMPLE
-    n = 1000
-    x = random.uniform(-1, 0, n)
-    y = -(1 - x**2)**0.5 * random.random(n)
-    z = -(1 - x**2 - y**2)**0.5 * random.random(n)
-    doe = zip(x, y, z)
+    #pareto_set = Slot(CaseSet,
+                        #desc="Resulting collection of pareto optimal cases.", copy="shallow")
 
-    pf.criteria = ['x', 'y', 'z']
+    #dominated_set = Slot(CaseSet,
+                           #desc="Resulting collection of dominated cases.", copy="shallow")
 
-    cases = CaseSet()
-    for x_0, y_0, z_0 in zip(x, y, z):
-        cases.record(Case(inputs=[("x", x_0),
-                                   ("y", y_0),
-                                   ("z", z_0)]))
+#if __name__ == "__main__":  # pragma: no cover
 
-    pf.case_sets = [cases]
-    pf.execute()
+    ## pylint: disable-msg=C0103, E1101
 
-    x_p, y_p, z_p = pf.pareto_set['x'], pf.pareto_set['y'], pf.pareto_set['z']
-    x_dom, y_dom, z_dom = pf.dominated_set['x'], pf.dominated_set['y'], pf.dominated_set['z']
-    fig1 = py.figure()
-    a1 = Axes3D(fig1)
-    a1.scatter(x_dom, y_dom, z_dom, c='b')
-    a1.scatter(x_p, y_p, z_p, c='r', edgecolor='r')
+    #from matplotlib import pyplot as py
+    #from mpl_toolkits.mplot3d import Axes3D
+    #from numpy import random
+    #random.seed(10)
 
-    py.show()
+    #from openmdao.main.case import Case
+    #pf = ParetoFilter()
+
+    ## 2D PARETO FILTERING EXAMPLE
+    #n = 1000
+    #x = random.uniform(-1, 0, n)
+    #y = -(1 - x**2)**0.5 * random.random(n)
+    #cases = CaseSet()
+    #for x_0, y_0 in zip(x, y):
+        #cases.record(Case(inputs=[("x", x_0), ("y", y_0)]))
+
+    #pf.case_sets = [cases]
+    #pf.criteria = ['x', 'y']
+    #pf.execute()
+
+    #x_p, y_p = pf.pareto_set['x'], pf.pareto_set['y']
+    #x_dom, y_dom = pf.dominated_set['x'], pf.dominated_set['y']
+
+    #py.figure()
+    #py.scatter(x, y, s=5)
+    #py.scatter(x_dom, y_dom, c='', edgecolor='b', s=80)
+    #py.scatter(x_p, y_p, c='', edgecolors='r', s=80)
+
+    ##3D PARETO FILTERING EXAMPLE
+    #n = 1000
+    #x = random.uniform(-1, 0, n)
+    #y = -(1 - x**2)**0.5 * random.random(n)
+    #z = -(1 - x**2 - y**2)**0.5 * random.random(n)
+    #doe = zip(x, y, z)
+
+    #pf.criteria = ['x', 'y', 'z']
+
+    #cases = CaseSet()
+    #for x_0, y_0, z_0 in zip(x, y, z):
+        #cases.record(Case(inputs=[("x", x_0),
+                                   #("y", y_0),
+                                   #("z", z_0)]))
+
+    #pf.case_sets = [cases]
+    #pf.execute()
+
+    #x_p, y_p, z_p = pf.pareto_set['x'], pf.pareto_set['y'], pf.pareto_set['z']
+    #x_dom, y_dom, z_dom = pf.dominated_set['x'], pf.dominated_set['y'], pf.dominated_set['z']
+    #fig1 = py.figure()
+    #a1 = Axes3D(fig1)
+    #a1.scatter(x_dom, y_dom, z_dom, c='b')
+    #a1.scatter(x_p, y_p, z_p, c='r', edgecolor='r')
+
+    #py.show()
--- openmdao.lib/src/openmdao/lib/components/test/test_metamodel.py
+++ openmdao.lib/src/openmdao/lib/components/test/test_metamodel.py
@@ -18,75 +18,76 @@ from openmdao.util.testutil import assert_rel_error
 class MetaModelTestCase(unittest.TestCase):
 
     def test_basics(self):
-        
+
         model = set_as_top(Assembly())
-        model.add('meta', MetaModel(params=('x1', 'x2'), 
+        model.add('meta', MetaModel(params=('x1', 'x2'),
                                     responses=('y1', 'y2')))
         model.driver.workflow.add('meta')
-        
+
         model.meta.params.x1 = [1.0, 2.0, 3.0]
         model.meta.params.x2 = [1.0, 3.0, 4.0]
         model.meta.responses.y1 = [3.0, 2.0, 1.0]
         model.meta.responses.y2 = [1.0, 4.0, 7.0]
-    
+
         model.meta.default_surrogate = ResponseSurface()
-    
+
         model.meta.x1 = 2.0
         model.meta.x2 = 3.0
         model.meta.run()
         assert_rel_error(self, model.meta.y1, 2.0, .00001)
         assert_rel_error(self, model.meta.y2, 4.0, .00001)
-        
+
         model.meta.x1 = 2.5
         model.meta.x2 = 3.5
         model.meta.run()
         assert_rel_error(self, model.meta.y1, 1.5934, .001)
-        
+
         # Slot a new surrogate.
         model.meta.default_surrogate = FloatKrigingSurrogate()
-        
+
         self.assertTrue(model.meta._train == True)
         model.meta.run()
         assert_rel_error(self, model.meta.y1, 1.4609, .001)
-        
-    def test_array_inputs(self):
-        
-        model = set_as_top(Assembly())
-        model.add('meta', MetaModel(params=('x',), 
-                                    responses=('y1', 'y2')))
-        model.driver.workflow.add('meta')
-        
-        model.meta.params.x = [[1.0, 1.0, 1.0, 1.0], 
-                               [2.0, 1.0, 1.0, 1.0],
-                               [1.0, 2.0, 1.0, 1.0],
-                               [1.0, 1.0, 2.0, 1.0],
-                               [1.0, 1.0, 1.0, 2.0]]
-        model.meta.responses.y1 = [3.0, 2.0, 1.0, 6.0, -2.0]
-        model.meta.responses.y2 = [1.0, 4.0, 7.0, -3.0, 3.0]
-    
-        model.meta.default_surrogate = ResponseSurface()
-    
-        model.meta.x1 = [[1.0, 2.0], [1.0, 1.0]]
-        model.meta.run()
-        assert_rel_error(self, model.meta.y1, 1.0, .00001)
-        assert_rel_error(self, model.meta.y2, 7.0, .00001)
-        
-        model.meta.x1 = 2.5
-        model.meta.x2 = 3.5
-        model.meta.run()
-        assert_rel_error(self, model.meta.y1, 1.5934, .001)
-        
-        # Slot a new surrogate.
-        model.meta.default_surrogate = FloatKrigingSurrogate()
-        
-        self.assertTrue(model.meta._train == True)
-        model.meta.run()
-        assert_rel_error(self, model.meta.y1, 1.4609, .001)
-        
+
+    # Array param not supported yet. - KTM
+    #def test_array_inputs(self):
+
+        #model = set_as_top(Assembly())
+        #model.add('meta', MetaModel(params=('x',),
+                                    #responses=('y1', 'y2')))
+        #model.driver.workflow.add('meta')
+
+        #model.meta.params.x = [[1.0, 1.0, 1.0, 1.0],
+                               #[2.0, 1.0, 1.0, 1.0],
+                               #[1.0, 2.0, 1.0, 1.0],
+                               #[1.0, 1.0, 2.0, 1.0],
+                               #[1.0, 1.0, 1.0, 2.0]]
+        #model.meta.responses.y1 = [3.0, 2.0, 1.0, 6.0, -2.0]
+        #model.meta.responses.y2 = [1.0, 4.0, 7.0, -3.0, 3.0]
+
+        #model.meta.default_surrogate = ResponseSurface()
+
+        #model.meta.x1 = [[1.0, 2.0], [1.0, 1.0]]
+        #model.meta.run()
+        #assert_rel_error(self, model.meta.y1, 1.0, .00001)
+        #assert_rel_error(self, model.meta.y2, 7.0, .00001)
+
+        #model.meta.x1 = 2.5
+        #model.meta.x2 = 3.5
+        #model.meta.run()
+        #assert_rel_error(self, model.meta.y1, 1.5934, .001)
+
+        ## Slot a new surrogate.
+        #model.meta.default_surrogate = FloatKrigingSurrogate()
+
+        #self.assertTrue(model.meta._train == True)
+        #model.meta.run()
+        #assert_rel_error(self, model.meta.y1, 1.4609, .001)
+
     def test_warm_start(self):
-        
+
         model = set_as_top(Assembly())
-        model.add('meta', MetaModel(params=('x1', 'x2'), 
+        model.add('meta', MetaModel(params=('x1', 'x2'),
                                     responses=('y1', 'y2')))
         model.driver.workflow.add('meta')
         model.meta.default_surrogate = ResponseSurface()
@@ -96,9 +97,9 @@ class MetaModelTestCase(unittest.TestCase):
         model.meta.params.x2 = [1.0, 4.0]
         model.meta.responses.y1 = [3.0, 1.0]
         model.meta.responses.y2 = [1.0, 7.0]
-    
+
         model.meta.default_surrogate = ResponseSurface()
-    
+
         model.meta.x1 = 2.0
         model.meta.x2 = 3.0
         model.meta.run()
@@ -107,21 +108,21 @@ class MetaModelTestCase(unittest.TestCase):
 
         # Adding the 3rd point moves the estimate for that point
         # back to where it should be.
-        
+
         model.meta.params.x1 = [2.0]
         model.meta.params.x2 = [3.0]
         model.meta.responses.y1 = [2.0]
         model.meta.responses.y2 = [4.0]
-    
+
         self.assertTrue(model.meta._train == True)
         model.meta.run()
         assert_rel_error(self, model.meta.y1, 2.0, .00001)
         assert_rel_error(self, model.meta.y2, 4.0, .00001)
 
     def test_multi_surrogate_models_bad_surrogate_dict(self):
-        
+
         model = set_as_top(Assembly())
-        model.add('meta', MetaModel(params=('x1', 'x2'), 
+        model.add('meta', MetaModel(params=('x1', 'x2'),
                                     responses=('y1', 'y2')))
         model.driver.workflow.add('meta')
         model.meta.warm_restart = True
@@ -130,7 +131,7 @@ class MetaModelTestCase(unittest.TestCase):
         model.meta.params.x2 = [1.0, 4.0]
         model.meta.responses.y1 = [3.0, 1.0]
         model.meta.responses.y2 = [1.0, 7.0]
-    
+
         model.meta.surrogates[ 'y1' ] = KrigingSurrogate()
         try:
             model.meta.run()
@@ -142,9 +143,9 @@ class MetaModelTestCase(unittest.TestCase):
             self.fail('ValueError expected')
 
     def test_multi_surrogate_models(self):
-        
+
         model = set_as_top(Assembly())
-        model.add('meta', MetaModel(params=('x1', 'x2'), 
+        model.add('meta', MetaModel(params=('x1', 'x2'),
                                     responses=('y1', 'y2')))
         model.driver.workflow.add('meta')
         model.meta.warm_restart = True
@@ -153,7 +154,7 @@ class MetaModelTestCase(unittest.TestCase):
         model.meta.params.x2 = [1.0, 4.0]
         model.meta.responses.y1 = [3.0, 1.0]
         model.meta.responses.y2 = [1.0, 7.0]
-    
+
         model.meta.surrogates['y1'] = KrigingSurrogate()
         model.meta.surrogates['y2'] = LogisticRegression()
 
@@ -173,11 +174,11 @@ class MetaModelTestCase(unittest.TestCase):
         model.meta.default_surrogate = KrigingSurrogate()
         model.meta.surrogates['y1'] = None
         model.meta.surrogates['y2'] = None
-        
+
         model.meta.run()
         self.assertTrue(isinstance(model.meta.y1, NormalDistribution))
         self.assertTrue(isinstance(model.meta.y2, NormalDistribution))
-        
+
         model.meta.surrogates['y1'] = LogisticRegression()
 
         model.meta.run()
