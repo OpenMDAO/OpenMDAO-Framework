a4080edf886b1895f798817af4feaf2a9ea26ab0
htaft <htaft@harpie.(none)>
2014-05-01 11:11:41 -0400

    Trying to fix _create_releaseinfo_file

--- openmdao_devtools/src/openmdao/devtools/releasetools.py
+++ openmdao_devtools/src/openmdao/devtools/releasetools.py
@@ -92,7 +92,7 @@ def _get_releaseinfo_str(version):
 
 def _create_releaseinfo_file(projname, relinfo_str):
     """Creates a releaseinfo.py file in the current directory"""
-    os.chdir(os.path.join(*dirs))
+    os.chdir(projname)
     print 'updating releaseinfo.py for %s' % projname
     with open('releaseinfo.py', 'w') as f:
         f.write(relinfo_str)
