755ccefacd53eb18c3b143ff3a81bf4b74a869a2
CJ <pyvelepor@gmail.com>
2014-06-03 13:05:58 -0400

    Added profiler based off of line_profiler

--- openmdao.devtools/src/openmdao/devtools/utils.py
+++ openmdao.devtools/src/openmdao/devtools/utils.py
@@ -480,3 +480,30 @@ def do_cprofile(func):
         finally:
             profile.print_stats()
     return profiled_func
+
+try:
+    from line_profiler import LineProfiler
+
+    def do_profile(follow=[]):
+        def inner(func):
+            def profiled_func(*args, **kwargs):
+                try:
+                    profiler = LineProfiler()
+                    profiler.add_function(func)
+                    for f in follow:
+                        profiler.add_function(f)
+                    profiler.enable_by_count()
+                    return func(*args, **kwargs)
+                finally:
+                    profiler.print_stats()
+            return profiled_func
+        return inner
+
+except ImportError:
+    def do_profile(follow=[]):
+        "Helpful if you accidentally leave in production!"
+        def inner(func):
+            def nothing(*args, **kwargs):
+                return func(*args, **kwargs)
+            return nothing
+        return inner
