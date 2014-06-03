bcbf62053bbed2aa3fa96e6fe082d90c34227961
CJ <pyvelepor@gmail.com>
2014-06-03 12:52:36 -0400

    Added method for profiling

--- openmdao.devtools/src/openmdao/devtools/utils.py
+++ openmdao.devtools/src/openmdao/devtools/utils.py
@@ -468,3 +468,14 @@ class Timer(object):
             return wrapper
         return decorator
 
+def do_cprofile(func):
+    def profiled_func(*args, **kwargs):
+        profile = cProfile.Profile()
+        try:
+            profile.enable()
+            result = func(*args, **kwargs)
+            profile.disable()
+            return result
+        finally:
+            profile.print_stats()
+    return profiled_func
