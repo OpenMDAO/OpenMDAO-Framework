82adc707a43772383b31aafa1713b68e7f2d353c
Scott Townsend <scott.e.townsend@nasa.gov>
2014-06-06 16:25:17 -0400

    Added BSONCaseRecorder.

--- openmdao.lib/src/openmdao/lib/casehandlers/jsoncase.py
+++ openmdao.lib/src/openmdao/lib/casehandlers/jsoncase.py
@@ -1,54 +1,44 @@
+"""
+JSON/BSON Case Recording.
+"""
+
 import cStringIO
 import StringIO
 import logging
 import sys
 import time
 
-from json  import dumps, JSONEncoder
-from numpy import ndarray
-from uuid  import uuid1
+import json
+import bson
+
+from numpy  import ndarray
+from struct import pack
+from uuid   import uuid1
 
 from openmdao.main.interfaces import implements, ICaseRecorder
 from openmdao.main.releaseinfo import __version__
 
 
-class JSONCaseRecorder(object):
-    """Dumps a run in JSON form to `out`, which may be a string or a
-    file-like object (defaults to ``stdout``). If `out` is ``stdout`` or
-    ``stderr``, then that standard stream is used. Otherwise, if `out` is a
-    string, then a file with that name will be opened in the current directory.
-    If `out` is None, cases will be ignored.
-    """
+class _BaseRecorder(object):
+    """ Base class for JSONRecorder and BSONRecorder. """
 
     implements(ICaseRecorder)
 
-    def __init__(self, out='stdout', indent=4, sort_keys=True):
-        if isinstance(out, basestring):
-            if out == 'stdout':
-                out = sys.stdout
-            elif out == 'stderr':
-                out = sys.stderr
-            else:
-                out = open(out, 'w')
-        self.out = out
-        self.indent = indent
-        self.sort_keys = sort_keys
+    def __init__(self):
         self._cfg_map = {}
         self._uuid = None
         self._cases = None
 
     def startup(self):
-        """Prepare for new run."""
+        """ Prepare for new run. """
         pass
 
     def register(self, driver, inputs, outputs):
-        """Register names for later record call from `driver`."""
+        """ Register names for later record call from `driver`. """
         self._cfg_map[driver] = (driver.get_pathname(), inputs, outputs)
 
-    def record_constants(self, constants):
-        """Record constant data."""
-        if not self.out:  # if self.out is None, just do nothing
-            return
+    def get_simulation_info(self, constants):
+        """ Return simulation info dictionary. """
 
         # Locate top level assembly from first driver registered.
         top = self._cfg_map.keys()[0].parent
@@ -119,26 +109,18 @@ class JSONCaseRecorder(object):
                             pcomp_name=prefix+con.pcomp_name)
                 expressions[prefix+str(con)] = info
 
-        # Write simulation info.
         self._uuid = str(uuid1())
         self._cases = 0
 
-        info = dict(
-            variable_metadata=variable_metadata,
-            expressions=expressions,
-            constants=constants,
-            OpenMDAO_Version=__version__,
-            uuid=self._uuid)
+        return dict(variable_metadata=variable_metadata,
+                    expressions=expressions,
+                    constants=constants,
+                    OpenMDAO_Version=__version__,
+                    uuid=self._uuid)
 
-        category = 'simulation_info'
-        data = self._dump(info, category,
-                          ('variable_metadata', 'expressions', 'constants'))
-        self.out.write('{\n"%s": ' % category)
-        self.out.write(data)
-        self.out.write('\n')
-
-        # Write info for each driver.
-        count = 0
+    def get_driver_info(self):
+        """ Return list of driver info dictionaries. """
+        driver_info = []
         for driver, (dname, ins, outs) in sorted(self._cfg_map.items(),
                                                  key=lambda item: item[1][0]):
             info = dict(name=dname)
@@ -157,9 +139,62 @@ class JSONCaseRecorder(object):
             if hasattr(driver, 'get_eq_constraints'):
                 info['eq_constraints'] = \
                     [str(con) for con in driver.get_eq_constraints().values()]
+            driver_info.append(info)
+        return driver_info
 
-            count += 1
-            category = 'driver_info_%s' % count
+    def get_case_info(self, driver, inputs, outputs, exc,
+                      case_uuid, parent_uuid):
+        """ Return case info dictionary. """
+        dname, in_names, out_names = self._cfg_map[driver]
+        data = dict(zip(in_names, inputs))
+        data.update(zip(out_names, outputs))
+
+        return dict(_id=case_uuid,
+                    _parent_id=parent_uuid or self._uuid,
+                    _driver_id=dname,
+                    error_status=None,
+                    error_message=str(exc) if exc else '',
+                    timestamp=time.time(),
+                    data=data)
+
+
+class JSONCaseRecorder(_BaseRecorder):
+    """
+    Dumps a run in JSON form to `out`, which may be a string or a file-like
+    object (defaults to ``stdout``). If `out` is ``stdout`` or ``stderr``,
+    then that standard stream is used. Otherwise, if `out` is a string, then
+    a file with that name will be opened in the current directory.
+    If `out` is None, cases will be ignored.
+    """
+
+    def __init__(self, out='stdout', indent=4, sort_keys=True):
+        super(JSONCaseRecorder, self).__init__()
+        if isinstance(out, basestring):
+            if out == 'stdout':
+                out = sys.stdout
+            elif out == 'stderr':
+                out = sys.stderr
+            else:
+                out = open(out, 'w')
+        self.out = out
+        self.indent = indent
+        self.sort_keys = sort_keys
+
+    def record_constants(self, constants):
+        """ Record constant data. """
+        if not self.out:  # if self.out is None, just do nothing
+            return
+
+        info = self.get_simulation_info(constants)
+        category = 'simulation_info'
+        data = self._dump(info, category,
+                          ('variable_metadata', 'expressions', 'constants'))
+        self.out.write('{\n"%s": ' % category)
+        self.out.write(data)
+        self.out.write('\n')
+
+        for i, info in enumerate(self.get_driver_info()):
+            category = 'driver_info_%s' % (i+1)
             data = self._dump(info, category)
             self.out.write(', "%s": ' % category)
             self.out.write(data)
@@ -168,25 +203,12 @@ class JSONCaseRecorder(object):
         self.out.flush()
 
     def record(self, driver, inputs, outputs, exc, case_uuid, parent_uuid):
-        """Dump the given run data in a "pretty" form."""
+        """ Dump the given run data in a "pretty" form. """
         if not self.out:  # if self.out is None, just do nothing
             return
 
-        # Collect case data.
-        dname, in_names, out_names = self._cfg_map[driver]
-        data = dict(zip(in_names, inputs))
-        data.update(zip(out_names, outputs))
-
-        # Write case.
-        info = dict(
-            _id=case_uuid,
-            _parent_id=parent_uuid or self._uuid,
-            _driver_id=dname,
-            error_status=None,
-            error_message=str(exc) if exc else '',
-            timestamp=time.time(),
-            data=data)
-
+        info = self.get_case_info(driver, inputs, outputs, exc,
+                                  case_uuid, parent_uuid)
         self._cases += 1
         category = 'iteration_case_%s' % self._cases
         data = self._dump(info, category, ('data',))
@@ -196,17 +218,19 @@ class JSONCaseRecorder(object):
         self.out.flush()
 
     def _dump(self, info, category, subcategories=None):
-        """Return JSON data, report any bad keys & values encountered."""
+        """ Return JSON data, report any bad keys & values encountered. """
         try:
-            return dumps(info, indent=self.indent, sort_keys=self.sort_keys,
-                         cls=Encoder, check_circular=False)
+            return json.dumps(info, indent=self.indent,
+                              sort_keys=self.sort_keys,
+                              cls=Encoder, check_circular=False)
         except Exception as exc:
             # Log bad keys & values.
             bad = []
             for key in sorted(info):
                 try:
-                    dumps(info[key], indent=self.indent, sort_keys=self.sort_keys,
-                          cls=Encoder, check_circular=False)
+                    json.dumps(info[key], indent=self.indent,
+                               sort_keys=self.sort_keys,
+                               cls=Encoder, check_circular=False)
                 except Exception:
                     bad.append(key)
 
@@ -218,8 +242,9 @@ class JSONCaseRecorder(object):
                 bad = []
                 for key in sorted(info):
                     try:
-                        dumps(info[key], indent=self.indent, sort_keys=self.sort_keys,
-                              cls=Encoder, check_circular=False)
+                        json.dumps(info[key], indent=self.indent,
+                                   sort_keys=self.sort_keys,
+                                   cls=Encoder, check_circular=False)
                     except Exception:
                         bad.append(key)
 
@@ -232,8 +257,10 @@ class JSONCaseRecorder(object):
             raise RuntimeError(msg)
 
     def close(self):
-        """Closes `out` unless it's ``sys.stdout`` or ``sys.stderr``.
-        Note that a closed recorder will do nothing in :meth:`record`."""
+        """
+        Closes `out` unless it's ``sys.stdout`` or ``sys.stderr``.
+        Note that a closed recorder will do nothing in :meth:`record`.
+        """
         if self.out is not None and self._cases is not None:
             self.out.write('}\n')
 
@@ -247,10 +274,7 @@ class JSONCaseRecorder(object):
         self._cases = None
 
     def get_attributes(self, io_only=True):
-        """ We need a custom get_attributes because we aren't using Traits to
-        manage our changeable settings. This is unfortunate and should be
-        changed to something that automates this somehow."""
-
+        """ Return attribute dictionary for GUI. """
         attrs = {}
         attrs['type'] = type(self).__name__
         variables = []
@@ -275,16 +299,127 @@ class JSONCaseRecorder(object):
         return attrs
 
     def get_iterator(self):
-        """Just returns None."""
+        """ Just returns None. """
         return None
 
 
-class Encoder(JSONEncoder):
-    """Special encoder to deal with types not handled by default encoder."""
+class Encoder(json.JSONEncoder):
+    """ Special encoder to deal with types not handled by default encoder. """
 
     def default(self, obj):
         if isinstance(obj, ndarray):
             return obj.tolist()
         else:
             super(Encoder, self).default(obj)
+
+
+class BSONCaseRecorder(_BaseRecorder):
+    """
+    Dumps a run in BSON form to `out`, which may be a string or a file-like
+    object. If `out` is a string, then a file with that name will be opened
+    in the current directory. If `out` is None, cases will be ignored.
+    """
+
+    def __init__(self, out):
+        super(BSONCaseRecorder, self).__init__()
+        if isinstance(out, basestring):
+            out = open(out, 'w')
+        self.out = out
+
+    def record_constants(self, constants):
+        """ Record constant data. """
+        if not self.out:  # if self.out is None, just do nothing
+            return
+
+        info = self.get_simulation_info(constants)
+        category = 'simulation_info'
+        data = self._dump(info, category,
+                          ('variable_metadata', 'expressions', 'constants'))
+        reclen = pack('<L', len(data))
+        self.out.write(reclen)
+        self.out.write(data)
+        self.out.write(reclen)
+
+        for i, info in enumerate(self.get_driver_info()):
+            category = 'driver_info_%s' % (i+1)
+            data = self._dump(info, category)
+            reclen = pack('<L', len(data))
+            self.out.write(reclen)
+            self.out.write(data)
+            self.out.write(reclen)
+
+        self.out.flush()
+
+    def record(self, driver, inputs, outputs, exc, case_uuid, parent_uuid):
+        """ Dump the given run data in a "pretty" form. """
+        if not self.out:  # if self.out is None, just do nothing
+            return
+
+        info = self.get_case_info(driver, inputs, outputs, exc,
+                                  case_uuid, parent_uuid)
+        self._cases += 1
+        category = 'iteration_case_%s' % self._cases
+        data = self._dump(info, category, ('data',))
+        reclen = pack('<L', len(data))
+        self.out.write(reclen)
+        self.out.write(data)
+        self.out.write(reclen)
+        self.out.flush()
+
+    def _dump(self, info, category, subcategories=None):
+        """ Return BSON data, report any bad keys & values encountered. """
+        try:
+            return bson.dumps(info)
+        except Exception as exc:
+            # Log bad keys & values.
+            bad = []
+            for key in sorted(info):
+                try:
+                    bson.dumps(info[key])
+                except Exception:
+                    bad.append(key)
+
+            # If it's in a subcategory we only report the first subcategory.
+            if subcategories is not None and bad[0] in subcategories:
+                key = bad[0]
+                category = '.'.join((category, key))
+                info = info[key]
+                bad = []
+                for key in sorted(info):
+                    try:
+                        bson.dumps(info[key])
+                    except Exception:
+                        bad.append(key)
+
+            msg = 'BSON write failed for %s:' % category
+            logging.error(msg)
+            for key in bad:
+                logging.error('    %s: %s', key, info[key])
+
+            msg = '%s keys %s: %s' % (msg, bad, exc)
+            raise RuntimeError(msg)
+
+    def close(self):
+        """
+        Closes `out`. Note that a closed recorder will do nothing in
+        :meth:`record`.
+        """
+        if self.out is not None:
+            if not isinstance(self.out,
+                              (StringIO.StringIO, cStringIO.OutputType)):
+                # Closing a StringIO deletes its contents.
+                self.out.close()
+            self.out = None
+
+        self._cases = None
+
+    def get_attributes(self, io_only=True):
+        """ Return attribute dictionary for GUI. """
+        attrs = {}
+        attrs['type'] = type(self).__name__
+        return attrs
+
+    def get_iterator(self):
+        """ Just returns None. """
+        return None
 
