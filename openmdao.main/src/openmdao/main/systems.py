7e7a2b326a850ad2d57eb25804a5cb7d937db911
Bret Naylor <naylor.b@gmail.com>
2014-08-12 10:23:20 -0400

    one mpi test passing

--- openmdao.main/src/openmdao/main/systems.py
+++ openmdao.main/src/openmdao/main/systems.py
@@ -973,9 +973,9 @@ class EqConstraintSystem(SimpleSystem):
     def run(self, iterbase, ffd_order=0, case_label='', case_uuid=None):
         if self.is_active():
             super(EqConstraintSystem, self).run(iterbase, ffd_order, case_label, case_uuid)
-            # if self._mapped_resids: # run implicit
-            #     state = self._mapped_resids[self.scope.name2collapsed[self.name+'.out0']]
-            #     self.vec['f'][state][:] = self._comp.out0
+            if self._mapped_resids: # run implicit
+                state = self._mapped_resids[self.scope.name2collapsed[self.name+'.out0']]
+                self.vec['f'][state][:] = self._comp.out0
 
 
 class AssemblySystem(SimpleSystem):
