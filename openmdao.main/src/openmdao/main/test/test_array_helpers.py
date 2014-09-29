ccbf7f5adad6b5c5b9488437488a9091f5c92e94
Kenneth-T-Moore <kenneth.t.moore-1@nasa.gov>
2014-09-29 11:35:50 -0400

    small fix

--- openmdao.main/src/openmdao/main/test/test_array_helpers.py
+++ openmdao.main/src/openmdao/main/test/test_array_helpers.py
@@ -6,7 +6,7 @@ import unittest
 from openmdao.main.array_helpers import flatten_slice, get_flattened_index
 
 class TestcaseArrayHelpers(unittest.TestCase):
-    
+
 
     def test_general_flatten_slice(self):
         # Test capability to flatten any slice.
@@ -59,13 +59,17 @@ class TestcaseArrayHelpers(unittest.TestCase):
         try:
             self.assertEqual(0, get_flattened_index(10, (10,10)))
         except IndexError as err:
-            self.assertEqual(str(err), "index out of bounds")
+            # Some versions of numpy have slightly different messages, so as
+            # long as it is an index error, we are fine.
+            pass
+        else:
+            self.fail('Should get an Indexerror')
 
         self.assertEqual(slice(22,23,None), get_flattened_index((2,2), (10,10)))
         self.assertEqual(slice(42,63, 10), get_flattened_index((slice(4,7),2), (10,10)))
         self.assertEqual(slice(40,61,10), get_flattened_index((slice(4,7),0), (10,10)))
         self.assertEqual(slice(4, 11, 2), get_flattened_index(slice(4,11,2), (20,)))
-        self.assertEqual(slice(40,50,1), 
+        self.assertEqual(slice(40,50,1),
                           get_flattened_index(slice(4,5,2), (20,10)))
 
         self.assertEqual(slice(1,2,None), get_flattened_index(1, (5,)))
