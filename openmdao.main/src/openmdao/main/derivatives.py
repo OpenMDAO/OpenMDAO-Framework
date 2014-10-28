30a6ded1a82c7250c644b2e08b98d846f8bbb502
Kenneth-T-Moore <kenneth.t.moore-1@nasa.gov>
2014-10-28 11:53:55 -0400

    debug

--- openmdao.main/src/openmdao/main/derivatives.py
+++ openmdao.main/src/openmdao/main/derivatives.py
@@ -155,7 +155,7 @@ def applyJ(system, variables):
             break
 
     if nonzero is False:
-        #print 'applyJ', obj.name, arg, result
+        print 'applyJ', obj.name, arg, result
         return
 
     # If storage of the local Jacobian is a problem, the user can specify the
@@ -188,7 +188,7 @@ def applyJ(system, variables):
             if hasattr(value, 'flatten'):
                 arg[key] = value.flatten()
 
-        #print 'applyJ', obj.name, arg, result
+        print 'applyJ', obj.name, arg, result
         return
 
     if is_sys:
@@ -255,7 +255,7 @@ def applyJ(system, variables):
 
             tmp += Jsub.dot(arg[ikey])
 
-    #print 'applyJ', obj.name, arg, result
+    print 'applyJ', obj.name, arg, result
 
 def applyJT(system, variables):
     """Multiply an input vector by the transposed Jacobian.
