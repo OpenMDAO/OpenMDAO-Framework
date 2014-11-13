13172f8e73300e5a4241f82551a7984c412f71aa
kmarsteller <Keith.G.Marsteller@nasa.gov>
2014-11-13 13:12:01 -0500

    adding requests to setup.py to fix release reqs.

--- openmdao.main/setup.py
+++ openmdao.main/setup.py
@@ -54,6 +54,7 @@ setup(name='openmdao.main',
           'openmdao.util',
           'pycrypto',
           'pyparsing',
+          'requests',
           'setuptools',
           'Sphinx',
           'Traits==4.3.0',
@@ -67,7 +68,7 @@ setup(name='openmdao.main',
       idle=idlelib.PyShell:main
       plugin=openmdao.main.plugin:plugin
       openmdao=openmdao.main.cli:openmdao
-      
+
       [openmdao.component]
       openmdao.main.assembly.Assembly = openmdao.main.assembly:Assembly
       openmdao.main.component_with_derivatives.ComponentWithDerivatives = openmdao.main.component_with_derivatives:ComponentWithDerivatives
@@ -94,6 +95,6 @@ setup(name='openmdao.main',
       openmdao.main.datatypes.uncertaindist.UncertainDistVar = openmdao.main.datatypes.uncertaindist:UncertainDistVar
       openmdao.main.datatypes.vtree.VarTree = openmdao.main.datatypes.vtree:VarTree
       openmdao.main.datatypes.array.Array = openmdao.main.datatypes.array:Array
-      
+
       """,
       )
