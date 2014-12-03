4ef098d8eb7945c7a31c57685cbe706e828159e2
Kenneth-T-Moore <kenneth.t.moore-1@nasa.gov>
2014-12-03 10:40:23 -0500

    cleanup

--- openmdao.main/src/openmdao/main/linearsolver.py
+++ openmdao.main/src/openmdao/main/linearsolver.py
@@ -483,18 +483,14 @@ class LinearGS(LinearSolver):
                         if subsystem is not subsystem2:
                             system.rhs_vec.array[:] = 0.0
                             args = subsystem.flat_vars.keys()
-                            #print 'C0', subsystem.name, subsystem2.name, system.vec['dp'].array, system.vec['du'].array, system.vec['df'].array, system.sol_buf[:], system.rhs_buf[:]
                             subsystem2.applyJ(args)
-                            print 'C1', subsystem.name, subsystem2.name, system.vec['dp'].array, system.vec['du'].array, system.vec['df'].array, system.sol_buf[:], system.rhs_buf[:]
+                            #print 'C1', subsystem.name, subsystem2.name, system.vec['dp'].array, system.vec['du'].array, system.vec['df'].array, system.sol_buf[:], system.rhs_buf[:]
                             system.scatter('du', 'dp', subsystem=subsystem2)
-                            print 'C2', subsystem.name, subsystem2.name, system.vec['dp'].array, system.vec['du'].array, system.vec['df'].array, system.sol_buf[:], system.rhs_buf[:]
+                            #print 'C2', subsystem.name, subsystem2.name, system.vec['dp'].array, system.vec['du'].array, system.vec['df'].array, system.sol_buf[:], system.rhs_buf[:]
                             system.sol_buf[:] -= system.rhs_vec.array[:]
                             system.vec['dp'].array[:] = 0.0
-                            #print 'C3', subsystem.name, subsystem2.name, system.vec['dp'].array, system.vec['du'].array, system.vec['df'].array, system.sol_buf[:], system.rhs_buf[:]
                     system.rhs_vec.array[:] = system.sol_buf[:]
-                    #print 'C4', subsystem.name, subsystem2.name, system.vec['dp'].array, system.vec['du'].array, system.vec['df'].array, system.sol_buf[:], system.rhs_buf[:]
                     subsystem.solve_linear(options)
-                    #print 'C5', subsystem.name, subsystem2.name, system.vec['dp'].array, system.vec['du'].array, system.vec['df'].array, system.sol_buf[:], system.rhs_buf[:]
  
             norm = self._norm()
             counter += 1
