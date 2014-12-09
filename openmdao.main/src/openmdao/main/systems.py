16ae20050b49c23e778e66cd307f79cf54e555c5
Kenneth-T-Moore <kenneth.t.moore-1@nasa.gov>
2014-12-09 14:38:22 -0500

    a little cleanup

--- openmdao.main/src/openmdao/main/systems.py
+++ openmdao.main/src/openmdao/main/systems.py
@@ -1037,9 +1037,9 @@ class SimpleSystem(System):
 
             for var in self.list_outputs():
 
-                collapsed = self.scope.name2collapsed.get(var)
-                if collapsed not in variables:
-                    continue
+                #collapsed = self.scope.name2collapsed.get(var)
+                #if collapsed not in variables:
+                #    continue
 
                 vec['du'][var][:] += vec['df'][var][:]
 
