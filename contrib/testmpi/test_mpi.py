56bf20580fb8e75c05f9dd7402fbd3d1aea5ba27
Bret Naylor <naylor.b@gmail.com>
2014-12-15 14:33:33 -0500

    remove systype = parallel from sellar test

--- contrib/testmpi/test_mpi.py
+++ contrib/testmpi/test_mpi.py
@@ -206,8 +206,6 @@ class MPITests2(MPITestCase):
 
         expected = { 'C1.y1': 3.160068, 'C2.y2': 3.755315 }
 
-        top.driver.system_type = 'parallel'
-
         top.run()
 
         if self.comm.rank == 0:
