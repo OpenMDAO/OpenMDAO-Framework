99b3ef619e1ebd57c6429ab58347ae7b0418fbba
hschilling <hschilling@nasa.gov>
2014-12-29 16:12:45 -0500

    Create conda recipe for fabric

--- conda-openmdao-dev.bat
+++ conda-openmdao-dev.bat
@@ -6,28 +6,30 @@ SET OLD_DIR=%CD%
 REM Create an openmdao environment and
 REM install numpy and scipy
 REM TODO: Should move all possible openmdao dependencies up here to simplify script
-SET DEPENDENCIES=numpy
-SET DEPENDENCIES=%DEPENDENCIES% scipy
-SET DEPENDENCIES=%DEPENDENCIES% setuptools
-SET DEPENDENCIES=%DEPENDENCIES% pyparsing
-SET DEPENDENCIES=%DEPENDENCIES% traits==4.3.0
-SET DEPENDENCIES=%DEPENDENCIES% nose
-SET DEPENDENCIES=%DEPENDENCIES% sphinx
-SET DEPENDENCIES=%DEPENDENCIES% "fabric>=0.9.3"
-SET DEPENDENCIES=%DEPENDENCIES% virtualenv
+REM SET DEPENDENCIES=numpy
+REM SET DEPENDENCIES=%DEPENDENCIES% scipy
+REM SET DEPENDENCIES=%DEPENDENCIES% setuptools
+REM SET DEPENDENCIES=%DEPENDENCIES% pyparsing
+REM SET DEPENDENCIES=%DEPENDENCIES% traits==4.3.0
+REM SET DEPENDENCIES=%DEPENDENCIES% nose
+REM SET DEPENDENCIES=%DEPENDENCIES% sphinx
+SET DEPENDENCIES=%DEPENDENCIES% "fabric==0.9.3"
+REM SET DEPENDENCIES=%DEPENDENCIES% virtualenv
 SET DEPENDENCIES=%DEPENDENCIES% boto
 SET DEPENDENCIES=%DEPENDENCIES% paramiko==1.7.7.1
 SET DEPENDENCIES=%DEPENDENCIES% requests
 SET DEPENDENCIES=%DEPENDENCIES% decorator
 SET DEPENDENCIES=%DEPENDENCIES% mock
 SET DEPENDENCIES=%DEPENDENCIES% networkx==1.8.1
-SET DEPENDENCIES=%DEPENDENCIES% zope.interface
-SET DEPENDENCIES=%DEPENDENCIES% "pytz>=2011"
-SET DEPENDENCIES=%DEPENDENCIES% pycrypto==2.3
-SET DEPENDENCIES=%DEPENDENCIES% cobyla
-SET DEPENDENCIES=%DEPENDENCIES% conmin
-SET DEPENDENCIES=%DEPENDENCIES% newsumt
-SET DEPENDENCIES=%DEPENDENCIES% slsqp
+REM SET DEPENDENCIES=%DEPENDENCIES% zope.interface
+REM SET DEPENDENCIES=%DEPENDENCIES% "pytz>=2011"
+REM SET DEPENDENCIES=%DEPENDENCIES% pycrypto==2.3
+REM SET DEPENDENCIES=%DEPENDENCIES% cobyla
+REM SET DEPENDENCIES=%DEPENDENCIES% conmin
+REM SET DEPENDENCIES=%DEPENDENCIES% newsumt
+REM SET DEPENDENCIES=%DEPENDENCIES% slsqp
+SET DEPENDENCIES=%DEPENDENCIES% bson
+SET DEPENDENCIES=%DEPENDENCIES% pyevolve
 
 @ECHO ON
 
@@ -35,28 +37,13 @@ conda create --yes --name openmdao %DEPENDENCIES%
 
 @ECHO OFF
 SET DEPENDENCIES=
-REM build and install some of the dependencies from OpenMDAO recipes
-REM bson
-conda install --yes --name openmdao bson
-IF %ERRORLEVEL% GTR 0 (
-   conda build conda-packaing\bson
-   conda install --yes --name openmdao bson
-)
-REM pyevolve
-conda install --yes --name openmdao pyevolve
-IF %ERRORLEVEL% GTR 0 (
-   conda build conda-packaging\pyevolve
-   conda install --yes --name openmdao pyevolve
-)
 
 REM Get the root directory of anaconda
 conda info --root >%TEMP%\conda-root
 SET /p CONDA_ROOT=<%TEMP%\conda-root
 DEL %TEMP%\conda-root
 SET PYTHON=%CONDA_ROOT%\envs\openmdao\python.exe
-SET PIP=%CONDA_ROOT%\envs\openmdao\Scripts\pip.exe
 
-%PIP% install
 REM install openmdao packages
 cd openmdao.units
 %PYTHON% setup.py develop
