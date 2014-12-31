b471f116202ea7ef42999ad4f829139082d945d9
hschilling <hschilling@nasa.gov>
2014-12-31 11:24:55 -0500

    Create libgfortran recipe. Set libgfortran as runtime dependency for slsqp.

---
+++ conda-packaging/libgfortran/build.sh
@@ -0,0 +1,3 @@
+cp /usr/local/lib/gcc/x86_64-apple-darwin12.5.0/4.8.3/libgfortran.3.dylib $PREFIX/lib/.
+cp /usr/local/lib/gcc/x86_64-apple-darwin12.5.0/4.8.3/libgcc_s.1.dylib $PREFIX/lib/.
+cp /usr/local/lib/gcc/x86_64-apple-darwin12.5.0/4.8.3/libquadmath.0.dylib $PREFIX/lib/.
