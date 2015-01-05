2384a653d9d3bae8c792d488ffc9611a089a2548
Robinson <crrobin3@viper.grc.nasa.gov>
2015-01-05 14:04:05 -0500

    Compile build extensions with rpaths. Create post-link scripts to fix hardcoded paths in extensions.

--- conda-packaging/conmin/build.sh
+++ conda-packaging/conmin/build.sh
@@ -1,10 +1,9 @@
 export CFLAGS="$CFLAGS -fPIC"
-export LDFLAGS="$LDFLAGS -shared"
 export PKG_VERSION="1.0.2"
 
-
 if [ -n "$OSX_ARCH" ]; then
-  export LDFLAGS="$LDFLAGS  -undefined dynamic_lookup -static-libgfortran"
+    export LDFLAGS="$LDFLAGS -dynamiclib -undefined dynamic_lookup -Xlinker -rpath -Xlinker @loader_path/../../../../../lib/openmdao"
 fi
 
+python setup.py build_ext
 python setup.py install
