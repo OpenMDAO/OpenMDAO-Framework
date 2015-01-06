76bac51ed3b34764057a282cdd819d2544c8c57c
unknown <crrobin3@grsdw00014149.ndc.nasa.gov>
2015-01-06 14:40:59 -0500

    Fixed build scripts to create shared objects for FORTRAN extensions.

--- conda-packaging/slsqp/build.sh
+++ conda-packaging/slsqp/build.sh
@@ -1,5 +1,8 @@
 export CFLAGS="$CFLAGS -fPIC"
-export PKG_VERSION="1.0.2"
+
+if [ "$(uname -s)" = "Linux" ]; then
+    export LDFLAGS="$LDFLAGS -shared"
+fi
 
 if [ -n "$OSX_ARCH" ]; then
     export LDFLAGS="$LDFLAGS -dynamiclib -undefined dynamic_lookup -Xlinker -rpath -Xlinker @loader_path/../../../../../lib/openmdao"
