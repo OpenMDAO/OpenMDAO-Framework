59d926181034e3cfda8c671769135988f380a0ca
Kenneth-T-Moore <kenneth.t.moore-1@nasa.gov>
2015-01-12 10:55:04 -0500

    test for Bret

--- contrib/testmpi/test_mpi_derivatives.py
+++ contrib/testmpi/test_mpi_derivatives.py
@@ -7,7 +7,7 @@ from openmdao.test.mpiunittest import MPITestCase, collective_assert_rel_error, 
 from openmdao.main.api import Assembly, Component, set_as_top
 from openmdao.main.datatypes.api import Float
 from openmdao.main.test.simpledriver import SimpleDriver
-from openmdao.test.execcomp import ExecCompWithDerivatives
+from openmdao.test.execcomp import ExecCompWithDerivatives, ExecComp
 
 class Paraboloid(Component):
     """ Evaluates the equation f(x,y) = (x-3)^2 + xy + (y+4)^2 - 3 """
@@ -557,6 +557,69 @@ class MPITests_2Proc(MPITestCase):
         collective_assert_rel_error(self, 
                                     J['comp5.y1']['comp1.x1'][0][0], 
                                     313.0, 0.0001)
+
+    def test_diverge_converge_nondiff_comp3_forward(self):
+        
+        self.top = set_as_top(Assembly())
+
+        exp1 = ['y1 = 2.0*x1**2',
+                'y2 = 3.0*x1']
+        deriv1 = ['dy1_dx1 = 4.0*x1',
+                  'dy2_dx1 = 3.0']
+
+        exp2 = ['y1 = 0.5*x1']
+        deriv2 = ['dy1_dx1 = 0.5']
+
+        exp3 = ['y1 = 3.5*x1']
+        deriv3 = ['dy1_dx1 = 3.5']
+
+        exp4 = ['y1 = x1 + 2.0*x2',
+                'y2 = 3.0*x1',
+                'y3 = x1*x2']
+        deriv4 = ['dy1_dx1 = 1.0',
+                  'dy1_dx2 = 2.0',
+                  'dy2_dx1 = 3.0',
+                  'dy2_dx2 = 0.0',
+                  'dy3_dx1 = x2',
+                  'dy3_dx2 = x1']
+
+        exp5 = ['y1 = x1 + 3.0*x2 + 2.0*x3']
+        deriv5 = ['dy1_dx1 = 1.0',
+                  'dy1_dx2 = 3.0',
+                  'dy1_dx3 = 2.0']
+        
+        self.top.add('driver', SimpleDriver())
+        self.top.add('comp1', ExecCompWithDerivatives(exp1, deriv1))
+        self.top.add('comp2', ExecCompWithDerivatives(exp2, deriv2))
+        self.top.add('comp3', ExecComp(exp3))
+        self.top.add('comp4', ExecCompWithDerivatives(exp4, deriv4))
+        self.top.add('comp5', ExecCompWithDerivatives(exp5, deriv5))
+
+        self.top.driver.workflow.add(['comp1', 'comp2', 'comp3', 'comp4', 'comp5'])
+
+        self.top.connect('comp1.y1', 'comp2.x1')
+        self.top.connect('comp1.y2', 'comp3.x1')
+        self.top.connect('comp2.y1', 'comp4.x1')
+        self.top.connect('comp3.y1', 'comp4.x2')
+        self.top.connect('comp4.y1', 'comp5.x1')
+        self.top.connect('comp4.y2', 'comp5.x2')
+        self.top.connect('comp4.y3', 'comp5.x3')
+        
+        self.top.driver.add_parameter('comp1.x1', low=-100, high=100)
+        self.top.driver.add_objective('comp5.y1')
+        
+        self.top.comp1.x1 = 2.0        
+        self.top.run()
+
+        J = self.top.driver.calc_gradient(inputs=['comp1.x1'],
+                                          outputs=['comp5.y1'],
+                                          mode='forward',
+                                          return_format='dict')
+        
+        collective_assert_rel_error(self, 
+                                    J['comp5.y1']['comp1.x1'][0][0], 
+                                    313.0, 0.0001)
+
 
 if __name__ == '__main__':
     import unittest
