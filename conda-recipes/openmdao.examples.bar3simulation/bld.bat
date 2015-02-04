c8fabeb27ffd762298da3e72a1fe6692b25acd12
unknown <crrobin3@grsdw00014149.ndc.nasa.gov>
2015-02-04 14:06:51 -0500

    Adding support for win-64 builds.

--- conda-recipes/openmdao.examples.bar3simulation/bld.bat
+++ conda-recipes/openmdao.examples.bar3simulation/bld.bat
@@ -1,3 +1,8 @@
+cd %SP_DIR%\numpy\distutils\fcompiler
+copy %RECIPE_DIR%\..\numpy.distutils-1.7.1.patch .
+git apply numpy.distutils-1.7.1.patch
+cd %SRC_DIR%
+
 cd examples\%PKG_NAME%
 python setup.py bdist_egg
 cd ..
