5e5cdbdf982328e05d2a643f6bfa27fdd67ca0c1
Kenneth-T-Moore <kenneth.t.moore-1@nasa.gov>
2015-02-06 15:50:23 -0500

    debug

--- openmdao.main/src/openmdao/main/systems.py
+++ openmdao.main/src/openmdao/main/systems.py
@@ -774,6 +774,9 @@ class System(object):
                       iterbase='', return_format='array'):
         """ Return the gradient for this system. """
 
+        print 'Name', self.name
+        print 'inputs', inputs
+        print 'outputs', outputs
         if not self.is_active():
             return
 
