caa3026bdf339071bcb6db298422b21a3be23323
Herb Schilling <hschilling@nasa.gov>
2015-03-24 16:34:10 -0400

    MPI test for parallel HDF5 recording is finished

--- contrib/testmpi/test_mpi_hdf5case.py
+++ contrib/testmpi/test_mpi_hdf5case.py
@@ -1,32 +1,12 @@
-from openmdao.lib.casehandlers.api import JSONCaseRecorder, BSONCaseRecorder, verify_json, CaseDataset, HDF5CaseRecorder
-import time
-import sys
-
 import numpy as np
-
-from openmdao.test.mpiunittest import MPITestCase
-from openmdao.util.testutil import assert_rel_error
-from openmdao.main.test.simpledriver import SimpleDriver
-
-from openmdao.main.api import Assembly, Component, set_as_top, Driver
-from openmdao.main.interfaces import implements, ISolver
-from openmdao.main.datatypes.api import Float, Array
-from openmdao.main.mpiwrap import MPI
-from openmdao.lib.drivers.iterate import FixedPointIterator
-from openmdao.lib.drivers.newton_solver import NewtonSolver
-from openmdao.test.execcomp import ExecComp
-from openmdao.main.test.test_derivatives import SimpleDriver
-
-from openmdao.lib.optproblems import sellar
-
 import h5py
 
-from openmdao.test.mpiunittest import MPITestCase, MPIContext
-
-import logging
-
-logger = logging.getLogger()
-logger.level = logging.DEBUG
+from openmdao.lib.casehandlers.api import HDF5CaseRecorder
+from openmdao.lib.drivers.iterate import FixedPointIterator
+from openmdao.lib.optproblems import sellar
+from openmdao.main.api import Assembly, set_as_top
+from openmdao.test.mpiunittest import MPITestCase
+from openmdao.util.testutil import assert_rel_error
 
 class SellarMDF(Assembly):
     """ Optimization of the Sellar problem using MDF
@@ -59,24 +39,36 @@ class SellarMDF(Assembly):
         self.driver.print_convergence = False
 
 def get_value_from_hdf5_file( names, values, name ):
+    """Get the value from an array whose keys are given in another array.
+
+    Args:
+       names (numpy array of str):  Keys of the value array.
+       values (numpy array of str/int/float):  Array of values.
+       name (str):  Key into the value array.
+
+    Returns:
+       str/int/float.  Returns the value from values using the key name::
+
+    """
 
     idx = np.nonzero( names == name )[0][0]
     return values[idx]
 
 
 def create_serial_file():
+    '''Create the HDF5 case record files for the serial case. Only run as needed'''
 
-        top = set_as_top(SellarMDF())
+    top = set_as_top(SellarMDF())
 
-        top.connect('C1.y1','C2.y1')
+    top.connect('C1.y1','C2.y1')
 
-        top.driver.add_parameter('C1.y2', low=-1.e99, high=1.e99)
-        top.driver.add_constraint('C1.y2 = C2.y2')
+    top.driver.add_parameter('C1.y2', low=-1.e99, high=1.e99)
+    top.driver.add_constraint('C1.y2 = C2.y2')
 
-        hdf5_filename = 'test_sellar_params1_serial.hdf5'
-        top.recorders = [HDF5CaseRecorder(hdf5_filename)]
+    hdf5_filename = 'test_sellar_params1_serial.hdf5'
+    top.recorders = [HDF5CaseRecorder(hdf5_filename)]
 
-        top.run()
+    top.run()
 
 #create_serial_file()  # Uncomment to create 'test_sellar_params1_serial.hdf5' and 'cases__driver.hdf5'
 
@@ -84,10 +76,12 @@ class MPITests1(MPITestCase):
 
     N_PROCS = 2
 
+    def setUp(self):
+        self.tolerance = 0.001
 
     def test_sellar_params1(self):
-        stream_handler = logging.StreamHandler(sys.stdout)
-        logger.addHandler(stream_handler)
+
+        global error_tolerance
 
         top = set_as_top(SellarMDF())
 
@@ -96,85 +90,58 @@ class MPITests1(MPITestCase):
         top.driver.add_parameter('C1.y2', low=-1.e99, high=1.e99)
         top.driver.add_constraint('C1.y2 = C2.y2')
 
-        expected = { 'C1.y1': 3.160068, 'C2.y2': 3.755315 }
-
         hdf5_filename = 'test_sellar_params1_parallel.hdf5'
         top.recorders = [HDF5CaseRecorder(hdf5_filename)]
 
         top.run()
 
+        # Only do the testing on rank 0 process
         if self.comm.rank == 0:
+            # Just check to make sure model returns the expected results
+            expected = { 'C1.y1': 3.160068, 'C2.y2': 3.755315 }
             for name, expval in expected.items():
                 val = top.get(name)
-                print 'asserts'
-                assert_rel_error(self, val, expval, 0.001)
+                assert_rel_error(self, val, expval, self.tolerance)
 
-
-        if self.comm.rank == 0:
+            # read the HDF5 file that was recorded to  
             hdf5_file = h5py.File(hdf5_filename,'r')
+        
+            # How many iterations were there?
+            num_iterations = 0
+            for key in hdf5_file['iteration_cases/driver/']:
+                if key.startswith( "iteration_case_"):
+                    num_iterations += 1
+
+            #   Were the expected results in the final case ?
             float_names = hdf5_file['iteration_cases/driver/float_names'].value
-            float_values = hdf5_file['iteration_cases/driver/iteration_case_6/data/array_of_floats'].value
-            if self.comm.rank == 0:
-                for name, expval in expected.items():
-                    val = get_value_from_hdf5_file(float_names, float_values, name)
-                    assert_rel_error(self, val, expval, 0.001)
+            float_values = hdf5_file['iteration_cases/driver/iteration_case_%d/data/array_of_floats' % num_iterations].value
+            for name, expval in expected.items():
+                val = get_value_from_hdf5_file(float_names, float_values, name)
+                assert_rel_error(self, val, expval, self.tolerance)
 
+            # Check to see that the constraint value is close to zero
             val = get_value_from_hdf5_file(float_names, float_values, '_pseudo_0.out0')
-            assert_rel_error(self, val, 0.0, 0.001)
+            assert_rel_error(self, val, 0.0, self.tolerance)
 
+            # Check to see that the exec_count matches the iteration count
             int_names = hdf5_file['iteration_cases/driver/int_names'].value
-            int_values = hdf5_file['iteration_cases/driver/iteration_case_6/data/array_of_ints'].value
-
-            self.assertTrue( get_value_from_hdf5_file( int_names, int_values, 'C1.exec_count' ) > 0 )
-            self.assertTrue( get_value_from_hdf5_file( int_names, int_values, 'C2.exec_count' ) > 0 )
-
+            int_values = hdf5_file['iteration_cases/driver/iteration_case_%d/data/array_of_ints' % num_iterations].value
+            self.assertEqual( get_value_from_hdf5_file( int_names, int_values, 'C1.exec_count' ) , num_iterations )
+            self.assertEqual( get_value_from_hdf5_file( int_names, int_values, 'C2.exec_count' ) , num_iterations )
 
             # Check the iternames
-            for i in range(6):
+            for i in range(num_iterations):
                 metadata = hdf5_file['iteration_cases/driver/iteration_case_%d/metadata' % (i+1)]
-                self.assertTrue(  metadata['_itername'][0] == '%d' % (i+1) )
+                self.assertEqual(  metadata['_itername'][0], '%d' % (i+1) )
 
-
-            logging.getLogger().info(  get_value_from_hdf5_file( int_names, int_values, 'C2.derivative_exec_count' ))
-
-
-
-
-
+            # make sure all the strings recorded have positive length
             str_names = hdf5_file['iteration_cases/driver/str_names'].value
-            str_values = hdf5_file['iteration_cases/driver/iteration_case_6/data/array_of_strs'].value
-
+            str_values = hdf5_file['iteration_cases/driver/iteration_case_%d/data/array_of_strs' % num_iterations].value
             for name in str_names:
                 self.assertTrue( len( get_value_from_hdf5_file( str_names, str_values, name) ) > 0 )
 
 
-
-        if self.comm.rank == 0:
-            # Read values from files to verify
-            print 'iteration_cases'
-            # logging.getLogger().info(dir(hdf5_file['iteration_cases']['driver']['float_names']))
-
-            expected = ['_pseudo_0.out0', 'C1.y1', 'C1.y2', 'C2.y2']
-            actual = hdf5_file['iteration_cases']['driver']['float_names'].value
-            for exp, act in zip( expected, actual ):
-                logging.getLogger().info( (exp, act) )
-                self.assertTrue( act == exp )
-
-            expected = [ -1.57732043e-06,   3.15986146e+00,   3.75520149e+00,   3.75519992e+00]
-            logging.getLogger().info(hdf5_file['iteration_cases/driver/float_names'].value)
-            logging.getLogger().info(hdf5_file['iteration_cases']['driver']['iteration_case_6']['data']['array_of_floats'].value)
-            logging.getLogger().info(hdf5_file['iteration_cases']['driver']['iteration_case_6']['metadata'].value)
-
-            print hdf5_file['iteration_cases']
-
-            #          GROUP "data" {
-            # DATASET "array_of_floats" {
-
-
-
     def test_sellar_compare_to_serial_case_recording(self):
-        stream_handler = logging.StreamHandler(sys.stdout)
-        logger.addHandler(stream_handler)
 
         top = set_as_top(SellarMDF())
 
@@ -190,29 +157,21 @@ class MPITests1(MPITestCase):
 
         if self.comm.rank == 0:
             # Open up and compare the non-metadata values in cases__driver_serial.hdf5 and cases__driver.hdf5
+            parallel_hdf5_cases_file = 'cases__driver.hdf5'
+            serial_hdf5_cases_file = 'cases__driver_serial.hdf5'
+            parallel_hdf5_file = h5py.File(parallel_hdf5_cases_file,'r')
+            serial_hdf5_file = h5py.File(serial_hdf5_cases_file,'r')
 
+            num_iterations = 0
+            for key in serial_hdf5_file:
+                if key.startswith( "iteration_case_"):
+                    num_iterations += 1
 
-
-
-
-
-# top = set_as_top(SellarMDF())
-
-# top.driver.max_iteration = 30
-# top.driver.max_iteration = 1
-# top.driver.add_parameter('C2.y1', low=-1e99, high=1e99)
-# top.driver.add_constraint('C1.y1 = C2.y1')
-# top.driver.add_parameter('C1.y2', low=-1.e99, high=1.e99)
-# top.driver.add_constraint('C2.y2 = C1.y2')
-
-# expected = { 'C1.y1': 3.1598617768014536, 'C2.y2': 3.7551999159927316 }
-
-# if MPI:
-#     top.recorders = [HDF5CaseRecorder('SellarMDF_parallel.hdf5')]
-# else:
-#     top.recorders = [HDF5CaseRecorder('SellarMDF_serial.hdf5')]
-
-# top.run()
+            for i in range(num_iterations): # check each of the iterations
+                parallel_float_values = parallel_hdf5_file['/iteration_case_%d/data/array_of_floats' % (i+1)].value
+                serial_float_values = serial_hdf5_file['/iteration_case_%d/data/array_of_floats' % (i+1)].value
+                for parallel_value, serial_value in zip( parallel_float_values, serial_float_values ):
+                    assert_rel_error(self, parallel_value, serial_value, self.tolerance)
 
 
 if __name__ == '__main__':
