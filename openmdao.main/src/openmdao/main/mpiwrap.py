2675d2dbc21dd383d97d8ae7414cf9fc5e3b79eb
Kenneth Moore <kenneth.t.moore-1@nasa.gov>
2015-03-24 14:41:37 -0400

    cleanup

--- openmdao.main/src/openmdao/main/mpiwrap.py
+++ openmdao.main/src/openmdao/main/mpiwrap.py
@@ -126,22 +126,22 @@ class MPI_info(object):
         return 0
 
 
-    def get_norm(vec, order=None):
-        """Either do a distributed norm or a local numpy
-        norm depending on whether we're running under MPI.
+def get_norm(vec, order=None):
+    """Either do a distributed norm or a local numpy
+    norm depending on whether we're running under MPI.
 
-        vec: VecWrapper
-            Returns the norm of this vector
+    vec: VecWrapper
+        Returns the norm of this vector
 
-        order: int, float, string (see numpy.linalg.norm)
-            Order of the norm (ignored in MPI)
-        """
+    order: int, float, string (see numpy.linalg.norm)
+        Order of the norm (ignored in MPI)
+    """
 
-        if MPI:
-            vec.petsc_vec.assemble()
-            return vec.petsc_vec.norm()
-        else:
-            return numpy.linalg.norm(vec.array, ord=order)
+    if MPI:
+        vec.petsc_vec.assemble()
+        return vec.petsc_vec.norm()
+    else:
+        return numpy.linalg.norm(vec.array, ord=order)
 
 
 # npnorm = numpy.linalg.norm
