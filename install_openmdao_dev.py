f4f6bcc1d78997ac37202d3a4ffcd19215f09d55
kmarsteller <Keith.G.Marsteller@nasa.gov>
2015-03-26 13:08:18 -0400

    cleaning up scripts, renaming one

--- install_openmdao_dev.py
+++ install_openmdao_dev.py
@@ -4,12 +4,12 @@
 #It will attempt to ascertain the user's environment
 #and then install using either: Anaconda Python or regular python/Virtualenv
 import sys
-import argparse
 import subprocess
 
 #If version of Python is unsatisfactory, throw an error with an
 #admonition that python must be of a certain version (currently >2.7, <3.0)
 def check_version():
+    print '\nInstalling OpenMDAO...'
     if sys.version_info < (2, 7) or sys.version_info > (3, 0):
         print'ERROR: {0}'.format(sys.exc_info()[1])
         print'ERROR: you have Python {0}.{1}.{2} installed'.format(sys.version_info[0], sys.version_info[1], sys.version_info[2])
@@ -17,25 +17,18 @@ def check_version():
         sys.exit(101)
 
     else:
-        print  'ACCEPTABLE PYTHON VERSION: {0}.{1}.{2} FOUND'.format(sys.version_info[0], sys.version_info[1], sys.version_info[2])
+        print  '\nACCEPTABLE PYTHON VERSION: {0}.{1}.{2} FOUND'.format(sys.version_info[0], sys.version_info[1], sys.version_info[2])
 
 #Find out which python (Anaconda/regular) is installed as the default Python
 def check_dist():
     version = sys.version
-    print "sys.version=={0}".format(version)
     #Looking for either 'Anaconda' or 'Continuum Analytics' for proof of Anaconda.
     if "Analytics" in version or "Anaconda" in version:
         dist = 'Anaconda'
-        if sys.platform == 'win32':
-            print 'Anaconda detected on Windows OS'
-        else:
-            print 'Anaconda detected on OSX or Linux'
+        print 'Anaconda detected. Running conda_build.py with args supplied.'
     else:
         dist = 'Virtualenv'
-        if sys.platform == 'win32':
-            print 'Regular Python detected on Windows'
-        else:
-            print 'Regular Python deteced on OSX or Linux'
+        print 'Regular Python detected. Running go-openmdao file with args supplied.'
 
     return dist
 
@@ -47,10 +40,10 @@ def install(dist):
     elif dist == "Virtualenv":
         cmd.append('go-openmdao-dev.py')
 
-    #Pass through args in argv other than the 0th entry, which is the cmd
+    #Pass through args in argv other than the 0th entry, which is a cmd
     for arg in sys.argv[1:]:
         cmd.append(arg)
-    print 'COMMAND CONSTRUCTED: {0}'.format(cmd)
+
     subprocess.call(cmd)
 
 
